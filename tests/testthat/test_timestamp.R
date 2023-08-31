6f286b5f63385f8105671a940701677c37e8a6db
Kirill Müller <kirill@cynkra.com>
2023-08-31 10:32:02 +0200

    Fix compatibility with R < 4.3

--- tests/testthat/test_timestamp.R
+++ tests/testthat/test_timestamp.R
@@ -2,7 +2,11 @@ test_that("fractional seconds can be roundtripped", {
   con <- dbConnect(duckdb())
   on.exit(dbDisconnect(con, shutdown = TRUE))
 
-  df <- data.frame(a = as.POSIXct(1.234567 + (1:100) * 1e-6, tz = "UTC"))
+  df <- data.frame(a = as.POSIXct(
+    1.234567 + (1:100) * 1e-6,
+    origin = structure(0, class = c("POSIXct", "POSIXt")),
+    tz = "UTC"
+  ))
   dbWriteTable(con, "df", df)
   df_out <- dbReadTable(con, "df")
   expect_equal(df_out, df)
