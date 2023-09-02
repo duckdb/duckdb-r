358bc9a8811af7e949a2118cecdd540daf1f16c4
Kirill Müller <kirill@cynkra.com>
2023-09-02 17:40:59 +0200

    Safer vendoring

--- vendor.sh
+++ vendor.sh
@@ -5,5 +5,21 @@ set -x
 
 cd `dirname $0`
 
+duckdir=../duckdb
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Error: working directory not clean"
+fi
+
+if [ -n "$(git -C $duckdir status --porcelain)" ]; then
+  echo "Error: working directory $duckdir not clean"
+fi
+
+commit=$(git -C $duckdir rev-parse HEAD)
+echo "Importing commit $commit"
+
 echo "R: configure"
 python3 rconfigure.py
+
+git add .
+git commit -m "Update vendored sources to duckdb/duckdb@$commit"
