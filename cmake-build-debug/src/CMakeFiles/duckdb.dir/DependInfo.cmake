c9f9e198f9313207bfd7814bb7118584103f2faf
Tmonster <tom@ebergen.com>
2023-09-14 12:51:20 +0200

    Update vendored sources to duckdb/duckdb@fe23ed6c34748706388fd8c4ffd61417472d1adc

---
+++ cmake-build-debug/src/CMakeFiles/duckdb.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/tomebergen/duckdb-r/src/connection.cpp" "src/CMakeFiles/duckdb.dir/connection.cpp.o" "gcc" "src/CMakeFiles/duckdb.dir/connection.cpp.o.d"
+  "/Users/tomebergen/duckdb-r/src/cpp11.cpp" "src/CMakeFiles/duckdb.dir/cpp11.cpp.o" "gcc" "src/CMakeFiles/duckdb.dir/cpp11.cpp.o.d"
+  "/Users/tomebergen/duckdb-r/src/database.cpp" "src/CMakeFiles/duckdb.dir/database.cpp.o" "gcc" "src/CMakeFiles/duckdb.dir/database.cpp.o.d"
+  "/Users/tomebergen/duckdb-r/src/register.cpp" "src/CMakeFiles/duckdb.dir/register.cpp.o" "gcc" "src/CMakeFiles/duckdb.dir/register.cpp.o.d"
+  "/Users/tomebergen/duckdb-r/src/relational.cpp" "src/CMakeFiles/duckdb.dir/relational.cpp.o" "gcc" "src/CMakeFiles/duckdb.dir/relational.cpp.o.d"
+  "/Users/tomebergen/duckdb-r/src/reltoaltrep.cpp" "src/CMakeFiles/duckdb.dir/reltoaltrep.cpp.o" "gcc" "src/CMakeFiles/duckdb.dir/reltoaltrep.cpp.o.d"
+  "/Users/tomebergen/duckdb-r/src/scan.cpp" "src/CMakeFiles/duckdb.dir/scan.cpp.o" "gcc" "src/CMakeFiles/duckdb.dir/scan.cpp.o.d"
+  "/Users/tomebergen/duckdb-r/src/statement.cpp" "src/CMakeFiles/duckdb.dir/statement.cpp.o" "gcc" "src/CMakeFiles/duckdb.dir/statement.cpp.o.d"
+  "/Users/tomebergen/duckdb-r/src/transform.cpp" "src/CMakeFiles/duckdb.dir/transform.cpp.o" "gcc" "src/CMakeFiles/duckdb.dir/transform.cpp.o.d"
+  "/Users/tomebergen/duckdb-r/src/types.cpp" "src/CMakeFiles/duckdb.dir/types.cpp.o" "gcc" "src/CMakeFiles/duckdb.dir/types.cpp.o.d"
+  "/Users/tomebergen/duckdb-r/src/utils.cpp" "src/CMakeFiles/duckdb.dir/utils.cpp.o" "gcc" "src/CMakeFiles/duckdb.dir/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/src/CMakeFiles/duckdb.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/duckdb.dir/connection.cpp.o"
+  "CMakeFiles/duckdb.dir/connection.cpp.o.d"
+  "CMakeFiles/duckdb.dir/cpp11.cpp.o"
+  "CMakeFiles/duckdb.dir/cpp11.cpp.o.d"
+  "CMakeFiles/duckdb.dir/database.cpp.o"
+  "CMakeFiles/duckdb.dir/database.cpp.o.d"
+  "CMakeFiles/duckdb.dir/register.cpp.o"
+  "CMakeFiles/duckdb.dir/register.cpp.o.d"
+  "CMakeFiles/duckdb.dir/relational.cpp.o"
+  "CMakeFiles/duckdb.dir/relational.cpp.o.d"
+  "CMakeFiles/duckdb.dir/reltoaltrep.cpp.o"
+  "CMakeFiles/duckdb.dir/reltoaltrep.cpp.o.d"
+  "CMakeFiles/duckdb.dir/scan.cpp.o"
+  "CMakeFiles/duckdb.dir/scan.cpp.o.d"
+  "CMakeFiles/duckdb.dir/statement.cpp.o"
+  "CMakeFiles/duckdb.dir/statement.cpp.o.d"
+  "CMakeFiles/duckdb.dir/transform.cpp.o"
+  "CMakeFiles/duckdb.dir/transform.cpp.o.d"
+  "CMakeFiles/duckdb.dir/types.cpp.o"
+  "CMakeFiles/duckdb.dir/types.cpp.o.d"
+  "CMakeFiles/duckdb.dir/utils.cpp.o"
+  "CMakeFiles/duckdb.dir/utils.cpp.o.d"
+  "libduckdb.a"
+  "libduckdb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/duckdb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
