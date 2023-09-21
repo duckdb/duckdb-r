e451c7ec6e0bff3f8a405fb3ad8c97ed0233bbbf
Kirill Müller <kirill@cynkra.com>
2023-09-20 22:46:45 -0500

    Install from binary

--- src/include/from-tar.mk
+++ src/include/from-tar.mk
@@ -1,8 +1,1 @@
-# This file is used from Makevars if $DUCKDB_R_PREBUILT_ARCHIVE points
-# to an existing file.
-# We print details on the object files already extracted in the configure script
-# for diagnostic purposes.
-duckdb.tar: $(DUCKDB_R_PREBUILT_ARCHIVE)
-	ls -lR
-	# Ensure the file is recreated in the next run
-	rm -f Makevars.duckdb
+PKG_LIBS_BINARY=-L"$(DUCKDB_BINARY_PATH)" -Wl,-rpath,"$(DUCKDB_BINARY_PATH)" -lduckdb
--- src/include/to-tar.mk
+++ src/include/to-tar.mk
@@ -1,3 +1,5 @@
+include include/sources.mk
+
 # This file is used from Makevars if $DUCKDB_R_PREBUILT_ARCHIVE is empty
 # or does not point to an existing file.
 # In the latter case, we create the file after the object files have been built.
