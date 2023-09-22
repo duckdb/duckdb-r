4a0f24c9b636935133dde9f1620a4dba8b830ff5
Tom Ebergen <tom@ebergen.com>
2023-09-22 15:05:13 +0200

    looks much better

--- tests/testthat/test_strings.R
+++ tests/testthat/test_strings.R
@@ -3,5 +3,5 @@ test_that("Invalid unicode produces an error", {
   on.exit(dbDisconnect(con, shutdown = TRUE))
 
   my_df <- structure(list(no_municipio_esc = "Est\xe2ncia", no_municipio_prova = "Est\xe2ncia"), row.names = 16L, class = "data.frame")
-  expect_error(dbWriteTable( con , 'my_table' , my_df ), "Cannot process strings that are not valid", ignore.case = TRUE)
+  expect_error(DBI::dbWriteTable( con , 'my_table' , my_df ), "Cannot process strings that are not valid", ignore.case = TRUE)
 })
