a76828c74ac21b4e1a34959e6bf6097230a2e1de
Kirill Müller <kirill@cynkra.com>
2023-10-11 16:06:21 +0200

    Update vendored sources to duckdb/duckdb@401c8061c6ece35949cac58c7770cc755710ca86

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.9.1-dev213"
+#define DUCKDB_VERSION "v0.9.1"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c49a0a20d6"
+#define DUCKDB_SOURCE_ID "401c8061c6"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
