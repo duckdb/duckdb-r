fbdeeb7cf1e1ba3d438e13e7945b28f2d570c5f7
Kirill Müller <kirill@cynkra.com>
2023-12-11 06:50:49 +0100

    chore: Update vendored sources to duckdb/duckdb@0c438555bd20304c819d1569f97234814899a90e
    Merge pull request duckdb/duckdb#9872 from Tishj/regexp_matches_null_optimizer
    Merge pull request duckdb/duckdb#9877 from hamilton/hamilton/autocomplete-add-from-to-initial-keywords
    Merge pull request duckdb/duckdb#9875 from Mytherin/progressbardisable

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.9.3-dev1065"
+#define DUCKDB_VERSION "v0.9.3-dev1071"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "13267997e8"
+#define DUCKDB_SOURCE_ID "0c438555bd"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
