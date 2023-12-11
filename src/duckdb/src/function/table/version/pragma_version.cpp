6d53dedbc94813c497645ea90494cf3fb5f8d55a
Kirill Müller <kirill@cynkra.com>
2023-12-11 06:49:43 +0100

    chore: Update vendored sources to duckdb/duckdb@95fb6b3e6e6e1f371e426e4350398958653bfc28
    Merge pull request duckdb/duckdb#9772 from hawkfish/skiplist-rand
    Merge pull request duckdb/duckdb#9767 from samansmink/fix-vcpkg-patching-on-windows
    Merge pull request duckdb/duckdb#9757 from Tmonster/712-unicode-bugs
    Merge pull request duckdb/duckdb#9758 from samansmink/fix-issue-9727
    Merge pull request duckdb/duckdb#9759 from samansmink/update-vcpkg-2023.10.19
    Merge pull request duckdb/duckdb#9761 from samansmink/add-nightly-deploy-script

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.9.3-dev785"
+#define DUCKDB_VERSION "v0.9.3-dev802"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b81d8c2df9"
+#define DUCKDB_SOURCE_ID "95fb6b3e6e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
