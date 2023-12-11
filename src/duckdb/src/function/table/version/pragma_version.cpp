2f30cfef219b28ced2d0e3bb092d97b717143955
Kirill Müller <kirill@cynkra.com>
2023-12-11 06:49:55 +0100

    chore: Update vendored sources to duckdb/duckdb@18f614f32df6363826d111c805517b7a9fb6eab3
    Merge pull request duckdb/duckdb#9736 from carlopi/format_bytes
    Merge pull request duckdb/duckdb#9659 from pdet/arrow_jdbc_segfault

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.9.3-dev849"
+#define DUCKDB_VERSION "v0.9.3-dev857"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "7524538e52"
+#define DUCKDB_SOURCE_ID "18f614f32d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
