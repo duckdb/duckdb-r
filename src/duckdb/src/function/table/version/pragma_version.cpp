11cf909074ffc8628d233b74e689b4943683f56a
Kirill Müller <kirill@cynkra.com>
2023-12-11 06:49:53 +0100

    chore: Update vendored sources to duckdb/duckdb@7524538e524d737cef3897fed00a3c879290921a
    Merge pull request duckdb/duckdb#9788 from carlopi/version_info
    Merge pull request duckdb/duckdb#9356 from carlopi/extension_template_wasm
    Merge pull request duckdb/duckdb#9782 from szarnyasg/freebsd
    Merge pull request duckdb/duckdb#9779 from dpprdan/chore/fix_duckdblabs_links

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.9.3-dev829"
+#define DUCKDB_VERSION "v0.9.3-dev849"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "9819b144fe"
+#define DUCKDB_SOURCE_ID "7524538e52"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
@@ -85,6 +85,8 @@ string DuckDB::Platform() {
 	os = "windows";
 #elif defined(__APPLE__)
 	os = "osx";
+#elif defined(__FreeBSD__)
+	os = "freebsd";
 #endif
 #if defined(__aarch64__) || defined(__ARM_ARCH_ISA_A64)
 	arch = "arm64";
