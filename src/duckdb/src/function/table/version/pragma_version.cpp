340cc82555cd25f86fbc68ed6d10b2fa0c42ce81
Kirill Müller <kirill@cynkra.com>
2023-12-11 06:50:54 +0100

    chore: Update vendored sources to duckdb/duckdb@fc961bb635f262c99e53eca13f891189695c588b
    Merge pull request duckdb/duckdb#9886 from jkub/bp_protected

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.9.3-dev1079"
+#define DUCKDB_VERSION "v0.9.3-dev1081"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d38d999df7"
+#define DUCKDB_SOURCE_ID "fc961bb635"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
