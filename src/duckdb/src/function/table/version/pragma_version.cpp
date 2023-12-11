7cfe70a3aa3a9d692b1382e684c213958c625f95
Kirill Müller <kirill@cynkra.com>
2023-12-11 06:51:11 +0100

    chore: Update vendored sources to duckdb/duckdb@8eaf58dc259f556ac1093ff8039bf8c51d40b97b
    Merge pull request duckdb/duckdb#9910 from hawkfish/strptime-isoweek

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.9.3-dev1175"
+#define DUCKDB_VERSION "v0.9.3-dev1179"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c14535ad5c"
+#define DUCKDB_SOURCE_ID "8eaf58dc25"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
