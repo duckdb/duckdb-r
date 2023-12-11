39dd8bb0a5591a000614494e91c14db922777e67
Kirill Müller <kirill@cynkra.com>
2023-12-11 06:50:27 +0100

    chore: Update vendored sources to duckdb/duckdb@849098b0031a241537033ef613b51200dfc1c945
    Merge pull request duckdb/duckdb#9438 from Mause/feature/c-api-create-nested-values

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.9.3-dev953"
+#define DUCKDB_VERSION "v0.9.3-dev972"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "73fa15b18a"
+#define DUCKDB_SOURCE_ID "849098b003"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
