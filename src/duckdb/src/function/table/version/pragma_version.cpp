3dfb9738b30c78f2aa764050a41591b5e63f215c
Kirill Müller <kirill@cynkra.com>
2023-12-11 06:49:34 +0100

    chore: Update vendored sources to duckdb/duckdb@5080b787204cffce031aeaa0478c523bac6467db
    Merge pull request duckdb/duckdb#9753 from Mytherin/attachname

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.9.3-dev759"
+#define DUCKDB_VERSION "v0.9.3-dev764"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "9898183b56"
+#define DUCKDB_SOURCE_ID "5080b78720"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
