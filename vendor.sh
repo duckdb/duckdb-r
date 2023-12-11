e2f26f0946b0dd8e6a6ddda5262a0105b1166805
Kirill Müller <kirill@cynkra.com>
2023-12-11 06:41:21 +0100

    chore: Add upstream log to commit message

--- vendor.sh
+++ vendor.sh
@@ -19,10 +19,12 @@ if [ -n "$(git -C "$duckdir" status --porcelain)" ]; then
   echo "Error: working directory $duckdir not clean"
 fi
 
-rm -rf src/duckdb
-
 commit=$(git -C "$duckdir" rev-parse HEAD)
 echo "Importing commit $commit"
+
+base=$(git log -n 1 --format="%s" -- src/duckdb | sed 's#^.*duckdb/duckdb@##')
+
+rm -rf src/duckdb
 
 echo "R: configure"
 python3 rconfigure.py
@@ -33,4 +35,9 @@ if [ $(git status --porcelain | wc -l) -le 1 ]; then
 fi
 
 git add .
-git commit -m "chore: Update vendored sources to duckdb/duckdb@$commit"
+
+(
+  echo "chore: Update vendored sources to duckdb/duckdb@$commit"
+  echo
+  git -C "$duckdir" log --first-parent --format="%s" ${base}..${commit} | sed -r 's%(#[0-9]+)%duckdb/duckdb\1%g'
+) | git commit --file /dev/stdin
