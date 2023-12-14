bd2c8fbb62c81a7408265926cc6aeae5bb0a14fd
Kirill Müller <kirill@cynkra.com>
2023-12-14 14:40:36 +0100

    chore: Update vendored sources to duckdb/duckdb@9f5e075ea348c39672b230fdf56b354a4e0cbf41
    Merge pull request duckdb/duckdb#9985 from hawkfish/summarize-overflow
    Merge pull request duckdb/duckdb#9972 from yiyuanliu/lyy/fix-9949
    Merge pull request duckdb/duckdb#9962 from Tishj/statement_error_expected_non_optional
    Merge pull request duckdb/duckdb#9961 from Tishj/python_supply_config_to_connect
    Merge pull request duckdb/duckdb#9947 from taniabogatsch/list-intersect
    Merge pull request duckdb/duckdb#9568 from Jens-H/append-BigDecimal
    Merge pull request duckdb/duckdb#9959 from Tishj/python_udf_arrow_side_effects
    Merge pull request duckdb/duckdb#9855 from StarveZhou/issue_9795_arg
    Merge pull request duckdb/duckdb#9944 from Tishj/pyproject_toml
    Merge pull request duckdb/duckdb#9946 from lnkuiper/issue9718
    Merge pull request duckdb/duckdb#9953 from mlafeldt/fix-httpfs-null-ptr
    Merge pull request duckdb/duckdb#9897 from Tmonster/left_semi_anti_feature_rebased
    Merge pull request duckdb/duckdb#9936 from hawkfish/empty-frames
    Merge pull request duckdb/duckdb#9715 from Tishj/dependency_set

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.9.3-dev1257"
+#define DUCKDB_VERSION "v0.9.3-dev1531"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e117c34a62"
+#define DUCKDB_SOURCE_ID "9f5e075ea3"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
---
+++ src/duckdb/ub_src_catalog_catalog_entry_dependency.cpp
@@ -0,0 +1,6 @@
+#include "src/catalog/catalog_entry/dependency/dependency_entry.cpp"
+
+#include "src/catalog/catalog_entry/dependency/dependency_subject_entry.cpp"
+
+#include "src/catalog/catalog_entry/dependency/dependency_dependent_entry.cpp"
+
