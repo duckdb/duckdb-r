8735f7c8876a79340b0180d2e93ad212c75787de
Kirill Müller <kirill@cynkra.com>
2024-02-23 10:34:48 +0100

    Pretend we're on the old version

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.1-dev2"
+#define DUCKDB_VERSION "v0.10.0"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "583508495b"
+#define DUCKDB_SOURCE_ID "20b1486d11"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
