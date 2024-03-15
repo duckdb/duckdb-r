10da4c0979a940c7f2ca29092ba333a3e0b577a7
krlmlr <krlmlr@users.noreply.github.com>
2024-03-15 19:34:47 +0000

    chore: Update vendored sources to duckdb/duckdb@b6d37ebe5c3766b08be9f8092132448673316120
    Merge pull request duckdb/duckdb#10446 from gitccl/fix_plan_join

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.1-dev414"
+#define DUCKDB_VERSION "v0.10.1-dev417"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "372c514af8"
+#define DUCKDB_SOURCE_ID "b6d37ebe5c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
