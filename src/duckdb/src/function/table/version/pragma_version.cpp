8d48f60acd5ba4deec049956af882365a759e47a
krlmlr <krlmlr@users.noreply.github.com>
2024-03-15 08:44:37 +0000

    chore: Update vendored sources to duckdb/duckdb@b22e8b709d82ef5f30c4a592c599f709dddaf39e
    Merge pull request duckdb/duckdb#10758 from barracuda156/fix_macos

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.1-dev225"
+#define DUCKDB_VERSION "v0.10.1-dev228"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1a400858ba"
+#define DUCKDB_SOURCE_ID "b22e8b709d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
