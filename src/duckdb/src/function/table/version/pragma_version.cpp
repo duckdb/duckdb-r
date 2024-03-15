ab485c9e2477227e1341cad0917d28742773f571
krlmlr <krlmlr@users.noreply.github.com>
2024-03-15 19:03:23 +0000

    chore: Update vendored sources to duckdb/duckdb@26696a1d8622f3885f6a9128f7676b576d3825ad
    Merge pull request duckdb/duckdb#10742 from hawkfish/fuzzer-decimal-order
    Merge pull request duckdb/duckdb#10832 from krlmlr/f-setup-python-v5

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.1-dev403"
+#define DUCKDB_VERSION "v0.10.1-dev409"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "fff576c338"
+#define DUCKDB_SOURCE_ID "26696a1d86"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
