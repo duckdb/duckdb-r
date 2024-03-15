f4155e7b754ee9235d8a72853a2908fbd0095dbf
krlmlr <krlmlr@users.noreply.github.com>
2024-03-15 16:04:25 +0000

    chore: Update vendored sources to duckdb/duckdb@3c135eba512fd18a7f377b962624189a3c284d5a
    Merge pull request duckdb/duckdb#10796 from yiyuanliu/lyy/arrow-map
    Merge pull request duckdb/duckdb#10692 from maiadegraaf/ODBC_Connect
    Merge pull request duckdb/duckdb#10814 from szarnyasg/update-readme

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,8 +1,8 @@
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.1-dev267"
+#define DUCKDB_VERSION "v0.10.1-dev342"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "35bbaac3f7"
+#define DUCKDB_SOURCE_ID "3c135eba51"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
