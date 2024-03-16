54d2d3d73b28586d7d4641e4a1e03ceb35b6e137
krlmlr <krlmlr@users.noreply.github.com>
2024-03-16 17:50:23 +0000

    chore: Update vendored sources to duckdb/duckdb@80fa08cca035ed762433552bbb46e283f0f48508
    Merge pull request duckdb/duckdb#10983 from hawkfish/fuzzer-quantile-hugeint

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev885"
+#define DUCKDB_PATCH_VERSION "1-dev887"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.1-dev885"
+#define DUCKDB_VERSION "v0.10.1-dev887"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "96be48e5f5"
+#define DUCKDB_SOURCE_ID "80fa08cca0"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
