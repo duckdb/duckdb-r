6b09198763942073a5bcb0ed02e5a1054e8a746f
krlmlr <krlmlr@users.noreply.github.com>
2024-03-16 14:15:21 +0000

    chore: Update vendored sources to duckdb/duckdb@65926ee2a8dd117f16b624236f0181deb335c911
    Merge pull request duckdb/duckdb#10957 from hawkfish/fuzzer-interval-diff

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev784"
+#define DUCKDB_PATCH_VERSION "1-dev786"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.1-dev784"
+#define DUCKDB_VERSION "v0.10.1-dev786"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "459094ea54"
+#define DUCKDB_SOURCE_ID "65926ee2a8"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
