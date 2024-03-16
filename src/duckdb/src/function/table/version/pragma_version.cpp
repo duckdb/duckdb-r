bae6a285af50fce8f561e01848f3ff105fde6b1b
krlmlr <krlmlr@users.noreply.github.com>
2024-03-16 21:37:19 +0000

    chore: Update vendored sources to duckdb/duckdb@f8730fe397710e0e212c03a67465f2b3bef2ad04
    Merge pull request duckdb/duckdb#11043 from kryonix/rec_cte_fix
    Merge pull request duckdb/duckdb#11036 from taniabogatsch/enable-block-size-tests

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev965"
+#define DUCKDB_PATCH_VERSION "1-dev969"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.1-dev965"
+#define DUCKDB_VERSION "v0.10.1-dev969"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "953959558c"
+#define DUCKDB_SOURCE_ID "f8730fe397"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
