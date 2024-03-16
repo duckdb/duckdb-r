44eb8bbaaeb25cd4cd8041442dddb87c237c5ae6
krlmlr <krlmlr@users.noreply.github.com>
2024-03-16 12:03:39 +0000

    chore: Update vendored sources to duckdb/duckdb@a7e8da76c0ae5658f1f9f77b384c4dda480c864d
    Merge pull request duckdb/duckdb#10944 from Maxxen/array-fuzzfix
    Merge pull request duckdb/duckdb#10931 from guenp/guen/eco-68-google-colab-oauth-does-not-work
    Merge pull request duckdb/duckdb#10942 from Mytherin/skipnumerictest

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev754"
+#define DUCKDB_PATCH_VERSION "1-dev767"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.1-dev754"
+#define DUCKDB_VERSION "v0.10.1-dev767"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "05176cd88e"
+#define DUCKDB_SOURCE_ID "a7e8da76c0"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
