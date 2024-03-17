1568b780c5f6060ec47587239c9c353626c5d441
krlmlr <krlmlr@users.noreply.github.com>
2024-03-17 21:08:55 +0000

    chore: Update vendored sources to duckdb/duckdb@1f42ae1473d132fb52f4ccb741971ff8f37785bb
    Merge pull request duckdb/duckdb#11179 from carlopi/override_git_describe_in_scripts

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev1358"
+#define DUCKDB_PATCH_VERSION "1-dev1367"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.1-dev1358"
+#define DUCKDB_VERSION "v0.10.1-dev1367"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "2bf340412a"
+#define DUCKDB_SOURCE_ID "g1f42ae1473"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
