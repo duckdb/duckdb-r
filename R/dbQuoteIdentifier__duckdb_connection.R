78fe40e5807405409656001928db4340efa43878
Kirill Müller <kirill@cynkra.com>
2024-03-24 06:36:50 +0100

    Remove dead code

--- R/dbQuoteIdentifier__duckdb_connection.R
+++ R/dbQuoteIdentifier__duckdb_connection.R
@@ -11,7 +11,7 @@ dbQuoteIdentifier__duckdb_connection <- function(conn, x, ...) {
   }
 
   x <- enc2utf8(x)
-  needs_escape <- !grepl("^[a-zA-Z_][a-zA-Z0-9_]*$", x) | tolower(x) %in% reserved_words(conn)
+  needs_escape <- !grepl("^[a-zA-Z_][a-zA-Z0-9_]*$", x) | tolower(x) %in% conn@reserved_words
   x[needs_escape] <- paste0('"', gsub('"', '""', x[needs_escape]), '"')
 
   SQL(x, names = names(x))
@@ -29,17 +29,6 @@ setMethod("dbQuoteIdentifier", signature("duckdb_connection", "SQL"), dbQuoteIde
 #' @rdname duckdb_connection-class
 #' @usage NULL
 setMethod("dbQuoteIdentifier", signature("duckdb_connection", "Id"), dbQuoteIdentifier__duckdb_connection)
-
-reserved_words <- function(con) {
-  if (!isS4(con)) {
-    con <- dbConnect(duckdb())
-    words <- get_reserved_words(con)
-    dbDisconnect(con, shutdown = TRUE)
-    words
-  }
-
-  con@reserved_words
-}
 
 get_reserved_words <- function(con) {
   dbGetQuery(con, "SELECT keyword_name FROM duckdb_keywords()")[[1]]
