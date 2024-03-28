30afbfc3c0204b92271b2520a1fb4f5b86e40b2e
krlmlr <krlmlr@users.noreply.github.com>
2024-03-28 12:50:45 +0000

    chore: Update vendored sources to duckdb/duckdb@4f80905a76f715244ad94461702eb933346a336a
    Merge pull request duckdb/duckdb#11325 from Mytherin/issue11284
    Merge pull request duckdb/duckdb#11333 from carlopi/fixup_py_upload2
    Merge pull request duckdb/duckdb#11328 from Mytherin/issue11319
    Merge pull request duckdb/duckdb#11324 from Mytherin/windowscli

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev179"
+#define DUCKDB_PATCH_VERSION "2-dev189"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.2-dev179"
+#define DUCKDB_VERSION "v0.10.2-dev189"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "5d065f3a19"
+#define DUCKDB_SOURCE_ID "4f80905a76"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
