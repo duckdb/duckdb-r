5f56f36fb6cbbc655a49179e2ee0b5420999ec62
Tom Ebergen <tom@ebergen.com>
2024-04-03 17:03:41 +0200

    view is created temporarily as create or replace temp view

--- tests/testthat/test-readonly.R
+++ tests/testthat/test-readonly.R
@@ -49,3 +49,17 @@ test_that("read_only flag and shutdown works as expected", {
 
   expect_true(TRUE)
 })
+
+test_that("read_only flag does not throw error when rel_sql is called", {
+    unlink("tmp.duckdb")
+    con <- dbConnect(duckdb(), "tmp.duckdb")
+    # just need a db to open in read only mode
+    dbExecute(con, "create table t1 as select range a from range(2)")
+    dbDisconnect(con)
+
+    con <- dbConnect(duckdb(), "tmp.duckdb", read_only=TRUE)
+    rel <- duckdb:::rel_from_df(con, mtcars)
+    ans <- data.frame(cyl=c(4), disp=c(71.1))
+    expect_rel2 <- duckdb:::rel_sql(rel, "SELECT cyl, disp FROM _ where disp = 71.1")
+    expect_equivalent(expect_rel2, ans)
+})
