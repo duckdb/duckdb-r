ce54dd1842b3e23d3261948f203c19d7cf8b167e
Kirill Müller <kirill@cynkra.com>
2024-04-29 12:21:49 +0200

    Disconnect

--- tests/testthat/test-rfuns.R
+++ tests/testthat/test-rfuns.R
@@ -7,4 +7,6 @@ test_that("can load rfuns extension and run functions", {
   rapi_load_rfuns(drv@database_ref)
 
   expect_equal(dbGetQuery(con, 'SELECT "r_base::=="(1, 2)')[[1]], FALSE)
+
+  dbDisconnect(con)
 })
