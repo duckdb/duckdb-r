8d123c1eeb4c711984f41d2d4f3baab89a77d13b
Kirill Müller <kirill@cynkra.com>
2024-05-03 08:39:01 +0200

    chore: Update vendored sources to duckdb/duckdb@ddfe1cd3f6e4be3e041eb892dea40b7b32fdb36d
    Merge pull request duckdb/duckdb#11725 from carlopi/c20
    Merge pull request duckdb/duckdb#11724 from Tishj/python_sqllogictest_fix_temp_directory

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev210"
+#define DUCKDB_PATCH_VERSION "3-dev222"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.3-dev210"
+#define DUCKDB_VERSION "v0.10.3-dev222"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "6bd83eed6a"
+#define DUCKDB_SOURCE_ID "ddfe1cd3f6"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
