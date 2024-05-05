bab7543e94fda419362e9148d8bd3af50dfc50f9
krlmlr <krlmlr@users.noreply.github.com>
2024-05-05 21:54:04 +0000

    chore: Update vendored sources to duckdb/duckdb@1bdf93c55098eaf0579d520b6df8dda78449a0c5
    Merge pull request duckdb/duckdb#11861 from hawkfish/timetz-compare
    Merge pull request duckdb/duckdb#11834 from Tishj/pyrelation_fetch_df_chunk_sync

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev413"
+#define DUCKDB_PATCH_VERSION "3-dev418"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.3-dev413"
+#define DUCKDB_VERSION "v0.10.3-dev418"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "50f9d9c095"
+#define DUCKDB_SOURCE_ID "1bdf93c550"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
