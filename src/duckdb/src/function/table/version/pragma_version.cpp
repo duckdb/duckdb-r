904d6c2512b2bf8190ad684dc985cd87acf2bd86
krlmlr <krlmlr@users.noreply.github.com>
2024-05-06 04:40:15 +0000

    chore: Update vendored sources to duckdb/duckdb@540b1949425a4652c30e891f298a42f425c9e3fa
    Merge pull request duckdb/duckdb#11441 from jkub/age_based_eviction

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev773"
+#define DUCKDB_PATCH_VERSION "3-dev775"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.3-dev773"
+#define DUCKDB_VERSION "v0.10.3-dev775"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e484b612f9"
+#define DUCKDB_SOURCE_ID "540b194942"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
