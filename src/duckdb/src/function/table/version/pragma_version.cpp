52ded679364ac71600653827350a1c489b2c3fd3
krlmlr <krlmlr@users.noreply.github.com>
2024-05-06 00:26:05 +0000

    chore: Update vendored sources to duckdb/duckdb@2c0e72a4025cb2aa77bb4a897eaffed32ddd0651
    Merge pull request duckdb/duckdb#11879 from Tishj/hugeint_decimal_merge
    Merge pull request duckdb/duckdb#11882 from Tmonster/1924-fix-nightly-constant-vector-verification

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev526"
+#define DUCKDB_PATCH_VERSION "3-dev534"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.3-dev526"
+#define DUCKDB_VERSION "v0.10.3-dev534"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "243083fd81"
+#define DUCKDB_SOURCE_ID "2c0e72a402"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
