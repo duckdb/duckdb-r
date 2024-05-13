9d22692c910c4dfef401ee361d692c50bc795254
krlmlr <krlmlr@users.noreply.github.com>
2024-05-13 08:03:42 +0000

    chore: Update vendored sources to duckdb/duckdb@87f33a9a4dfac5f59cb32fd65d8632ecc1d8e86a
    Merge pull request duckdb/duckdb#11994 from softprops/fix-extension-load-error-message-grammar

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev857"
+#define DUCKDB_PATCH_VERSION "3-dev861"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.3-dev857"
+#define DUCKDB_VERSION "v0.10.3-dev861"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "288d68e00c"
+#define DUCKDB_SOURCE_ID "87f33a9a4d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
