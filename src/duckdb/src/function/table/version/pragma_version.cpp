4d6226b4858f5e281677e93ed151024050c468f9
krlmlr <krlmlr@users.noreply.github.com>
2024-05-13 08:21:05 +0000

    chore: Update vendored sources to duckdb/duckdb@e6b98644ebcacc7ac112641ec259088bf03cd6ff
    Merge pull request duckdb/duckdb#11982 from Tishj/python_empty_polars_df

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev861"
+#define DUCKDB_PATCH_VERSION "3-dev866"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.3-dev861"
+#define DUCKDB_VERSION "v0.10.3-dev866"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "87f33a9a4d"
+#define DUCKDB_SOURCE_ID "e6b98644eb"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
