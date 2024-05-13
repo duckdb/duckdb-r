03f1ffdac779b1cd91e8dd5d533d34235d1c893d
krlmlr <krlmlr@users.noreply.github.com>
2024-05-13 12:18:28 +0000

    chore: Update vendored sources to duckdb/duckdb@b9ecd8da3a073ced0a9b6efbdf874f66564e68ba
    Merge pull request duckdb/duckdb#11831 from samansmink/hugging-face-fs
    Merge pull request duckdb/duckdb#11981 from Tishj/python_optional_numpy
    Merge pull request duckdb/duckdb#11980 from Tishj/pyfs_needs_gil

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev876"
+#define DUCKDB_PATCH_VERSION "3-dev897"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.3-dev876"
+#define DUCKDB_VERSION "v0.10.3-dev897"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ffb3f9789f"
+#define DUCKDB_SOURCE_ID "b9ecd8da3a"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
