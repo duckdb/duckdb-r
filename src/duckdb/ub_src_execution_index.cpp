1af9c1e05913143f253b4ffb30eb3ab52736ae52
krlmlr <krlmlr@users.noreply.github.com>
2024-05-13 17:15:49 +0000

    chore: Update vendored sources to duckdb/duckdb@2b476ebec7bafa57cc9860535612558e90816247
    Merge pull request duckdb/duckdb#11867 from Maxxen/index-binding
    Merge pull request duckdb/duckdb#11948 from lnkuiper/json_structure_fix

--- src/duckdb/ub_src_execution_index.cpp
+++ src/duckdb/ub_src_execution_index.cpp
@@ -2,7 +2,9 @@
 
 #include "src/execution/index/fixed_size_buffer.cpp"
 
-#include "src/execution/index/unknown_index.cpp"
+#include "src/execution/index/unbound_index.cpp"
 
 #include "src/execution/index/index_type_set.cpp"
+
+#include "src/execution/index/bound_index.cpp"
 
