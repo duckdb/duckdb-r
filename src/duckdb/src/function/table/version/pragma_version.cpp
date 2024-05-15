87aa2d6d84c9c672f13a79a16cc5d1ba32fc2e1c
krlmlr <krlmlr@users.noreply.github.com>
2024-05-15 18:21:10 +0000

    chore: Update vendored sources to duckdb/duckdb@a79719e2f9fe51994ef89f7763df568b5f955957
    Merge pull request duckdb/duckdb#12052 from hawkfish/timetz-nanos

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev1017"
+#define DUCKDB_PATCH_VERSION "3-dev1019"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.3-dev1017"
+#define DUCKDB_VERSION "v0.10.3-dev1019"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e372eab183"
+#define DUCKDB_SOURCE_ID "a79719e2f9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
