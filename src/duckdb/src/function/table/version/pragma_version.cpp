670bfeac9851541d2db39e8b99bd1f6fa3502bce
krlmlr <krlmlr@users.noreply.github.com>
2024-05-16 18:42:10 +0000

    chore: Update vendored sources to duckdb/duckdb@73784670cbd05859f780d298b5a08fa13c3f19c9
    Merge pull request duckdb/duckdb#12087 from Mytherin/windowsubquery

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev1144"
+#define DUCKDB_PATCH_VERSION "3-dev1147"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.3-dev1144"
+#define DUCKDB_VERSION "v0.10.3-dev1147"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b3fb5cfba0"
+#define DUCKDB_SOURCE_ID "73784670cb"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
