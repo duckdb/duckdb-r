52499c4c82fcadcc68cd31e18e9f5c7f37ffeb20
krlmlr <krlmlr@users.noreply.github.com>
2024-05-17 21:50:52 +0000

    chore: Update vendored sources to duckdb/duckdb@cd2798920d5e3aeaa2003f6e0bc1bf530f8ceb0c
    Merge pull request duckdb/duckdb#12112 from samansmink/fix-huggingface-credential-chain-autoload
    Merge pull request duckdb/duckdb#12107 from carlopi/bump-extensions

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev1224"
+#define DUCKDB_PATCH_VERSION "3-dev1233"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.3-dev1224"
+#define DUCKDB_VERSION "v0.10.3-dev1233"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1c832c2ad0"
+#define DUCKDB_SOURCE_ID "cd2798920d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
