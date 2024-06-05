814150704e985dc810092070615719e60411c075
krlmlr <krlmlr@users.noreply.github.com>
2024-06-05 21:05:36 +0000

    chore: Update vendored sources to duckdb/duckdb@6ca5d6014bf29e2403c38effe0e5dc3b8f011fe4
    Merge pull request duckdb/duckdb#12266 from samansmink/add-virtual-descructor-to-mfr-global-state
    Merge pull request duckdb/duckdb#12258 from taniabogatsch/wal-unpinned
    Merge pull request duckdb/duckdb#12255 from Tishj/fix_python_examples
    Merge pull request duckdb/duckdb#12253 from carlopi/fix_git_describe
    Merge pull request duckdb/duckdb#12223 from paparodeo/add-hffs-httpfs-config
    Merge pull request duckdb/duckdb#12245 from carlopi/fixup_staged_3

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev61"
+#define DUCKDB_PATCH_VERSION "4-dev76"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 10
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 0
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v0.10.4-dev61"
+#define DUCKDB_VERSION "v0.10.4-dev76"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b54303fa0e"
+#define DUCKDB_SOURCE_ID "6ca5d6014b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
