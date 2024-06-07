bd9d247a246d2774c7403f4eb2a84d296dbf2f99
krlmlr <krlmlr@users.noreply.github.com>
2024-06-07 16:17:06 +0000

    chore: Update vendored sources to duckdb/duckdb@7732a694863a78c539692ec9febad9fe3017a5c1
    Merge pull request duckdb/duckdb#12391 from lnkuiper/pin_deadlock
    Merge pull request duckdb/duckdb#12383 from gjmwoods/fix-swift-type

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev50"
+#define DUCKDB_PATCH_VERSION "1-dev55"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev50"
+#define DUCKDB_VERSION "v1.0.1-dev55"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d0616c5f74"
+#define DUCKDB_SOURCE_ID "7732a69486"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
