d94685e2de214c228fa03ff5352c02ab0183e432
krlmlr <krlmlr@users.noreply.github.com>
2024-06-10 11:20:17 +0000

    chore: Update vendored sources to duckdb/duckdb@2827fc2c41f52df99d2891ce22786c15580ed04b
    Merge pull request duckdb/duckdb#12448 from hawkfish/asof-filters

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev109"
+#define DUCKDB_PATCH_VERSION "1-dev111"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev109"
+#define DUCKDB_VERSION "v1.0.1-dev111"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "cb8e128b11"
+#define DUCKDB_SOURCE_ID "2827fc2c41"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
