c181566db99ebc85e518debe6b1783af394fc13d
krlmlr <krlmlr@users.noreply.github.com>
2024-06-12 09:07:46 +0000

    chore: Update vendored sources to duckdb/duckdb@8730d00291e99715cb4554711052f4a6e6605c24
    Merge pull request duckdb/duckdb#12456 from Mytherin/informationschemainternal

--- src/duckdb/ub_src_catalog_default.cpp
+++ src/duckdb/ub_src_catalog_default.cpp
@@ -1,5 +1,7 @@
 #include "src/catalog/default/default_functions.cpp"
 
+#include "src/catalog/default/default_generator.cpp"
+
 #include "src/catalog/default/default_schemas.cpp"
 
 #include "src/catalog/default/default_types.cpp"
