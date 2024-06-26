6bebfaadf5cef01d108c320e1c035e34a69365be
Kirill Müller <kirill@cynkra.com>
2024-06-26 11:39:46 +0200

    Revert "fix: Avoid `R CMD check` warning regarding `SETLENGTH()` and `SET_TRUELENGTH()`"
    This reverts commit 514ae5217ba71998eeda142739f879744eb4176c.

--- inst/include/cpp11/r_vector.hpp
+++ inst/include/cpp11/r_vector.hpp
@@ -890,8 +890,13 @@ inline void r_vector<T>::clear() {
 }
 
 inline SEXP truncate(SEXP x, R_xlen_t length, R_xlen_t capacity) {
-  // Avoid SETLENGTH() and SET_TRUELENGTH() which trigger a warning on R-devel 4.5
+#if R_VERSION >= R_Version(3, 4, 0)
+  SETLENGTH(x, length);
+  SET_TRUELENGTH(x, capacity);
+  SET_GROWABLE_BIT(x);
+#else
   x = safe[Rf_lengthgets](x, length);
+#endif
   return x;
 }
 
