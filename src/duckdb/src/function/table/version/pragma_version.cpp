6bbf13a4cc70501df6f6e4abcbffb4fefd70abc3
krlmlr <krlmlr@users.noreply.github.com>
2024-07-04 21:30:36 +0000

    chore: Update vendored sources to duckdb/duckdb@f5c3b6996f5e7d510429be589e343d59ea396f73
    Merge pull request duckdb/duckdb#12765 from ccfelius/win_dlopen

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev2074"
+#define DUCKDB_PATCH_VERSION "1-dev2076"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev2074"
+#define DUCKDB_VERSION "v1.0.1-dev2076"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "5ebd8ca697"
+#define DUCKDB_SOURCE_ID "f5c3b6996f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
