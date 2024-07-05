1ddc656b1e8ad9924c2bfce398a1a22ae4385654
krlmlr <krlmlr@users.noreply.github.com>
2024-07-05 14:20:09 +0000

    chore: Update vendored sources to duckdb/duckdb@e92a3d07c98996dd906b617fc6ea116d890b086a
    Merge pull request duckdb/duckdb#12847 from Maxxen/fix-union-struct-implict-cast

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev2242"
+#define DUCKDB_PATCH_VERSION "1-dev2248"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev2242"
+#define DUCKDB_VERSION "v1.0.1-dev2248"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f55702271b"
+#define DUCKDB_SOURCE_ID "e92a3d07c9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
