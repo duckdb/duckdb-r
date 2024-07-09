16dc142e9daf6f4e0a753b70ea83ce853f4536f6
krlmlr <krlmlr@users.noreply.github.com>
2024-07-09 13:56:23 +0000

    chore: Update vendored sources to duckdb/duckdb@60dbbd87ed667fd941135a4d571a84333378fcb6
    Merge pull request duckdb/duckdb#12851 from Tmonster/respect_limit_during_join_order

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev2387"
+#define DUCKDB_PATCH_VERSION "1-dev2394"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev2387"
+#define DUCKDB_VERSION "v1.0.1-dev2394"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ef56991b4d"
+#define DUCKDB_SOURCE_ID "60dbbd87ed"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
