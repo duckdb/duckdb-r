838348027bb67b5259a92324baea14815e63fd30
krlmlr <krlmlr@users.noreply.github.com>
2024-07-09 08:34:10 +0000

    chore: Update vendored sources to duckdb/duckdb@fd883da87821a3e4a60169df7cba49f29d7a252e
    Merge pull request duckdb/duckdb#12888 from Tishj/pysqllogictest_fixes2
    Merge pull request duckdb/duckdb#12899 from dhanak/main
    Merge pull request duckdb/duckdb#12896 from Tishj/pandas_timestamp_3_7_issue
    Merge pull request duckdb/duckdb#12894 from Tishj/test_relation_api_order
    Merge pull request duckdb/duckdb#12895 from carlopi/no_ext_python_ci
    Merge pull request duckdb/duckdb#12891 from carlopi/fixup_linux_ci

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev2356"
+#define DUCKDB_PATCH_VERSION "1-dev2370"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev2356"
+#define DUCKDB_VERSION "v1.0.1-dev2370"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "65cda104b7"
+#define DUCKDB_SOURCE_ID "fd883da878"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
