23cb2f8c99e48236d855fe4c02f355451521c4d1
krlmlr <krlmlr@users.noreply.github.com>
2024-07-17 10:22:23 +0000

    chore: Update vendored sources to duckdb/duckdb@8809fdd6da39d6a99b79dd66685a009be2c31b5f
    Merge pull request duckdb/duckdb#12912 from Tishj/http_state_out_of_core

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev2622"
+#define DUCKDB_PATCH_VERSION "1-dev2630"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev2622"
+#define DUCKDB_VERSION "v1.0.1-dev2630"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "9b35c08d16"
+#define DUCKDB_SOURCE_ID "8809fdd6da"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
