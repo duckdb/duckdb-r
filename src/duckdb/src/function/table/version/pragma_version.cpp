61f7ca6b95a7ed3e0ac96b8bb9f5494bfb0225cd
krlmlr <krlmlr@users.noreply.github.com>
2024-07-17 12:23:34 +0000

    chore: Update vendored sources to duckdb/duckdb@eb6db5915175834587c299b1ec2c36e9449aacfa
    Merge pull request duckdb/duckdb#12970 from Mytherin/toplevelhivepartition
    Merge pull request duckdb/duckdb#12964 from Tishj/python_fetchnumpy_enum

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev2671"
+#define DUCKDB_PATCH_VERSION "1-dev2677"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev2671"
+#define DUCKDB_VERSION "v1.0.1-dev2677"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b3ffaa9293"
+#define DUCKDB_SOURCE_ID "eb6db59151"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
