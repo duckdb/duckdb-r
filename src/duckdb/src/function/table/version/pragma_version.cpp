4726acd1e6b5aa61ac5bc97f5b5df15c5bd8665e
krlmlr <krlmlr@users.noreply.github.com>
2024-07-18 01:15:38 +0000

    chore: Update vendored sources to duckdb/duckdb@7455c84aff8c15fe3900756d621b4312a13f63bc
    Merge pull request duckdb/duckdb#13002 from hawkfish/segtree-finalize

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev2961"
+#define DUCKDB_PATCH_VERSION "1-dev2968"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev2961"
+#define DUCKDB_VERSION "v1.0.1-dev2968"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "223004822a"
+#define DUCKDB_SOURCE_ID "7455c84aff"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
