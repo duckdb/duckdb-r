bed8948350429b9b02c2bdee79503ef76a11415d
krlmlr <krlmlr@users.noreply.github.com>
2024-07-18 13:38:21 +0000

    chore: Update vendored sources to duckdb/duckdb@c8fa71cd5ed2546c08f1ef5f2c4b0fb1420ddaff
    Merge pull request duckdb/duckdb#13052 from samansmink/make-client-reuse-threadsfe

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev3019"
+#define DUCKDB_PATCH_VERSION "1-dev3024"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev3019"
+#define DUCKDB_VERSION "v1.0.1-dev3024"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "766afc2141"
+#define DUCKDB_SOURCE_ID "c8fa71cd5e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
