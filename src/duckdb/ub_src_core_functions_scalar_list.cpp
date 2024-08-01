97d6dbc72afb5274d0c7bcd2d2fa997f6e74491d
krlmlr <krlmlr@users.noreply.github.com>
2024-08-01 23:56:53 +0000

    chore: Update vendored sources to duckdb/duckdb@b7cc3f85e98083d454e8b0aa9a87758242d7a668
    Merge pull request duckdb/duckdb#13163 from Maxxen/list-has-any-impl

--- src/duckdb/ub_src_core_functions_scalar_list.cpp
+++ src/duckdb/ub_src_core_functions_scalar_list.cpp
@@ -6,6 +6,8 @@
 
 #include "src/core_functions/scalar/list/list_filter.cpp"
 
+#include "src/core_functions/scalar/list/list_has_any.cpp"
+
 #include "src/core_functions/scalar/list/list_sort.cpp"
 
 #include "src/core_functions/scalar/list/list_distance.cpp"
