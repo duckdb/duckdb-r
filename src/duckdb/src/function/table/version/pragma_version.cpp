7e653253b8683b37d12b9b80427292a0e0e83575
krlmlr <krlmlr@users.noreply.github.com>
2024-08-01 19:18:18 +0000

    chore: Update vendored sources to duckdb/duckdb@43532074bb0ca5417eb479205d4bc82881520a75
    Merge pull request duckdb/duckdb#13108 from chrisiou/fix-regexfindall
    Merge pull request duckdb/duckdb#13124 from carlopi/update_threadsanitizer

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev3346"
+#define DUCKDB_PATCH_VERSION "1-dev3352"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev3346"
+#define DUCKDB_VERSION "v1.0.1-dev3352"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "465527ce3a"
+#define DUCKDB_SOURCE_ID "43532074bb"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
