0c20feb6f72283056a3ab3ee154114d2ff4e56ca
krlmlr <krlmlr@users.noreply.github.com>
2024-08-01 21:36:37 +0000

    chore: Update vendored sources to duckdb/duckdb@c76e485b5213b7aec3c429511c7157936029766f
    Merge pull request duckdb/duckdb#13151 from carlopi/fix_compilation
    Merge pull request duckdb/duckdb#13147 from Tishj/python_udf_struct_support_different_field_order

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev3399"
+#define DUCKDB_PATCH_VERSION "1-dev3404"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev3399"
+#define DUCKDB_VERSION "v1.0.1-dev3404"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b8db9345ac"
+#define DUCKDB_SOURCE_ID "c76e485b52"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
