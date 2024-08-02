0ea9e8af9746cc4feda6ee15d0e21e0de64b7a4f
Kirill Müller <kirill@cynkra.com>
2024-08-02 05:41:41 +0200

    chore: Update vendored sources to duckdb/duckdb@22040decf7ad0d87b449a87e6cb805978e34a5c1
    Merge pull request duckdb/duckdb#13109 from Tishj/render_tree_node_extra_info_with_rework
    Merge pull request duckdb/duckdb#13158 from carlopi/clang-tidy-diff

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev3515"
+#define DUCKDB_PATCH_VERSION "1-dev3542"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev3515"
+#define DUCKDB_VERSION "v1.0.1-dev3542"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "2c476e76ef"
+#define DUCKDB_SOURCE_ID "22040decf7"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
