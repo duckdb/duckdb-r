711c2290488c320431e88099ae31d3ea2b812b42
krlmlr <krlmlr@users.noreply.github.com>
2024-08-13 19:06:55 +0000

    chore: Update vendored sources to duckdb/duckdb@7bdbcd074dbddcd400a3c6c8a2270ed107451011
    Merge pull request duckdb/duckdb#13383 from Tmonster/fix_vector_sizes_nightly_failure
    Merge pull request duckdb/duckdb#13386 from Tishj/pycapsule_test

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev4130"
+#define DUCKDB_PATCH_VERSION "1-dev4138"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev4130"
+#define DUCKDB_VERSION "v1.0.1-dev4138"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e38204698c"
+#define DUCKDB_SOURCE_ID "7bdbcd074d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
