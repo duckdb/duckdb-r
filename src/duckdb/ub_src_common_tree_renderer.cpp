8a7561742fdb2f7fb933179f5583438e03ee0b40
krlmlr <krlmlr@users.noreply.github.com>
2024-08-15 18:37:41 +0000

    chore: Update vendored sources to duckdb/duckdb@ee3499ae1b3dd174fab3a510b6e8752cbfe3a464
    Merge pull request duckdb/duckdb#13202 from Tishj/html_tree_renderer

--- src/duckdb/ub_src_common_tree_renderer.cpp
+++ src/duckdb/ub_src_common_tree_renderer.cpp
@@ -1,6 +1,8 @@
 #include "src/common/tree_renderer/json_tree_renderer.cpp"
 
-#include "src/common/tree_renderer/text_tree_renderer.cpp"
+#include "src/common/tree_renderer/html_tree_renderer.cpp"
 
 #include "src/common/tree_renderer/graphviz_tree_renderer.cpp"
+
+#include "src/common/tree_renderer/text_tree_renderer.cpp"
 
