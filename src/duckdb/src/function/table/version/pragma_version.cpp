88a82a61d5cb3ed461e0951609749c5658f32e42
krlmlr <krlmlr@users.noreply.github.com>
2024-09-02 15:23:45 +0000

    chore: Update vendored sources to duckdb/duckdb@9ad037f3adfe372f17b5178a449ac4b6f9142240
    Merge pull request duckdb/duckdb#13494 from Alex-Monahan/scalar-macro-default-params

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev4470"
+#define DUCKDB_PATCH_VERSION "1-dev4474"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev4470"
+#define DUCKDB_VERSION "v1.0.1-dev4474"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1345b3013e"
+#define DUCKDB_SOURCE_ID "9ad037f3ad"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
