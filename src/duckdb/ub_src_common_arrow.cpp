0547d612f7156151ae41e57d6599148e53810f77
krlmlr <krlmlr@users.noreply.github.com>
2024-09-02 11:38:34 +0000

    chore: Update vendored sources to duckdb/duckdb@bb97c95a1ad2c277fcf2a60bb1a8af4b0f29b6c7
    Merge pull request duckdb/duckdb#13446 from pdet/arrow_extension_types

--- src/duckdb/ub_src_common_arrow.cpp
+++ src/duckdb/ub_src_common_arrow.cpp
@@ -2,13 +2,15 @@
 
 #include "src/common/arrow/arrow_converter.cpp"
 
+#include "src/common/arrow/arrow_merge_event.cpp"
+
+#include "src/common/arrow/arrow_query_result.cpp"
+
 #include "src/common/arrow/arrow_wrapper.cpp"
 
 #include "src/common/arrow/physical_arrow_collector.cpp"
 
 #include "src/common/arrow/physical_arrow_batch_collector.cpp"
 
-#include "src/common/arrow/arrow_merge_event.cpp"
-
-#include "src/common/arrow/arrow_query_result.cpp"
+#include "src/common/arrow/schema_metadata.cpp"
 
