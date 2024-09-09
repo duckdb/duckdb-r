5cc9ad78c7664db6153c3191dd02c4fda530ed0b
krlmlr <krlmlr@users.noreply.github.com>
2024-09-09 13:11:56 +0000

    chore: Update vendored sources to duckdb/duckdb@4b8274729b3037ce1c3528e90896aa3f6d94559b
    Merge pull request duckdb/duckdb#13654 from samansmink/improve-secret-deserialization-error

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev5060"
+#define DUCKDB_PATCH_VERSION "1-dev5063"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.0.1-dev5060"
+#define DUCKDB_VERSION "v1.0.1-dev5063"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "de5f77c08b"
+#define DUCKDB_SOURCE_ID "4b8274729b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
