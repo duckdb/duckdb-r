52f77ed1b2f3c818b2908a8d0b33ef69f03d0047
Kirill Müller <kirill@cynkra.com>
2024-09-24 11:20:38 +0200

    chore: Move stripping logic to `install.libs.R`

--- src/install.libs.R
+++ src/install.libs.R
@@ -2,6 +2,15 @@ files <- Sys.glob(paste0("*", SHLIB_EXT))
 dest <- file.path(R_PACKAGE_DIR, paste0('libs', R_ARCH))
 dir.create(dest, recursive = TRUE, showWarnings = FALSE)
 file.copy(files, dest, overwrite = TRUE)
+
+if (Sys.which("strip") != "" && Sys.info()["sysname"] == "Linux" && isTRUE(as.logical(Sys.getenv("_R_SHLIB_STRIP_"))) && Sys.getenv("R_STRIP_SHARED_LIB") != "") {
+  dest_files <- file.path(dest, files)
+  for (file in dest_files) {
+    message("Stripping: ", file)
+    system2(Sys.getenv("R_STRIP_SHARED_LIB"), args = file)
+  }
+}
+
 if (file.exists("symbols.rds")) {
   file.copy("symbols.rds", dest, overwrite = TRUE)
 }
