57a26164c4038d90659ce213192516fc999cbd54
krlmlr <krlmlr@users.noreply.github.com>
2024-09-25 06:47:28 +0000

    chore: Update vendored sources to duckdb/duckdb@329bb5393b686421b40261211354f4d77cac1633
    Merge pull request duckdb/duckdb#13826 from pdet/unquoted_error

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev9"
+#define DUCKDB_PATCH_VERSION "1-dev13"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.1-dev9"
+#define DUCKDB_VERSION "v1.1.1-dev13"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "403f0fc645"
+#define DUCKDB_SOURCE_ID "329bb5393b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
