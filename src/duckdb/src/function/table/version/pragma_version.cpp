cc5b64cf84942730835d39e2270bdaff1713edfe
krlmlr <krlmlr@users.noreply.github.com>
2024-09-25 12:08:02 +0000

    chore: Update vendored sources to duckdb/duckdb@1d7e05c9737821fdb2c8eba996642c9953de52f6
    Update description of 'max_temp_directory_size' (duckdb/duckdb#13724)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev71"
+#define DUCKDB_PATCH_VERSION "1-dev73"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.1-dev71"
+#define DUCKDB_VERSION "v1.1.1-dev73"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b383f36680"
+#define DUCKDB_SOURCE_ID "1d7e05c973"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
