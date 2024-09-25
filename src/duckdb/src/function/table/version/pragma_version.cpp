5a14d09f9fdbe818a817702143838e29adc1e8eb
krlmlr <krlmlr@users.noreply.github.com>
2024-09-25 08:36:54 +0000

    chore: Update vendored sources to duckdb/duckdb@6d9d429d5e7f464b69671b46dcbc99a6e46378df
    [Fix] C API - Correct type comparison in MAP value functions (duckdb/duckdb#13844)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev25"
+#define DUCKDB_PATCH_VERSION "1-dev29"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.1-dev25"
+#define DUCKDB_VERSION "v1.1.1-dev29"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d9e89b5cc1"
+#define DUCKDB_SOURCE_ID "6d9d429d5e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
