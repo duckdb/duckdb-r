957ad4722dba96ca7b5fdc1334c0e6734f1e9874
krlmlr <krlmlr@users.noreply.github.com>
2024-09-25 09:04:22 +0000

    chore: Update vendored sources to duckdb/duckdb@5fd2501220b80adaddf009b78cac44b97813258c
    [Dev] Minor cleanup to BufferManager and BlockHandle API (duckdb/duckdb#13838)
    [C-API Dev] Fix up `"Test DataChunk populate ListVector in C API"` test (duckdb/duckdb#13839)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev29"
+#define DUCKDB_PATCH_VERSION "1-dev38"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.1-dev29"
+#define DUCKDB_VERSION "v1.1.1-dev38"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "6d9d429d5e"
+#define DUCKDB_SOURCE_ID "5fd2501220"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
