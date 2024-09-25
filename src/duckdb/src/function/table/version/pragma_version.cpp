968f3a1190362b0f50fb5c06430beeb2de3446cb
krlmlr <krlmlr@users.noreply.github.com>
2024-09-25 14:03:09 +0000

    chore: Update vendored sources to duckdb/duckdb@8c2ee1eb7987a981cdf4bb1ed52683784a26e3bf
    Fix duckdb/duckdb#13880: correctly name http_proxy_password setting (duckdb/duckdb#13890)
    [Python] Fix issue related to the GIL when using `execute` with multiple statements (duckdb/duckdb#13893)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev85"
+#define DUCKDB_PATCH_VERSION "1-dev90"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.1-dev85"
+#define DUCKDB_VERSION "v1.1.1-dev90"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "081a748340"
+#define DUCKDB_SOURCE_ID "8c2ee1eb79"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
