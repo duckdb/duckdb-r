dc402aefe8a81f5e4e531a2ec1208cb008340b49
krlmlr <krlmlr@users.noreply.github.com>
2024-09-25 18:18:00 +0000

    chore: Update vendored sources to duckdb/duckdb@75d4bd0cc759dcb609ab349b87bff07dddf2ebb7
    Create a balanced union tree, also for export (duckdb/duckdb#13956)
    [Dev] Add exclusion for `pybind11` internal `_pybind11_conduit_v1_` method (duckdb/duckdb#13961)
    [Swift] Update README.md in Swift repo (duckdb/duckdb#13955)
    CI: Trigger actions for labeled discussions (duckdb/duckdb#13937)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev124"
+#define DUCKDB_PATCH_VERSION "1-dev132"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.1-dev124"
+#define DUCKDB_VERSION "v1.1.1-dev132"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c0f2946562"
+#define DUCKDB_SOURCE_ID "75d4bd0cc7"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
