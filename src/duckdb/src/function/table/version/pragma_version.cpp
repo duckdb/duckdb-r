a83113e7ddfc564393a28e07d6e8391804aaf22f
krlmlr <krlmlr@users.noreply.github.com>
2024-09-25 10:25:50 +0000

    chore: Update vendored sources to duckdb/duckdb@dfbfdef89aad145dc9d81c275bc2c9fad4062bed
    Fix for internal error when using rejects tables and adding implicit cast for boolean values. (duckdb/duckdb#13849)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev46"
+#define DUCKDB_PATCH_VERSION "1-dev51"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.1-dev46"
+#define DUCKDB_VERSION "v1.1.1-dev51"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c41ae2cb6e"
+#define DUCKDB_SOURCE_ID "dfbfdef89a"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
