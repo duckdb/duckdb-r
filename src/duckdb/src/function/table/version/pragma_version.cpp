5131cbe3d22f4b34aa4011ee41a5c2dd4bef782a
krlmlr <krlmlr@users.noreply.github.com>
2024-09-27 06:37:55 +0000

    chore: Update vendored sources to duckdb/duckdb@0ce863113043806780e776bcfb86b24afcb0263c
    [CSV Sniffer] Slight change of rules for dialect detection (duckdb/duckdb#14013)
    fix minor typos in comments of aggregate function tests (duckdb/duckdb#14007)
    Disable swift linux tests (duckdb/duckdb#14019)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev236"
+#define DUCKDB_PATCH_VERSION "1-dev243"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.1-dev236"
+#define DUCKDB_VERSION "v1.1.1-dev243"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f9e96b1910"
+#define DUCKDB_SOURCE_ID "0ce8631130"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
