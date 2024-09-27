4c58f41c0c96a0db5fd15c7eb933babd14ba6adf
krlmlr <krlmlr@users.noreply.github.com>
2024-09-27 05:42:14 +0000

    chore: Update vendored sources to duckdb/duckdb@2ff9c687e2c448914a28c59bd50f48f54e47de3c
    [Arrow] Only produce 'arrow.json' Extension types when `arrow_lossless_conversion` is enabled. (duckdb/duckdb#13989)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev220"
+#define DUCKDB_PATCH_VERSION "1-dev223"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.1-dev220"
+#define DUCKDB_VERSION "v1.1.1-dev223"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "dcc302aef4"
+#define DUCKDB_SOURCE_ID "2ff9c687e2"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
