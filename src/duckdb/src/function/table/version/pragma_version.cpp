f7cf51d041fad61d3c82491d3112efd6aa7e5a10
krlmlr <krlmlr@users.noreply.github.com>
2024-10-03 06:48:58 +0000

    chore: Update vendored sources to duckdb/duckdb@4ce455c84029195ffa4c3e540c10360ae8c73724
    [CSV Sniffer] Verify validity of header before value access (duckdb/duckdb#14040)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev271"
+#define DUCKDB_PATCH_VERSION "1-dev273"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.1-dev271"
+#define DUCKDB_VERSION "v1.1.1-dev273"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "03dd0df618"
+#define DUCKDB_SOURCE_ID "4ce455c840"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
