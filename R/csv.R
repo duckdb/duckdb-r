d6ebb1240c8a07de9bfcfa9f45f1f6f0f8ab543f
Kirill Müller <kirill@cynkra.com>
2024-10-04 17:53:31 +0200

    Reformat and comment

--- R/csv.R
+++ R/csv.R
@@ -31,8 +31,8 @@
 #' dbDisconnect(con)
 duckdb_read_csv <- function(conn, name, files, ..., header = TRUE, na.strings = "", nrow.check = 500,
                             delim = ",", quote = "\"", col.names = NULL, lower.case.names = FALSE, sep = delim, transaction = TRUE, temporary = FALSE) {
-  #
-  if (length(list(...))) warning("Arguments passed to ... are currently not used")
+  # FIXME: Warning as of duckdb 1.1.1, turn this into an error later
+  if (...length() > 0) warning("Arguments passed to ... are currently not used")
   if (length(na.strings) > 1) stop("na.strings must be of length 1")
   if (!missing(sep)) delim <- sep
 
