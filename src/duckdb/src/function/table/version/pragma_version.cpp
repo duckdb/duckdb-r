09e471645dd5209247334eb61d572656fc14f7a5
krlmlr <krlmlr@users.noreply.github.com>
2024-10-16 16:17:12 +0000

    chore: Update vendored sources to duckdb/duckdb@0e398d95c50ae40730467c53922c8fb8d5c69f90
    [Dev] Add the ExecutorException class, making use of the EXECUTOR ExceptionType (duckdb/duckdb#14231)
    Fix typos in code (duckdb/duckdb#14243)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev90"
+#define DUCKDB_PATCH_VERSION "2-dev94"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.2-dev90"
+#define DUCKDB_VERSION "v1.1.2-dev94"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1eac05ecd3"
+#define DUCKDB_SOURCE_ID "0e398d95c5"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
