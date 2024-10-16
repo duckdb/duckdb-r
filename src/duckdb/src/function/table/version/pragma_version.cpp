3a5ff746671623e4523aa3f555fcde661537694d
krlmlr <krlmlr@users.noreply.github.com>
2024-10-16 07:05:26 +0000

    chore: Update vendored sources to duckdb/duckdb@c87246586490b442706d0be66b82d71930a00578
    Fix: remove is_probe_in_domain (duckdb/duckdb#14084)
    fix maximum_threads test inside containers (duckdb/duckdb#14083)
    Fix test (duckdb/duckdb#14079)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev9"
+#define DUCKDB_PATCH_VERSION "2-dev15"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.2-dev9"
+#define DUCKDB_VERSION "v1.1.2-dev15"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "cd8cb3f1c8"
+#define DUCKDB_SOURCE_ID "c872465864"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
