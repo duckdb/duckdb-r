7c8ed04f21452c49ff00ed5ab5182f09a6f6d464
krlmlr <krlmlr@users.noreply.github.com>
2024-10-16 10:12:38 +0000

    chore: Update vendored sources to duckdb/duckdb@47e1d3d60b4d6d075cf88c2707572df12a630a3a
    fix parquet cardinality when first file is empty (duckdb/duckdb#14058)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev38"
+#define DUCKDB_PATCH_VERSION "2-dev40"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.2-dev38"
+#define DUCKDB_VERSION "v1.1.2-dev40"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "45559f5eeb"
+#define DUCKDB_SOURCE_ID "47e1d3d60b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
