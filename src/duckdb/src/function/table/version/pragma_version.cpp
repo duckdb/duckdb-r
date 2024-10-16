39e00386083a6834242b9eaaede15087ca3f9ecb
krlmlr <krlmlr@users.noreply.github.com>
2024-10-16 23:24:51 +0000

    chore: Update vendored sources to duckdb/duckdb@6dc2e9375870e60f82becb1cece4cc878289d3b8
    Add option to ignore GeoParquet, disable spatial autoloading when reading GeoParquet (duckdb/duckdb#14297)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev168"
+#define DUCKDB_PATCH_VERSION "2-dev176"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.2-dev168"
+#define DUCKDB_VERSION "v1.1.2-dev176"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "52b19d5ece"
+#define DUCKDB_SOURCE_ID "6dc2e93758"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
