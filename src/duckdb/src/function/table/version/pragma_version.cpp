c472390f18a718510d63795eaa92e675ffddcdf0
krlmlr <krlmlr@users.noreply.github.com>
2024-10-17 03:46:12 +0000

    chore: Update vendored sources to duckdb/duckdb@2c21eb1c2eec3a1e359d87fb2a2cd8e427dc03c1
    [Appender] Support appending to table with generated columns (duckdb/duckdb#14346)
    JSON reader - never generate maps if map_inference_threshold is -1 (duckdb/duckdb#14348)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev204"
+#define DUCKDB_PATCH_VERSION "2-dev208"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.2-dev204"
+#define DUCKDB_VERSION "v1.1.2-dev208"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "cc067e6b7d"
+#define DUCKDB_SOURCE_ID "2c21eb1c2e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
