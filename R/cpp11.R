716436a0120ef6bee18ed15b926a72b67b12e413
Kirill Müller <kirill@cynkra.com>
2024-10-26 21:07:41 +0200

    duckplyr compat

--- R/cpp11.R
+++ R/cpp11.R
@@ -173,7 +173,7 @@ rapi_rel_from_table_function <- function(con, function_name, positional_paramete
 }
 
 # allow_materialization = TRUE: compatibility with duckplyr <= 0.4.1
-rapi_rel_to_altrep <- function(rel, allow_materialization) {
+rapi_rel_to_altrep <- function(rel, allow_materialization = TRUE) {
   .Call(`_duckdb_rapi_rel_to_altrep`, rel, allow_materialization)
 }
 
