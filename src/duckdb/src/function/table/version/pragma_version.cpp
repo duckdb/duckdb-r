a1f1c9faa89cea9752c4e3022683bd5d118c6d11
krlmlr <krlmlr@users.noreply.github.com>
2024-10-31 16:40:22 +0000

    chore: Update vendored sources to duckdb/duckdb@217ec4722e949eaa49568bd707e49431ef727ab5
    [Adaptive Sniffer] In case files have only one row, be more permissive to detect headers and types. (duckdb/duckdb#14174)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2"
+#define DUCKDB_PATCH_VERSION "3-dev11"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.2"
+#define DUCKDB_VERSION "v1.1.3-dev11"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f680b7d08f"
+#define DUCKDB_SOURCE_ID "217ec4722e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
