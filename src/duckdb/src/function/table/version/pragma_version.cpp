4ddb209c23ed0aeee60a2ca8faac3fcc60136ac7
krlmlr <krlmlr@users.noreply.github.com>
2024-11-02 16:19:58 +0000

    chore: Update vendored sources to duckdb/duckdb@43d26298affa89bc6ca829a1defc4819b42b6fb4
    SingleFileBlockManager::MarkBlockAsUsed - also erase from newly_freed_list to ensure trim does not prune blocks that are in-use (duckdb/duckdb#14467)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev38"
+#define DUCKDB_PATCH_VERSION "3-dev40"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.3-dev38"
+#define DUCKDB_VERSION "v1.1.3-dev40"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "52b43b1660"
+#define DUCKDB_SOURCE_ID "43d26298af"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
