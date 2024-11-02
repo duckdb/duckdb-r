13480e917cc8702a2f165c3f927ac09e757eda7a
krlmlr <krlmlr@users.noreply.github.com>
2024-11-02 23:22:46 +0000

    chore: Update vendored sources to duckdb/duckdb@3293c92b6e657084318f7556b14077896b333109
    [bufferpool] Fix PurgeAgedBlocksInternal() evictions (duckdb/duckdb#14446)
    HTTPFS: HTTPException no longer inherits from IOException (duckdb/duckdb#14585)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev92"
+#define DUCKDB_PATCH_VERSION "3-dev97"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.3-dev92"
+#define DUCKDB_VERSION "v1.1.3-dev97"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "8664b710be"
+#define DUCKDB_SOURCE_ID "3293c92b6e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
