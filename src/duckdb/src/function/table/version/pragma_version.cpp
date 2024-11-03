c21d779643879749928a8e9352d752330ee856c9
krlmlr <krlmlr@users.noreply.github.com>
2024-11-03 10:58:50 +0000

    chore: Update vendored sources to duckdb/duckdb@c3ca3607c221d315f38227b8bf58e68746c59083
    Force error on CSV Sniffer Failure (duckdb/duckdb#14661)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev156"
+#define DUCKDB_PATCH_VERSION "3-dev158"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.3-dev156"
+#define DUCKDB_VERSION "v1.1.3-dev158"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "9cba6a2a03"
+#define DUCKDB_SOURCE_ID "c3ca3607c2"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
