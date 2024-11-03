dde02194aa018abe70c51eebd6cab25be1de3ffd
krlmlr <krlmlr@users.noreply.github.com>
2024-11-03 11:27:57 +0000

    chore: Update vendored sources to duckdb/duckdb@19864453f7d0ed095256d848b46e7b8630989bac
    fix scoping problem with function argument (duckdb/duckdb#14666)
    Bump extensions: AWS, Delta, Iceberg, INET (duckdb/duckdb#14669)
    bump vss + spatial (duckdb/duckdb#14667)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev158"
+#define DUCKDB_PATCH_VERSION "3-dev165"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.3-dev158"
+#define DUCKDB_VERSION "v1.1.3-dev165"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c3ca3607c2"
+#define DUCKDB_SOURCE_ID "19864453f7"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
