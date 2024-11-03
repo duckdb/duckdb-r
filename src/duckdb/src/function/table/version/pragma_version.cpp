565d74e41de943ce2313a8bc1a5888b8f0936e7b
krlmlr <krlmlr@users.noreply.github.com>
2024-11-03 10:33:34 +0000

    chore: Update vendored sources to duckdb/duckdb@9cba6a2a03e3fbca4364cab89d81a19ab50511b8
    Add serialization for bitstring_agg function (duckdb/duckdb#14654)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev154"
+#define DUCKDB_PATCH_VERSION "3-dev156"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.3-dev154"
+#define DUCKDB_VERSION "v1.1.3-dev156"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c6c08d4c1b"
+#define DUCKDB_SOURCE_ID "9cba6a2a03"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
