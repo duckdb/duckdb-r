6148e29cbd66f9db592198cba9733ca962c6c492
krlmlr <krlmlr@users.noreply.github.com>
2024-12-20 21:59:56 +0000

    vendor: Update vendored sources to duckdb/duckdb@ec0304403710ca7e87814a042d23058a5ad91f9f
    Fix duckdb/duckdb#14701 - avoid flattening in-place in ColumnData Append method (duckdb/duckdb#14708)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev1505"
+#define DUCKDB_PATCH_VERSION "4-dev1507"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev1505"
+#define DUCKDB_VERSION "v1.1.4-dev1507"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "fc413a0c26"
+#define DUCKDB_SOURCE_ID "ec03044037"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
