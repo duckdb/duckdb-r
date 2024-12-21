4dabed6965d9e3833d091dd4006c92134ebe1871
krlmlr <krlmlr@users.noreply.github.com>
2024-12-21 11:40:04 +0000

    vendor: Update vendored sources to duckdb/duckdb@ca8d6057ae5cf611a4d921cdb6d0f34097bd8094
    adaptive filters should not reorder filters that can throw (duckdb/duckdb#14672)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev1557"
+#define DUCKDB_PATCH_VERSION "4-dev1561"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev1557"
+#define DUCKDB_VERSION "v1.1.4-dev1561"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ab29daf310"
+#define DUCKDB_SOURCE_ID "ca8d6057ae"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
