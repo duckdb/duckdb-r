2bd8c6860b46bb8e23508aaa78f7c7635d61c50b
krlmlr <krlmlr@users.noreply.github.com>
2024-12-21 13:59:23 +0000

    vendor: Update vendored sources to duckdb/duckdb@3200280212febd461ee658e61c057229319cbc53
    Added list value getters duckdb_get_list_child and duckdb_get_list_size (duckdb/duckdb#14714)
    [PySpark] Add more functions such as ascii, asin, btrim, char, corr, ... and fix differences in ordering of null values between PySpark and DuckDB (duckdb/duckdb#14738)
    Revert "CMake: Avoid dependency-inducing codegeneration of extension headers" (duckdb/duckdb#14723)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev1619"
+#define DUCKDB_PATCH_VERSION "4-dev1645"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev1619"
+#define DUCKDB_VERSION "v1.1.4-dev1645"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "2d1b7d796d"
+#define DUCKDB_SOURCE_ID "3200280212"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
