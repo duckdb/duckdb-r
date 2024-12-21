1ae98411b8f4616c6dd453bb170aa5e1a2671521
krlmlr <krlmlr@users.noreply.github.com>
2024-12-21 14:46:11 +0000

    vendor: Update vendored sources to duckdb/duckdb@242d3b85ea9fc7fde6e96babd65e360e9c369c2f
    [Python] Use nullable dtypes in Pandas `DataFrame` creation when possible (duckdb/duckdb#14377)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev1649"
+#define DUCKDB_PATCH_VERSION "4-dev1662"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev1649"
+#define DUCKDB_VERSION "v1.1.4-dev1662"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f06cd12036"
+#define DUCKDB_SOURCE_ID "242d3b85ea"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
