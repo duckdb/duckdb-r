846d12cf8945de08f04d136179d9d267c8e77846
krlmlr <krlmlr@users.noreply.github.com>
2024-12-22 04:08:34 +0000

    vendor: Update vendored sources to duckdb/duckdb@810cfa4568ffb4b4019480287ab6a6e414b47cd7
    Output exception message on parse exception (duckdb/duckdb#14852)
    Increase map inference threshold (duckdb/duckdb#14848)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev1914"
+#define DUCKDB_PATCH_VERSION "4-dev1919"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev1914"
+#define DUCKDB_VERSION "v1.1.4-dev1919"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c92a86dd7e"
+#define DUCKDB_SOURCE_ID "810cfa4568"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
