5a52bf10eccd58122089d27646a3b9b74e8e12a1
krlmlr <krlmlr@users.noreply.github.com>
2024-12-24 17:30:09 +0000

    vendor: Update vendored sources to duckdb/duckdb@8e2c944aac9b39b0482569e1a2ada87e035c2d57
    Top-N: Improve performance with large heaps, and correctly call Reduce (duckdb/duckdb#14900)
    python: use PyUnicode_FromStringAndSize() (duckdb/duckdb#14895)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2061"
+#define DUCKDB_PATCH_VERSION "4-dev2075"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2061"
+#define DUCKDB_VERSION "v1.1.4-dev2075"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "36e3702a63"
+#define DUCKDB_SOURCE_ID "8e2c944aac"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
