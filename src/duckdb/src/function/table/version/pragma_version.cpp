c32abdd0e82875a991246a407474575f3204d718
krlmlr <krlmlr@users.noreply.github.com>
2024-12-27 03:08:32 +0000

    vendor: Update vendored sources to duckdb/duckdb@044b919545f70d3850d358795327dc31d6dd14de
    Fix getting named parameter type information. (duckdb/duckdb#14952)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2223"
+#define DUCKDB_PATCH_VERSION "4-dev2226"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2223"
+#define DUCKDB_VERSION "v1.1.4-dev2226"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "68a3ce9d4b"
+#define DUCKDB_SOURCE_ID "044b919545"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
