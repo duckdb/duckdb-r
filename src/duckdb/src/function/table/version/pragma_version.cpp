1181f06bb340ecc1c0225eba7a032e58c50f1ccc
Kirill Müller <kirill@cynkra.com>
2024-12-27 23:32:07 +0100

    vendor: Update vendored sources to duckdb/duckdb@1db3def66e16a51f94c90c5cb53d37f3ff910725
    Keep track of compression function in ColumnData, and add dedicated `select` call to compression function (duckdb/duckdb#15186)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3046"
+#define DUCKDB_PATCH_VERSION "4-dev3057"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3046"
+#define DUCKDB_VERSION "v1.1.4-dev3057"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "63a971e742"
+#define DUCKDB_SOURCE_ID "1db3def66e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
