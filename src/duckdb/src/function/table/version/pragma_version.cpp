772ef14ff56a5dc6732d002b8b6bc338e6aba47b
krlmlr <krlmlr@users.noreply.github.com>
2024-12-27 19:15:06 +0000

    vendor: Update vendored sources to duckdb/duckdb@f43fbd7527bfe34b879d3a07e2ce4b019a609e45
    Add virtual callback to get dependency manager to the catalog (duckdb/duckdb#15043)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2575"
+#define DUCKDB_PATCH_VERSION "4-dev2577"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2575"
+#define DUCKDB_VERSION "v1.1.4-dev2577"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "440bdb60b7"
+#define DUCKDB_SOURCE_ID "f43fbd7527"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
