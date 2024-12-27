f70907e503b064ef05badb6751f8c8ee7b75d93a
krlmlr <krlmlr@users.noreply.github.com>
2024-12-27 06:45:54 +0000

    vendor: Update vendored sources to duckdb/duckdb@aa2fe677d6b8ec4bb4b35d47b5a521cd8e1a76bf
    Properly register successful dialect runs (duckdb/duckdb#14977)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2270"
+#define DUCKDB_PATCH_VERSION "4-dev2273"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2270"
+#define DUCKDB_VERSION "v1.1.4-dev2273"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "47490b7f4f"
+#define DUCKDB_SOURCE_ID "aa2fe677d6"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
