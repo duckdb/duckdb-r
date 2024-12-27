12c61d02bd02e46d90cbbb819f95bc2646b5d1c2
krlmlr <krlmlr@users.noreply.github.com>
2024-12-27 12:55:19 +0000

    vendor: Update vendored sources to duckdb/duckdb@5d48575b112de7ad2759bad4b65bc86dd21185a2
    No salt for Android (duckdb/duckdb#14923)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2447"
+#define DUCKDB_PATCH_VERSION "4-dev2463"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2447"
+#define DUCKDB_VERSION "v1.1.4-dev2463"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1ef91baeae"
+#define DUCKDB_SOURCE_ID "5d48575b11"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
