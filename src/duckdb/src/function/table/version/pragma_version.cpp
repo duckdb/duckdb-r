5417b944c8a37d8c97a447092455ba3323ca081e
Kirill Müller <kirill@cynkra.com>
2024-12-27 23:46:18 +0100

    vendor: Update vendored sources to duckdb/duckdb@d9965a511aa15178af155a4ef8581198e067570b
    [Dev] Fix Roaring compression bug on appending small vectors (duckdb/duckdb#15326)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3570"
+#define DUCKDB_PATCH_VERSION "4-dev3573"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3570"
+#define DUCKDB_VERSION "v1.1.4-dev3573"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "5302c13337"
+#define DUCKDB_SOURCE_ID "d9965a511a"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
