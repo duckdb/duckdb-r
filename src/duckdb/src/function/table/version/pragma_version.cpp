43693b2348be1ff501ceceee463402c8832cc7a2
Kirill Müller <kirill@cynkra.com>
2024-12-27 12:44:12 +0100

    vendor: Update vendored sources to duckdb/duckdb@4672042ed535f5ab7e46e782cabe2d3eb49323dc
    Fix `BLOB` conversion in `parquet_metadata` (duckdb/duckdb#15132)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2840"
+#define DUCKDB_PATCH_VERSION "4-dev2843"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2840"
+#define DUCKDB_VERSION "v1.1.4-dev2843"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "59b237ad1c"
+#define DUCKDB_SOURCE_ID "4672042ed5"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
