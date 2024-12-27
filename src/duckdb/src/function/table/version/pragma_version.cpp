dd81bab153f2e187fb106f46117e998f12f33d42
krlmlr <krlmlr@users.noreply.github.com>
2024-12-27 13:48:47 +0000

    vendor: Update vendored sources to duckdb/duckdb@3be8f0ba9f8bca47c8a65bdd361bcadb45644e9c
    SUM(x + C) rewrite (duckdb/duckdb#15017)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2472"
+#define DUCKDB_PATCH_VERSION "4-dev2481"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2472"
+#define DUCKDB_VERSION "v1.1.4-dev2481"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a01f5d538f"
+#define DUCKDB_SOURCE_ID "3be8f0ba9f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
