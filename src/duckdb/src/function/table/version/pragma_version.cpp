50bd0e1bc1843a56e5f0c004b93b6a5b59a1fad0
Kirill Müller <kirill@cynkra.com>
2024-12-27 23:47:14 +0100

    vendor: Update vendored sources to duckdb/duckdb@a5d55eaf1506028b5dc9577e8a92bffc10a2ba3d
    Unified use of constant MainHeader: FLAG_COUNT (duckdb/duckdb#15338)
    Use correct element rename_list_el in grammar (duckdb/duckdb#15339)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3594"
+#define DUCKDB_PATCH_VERSION "4-dev3598"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3594"
+#define DUCKDB_VERSION "v1.1.4-dev3598"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f1ccf047d1"
+#define DUCKDB_SOURCE_ID "a5d55eaf15"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
