d1a75786311c57f6d4a0599c564461ee70e52aa6
Kirill Müller <kirill@cynkra.com>
2024-12-27 12:44:49 +0100

    vendor: Update vendored sources to duckdb/duckdb@edfa1f2efd36c8d8cf96663b25aa71b81592d16e
    Store transactions that have deletes on a table with indexes (duckdb/duckdb#15144)
    Execute test/sql/aggregate/aggregates/first_memory_usage.test_slow single threaded (duckdb/duckdb#15146)
    Issue duckdb/duckdb#15138: Friendlier ICU Settings (duckdb/duckdb#15139)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2845"
+#define DUCKDB_PATCH_VERSION "4-dev2851"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2845"
+#define DUCKDB_VERSION "v1.1.4-dev2851"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4a826fd17d"
+#define DUCKDB_SOURCE_ID "edfa1f2efd"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
