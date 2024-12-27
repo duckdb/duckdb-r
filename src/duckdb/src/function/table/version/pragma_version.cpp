0d6c47679e00b3c0936e4a203fd204daa3c74f92
krlmlr <krlmlr@users.noreply.github.com>
2024-12-27 02:34:54 +0000

    vendor: Update vendored sources to duckdb/duckdb@68a3ce9d4b5ab8f5033f9c7b27cb2cbc21d1b1ef
    Pullup empty results through delim joins as well (duckdb/duckdb#14920)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2218"
+#define DUCKDB_PATCH_VERSION "4-dev2223"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2218"
+#define DUCKDB_VERSION "v1.1.4-dev2223"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "38feda3af9"
+#define DUCKDB_SOURCE_ID "68a3ce9d4b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
