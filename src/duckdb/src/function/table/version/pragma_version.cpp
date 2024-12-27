8c526856294663d4302c8265301a352eec7282e6
Kirill Müller <kirill@cynkra.com>
2024-12-27 23:51:02 +0100

    vendor: Update vendored sources to duckdb/duckdb@7014b6aaf50b6ffd4f2e656d1a306475d2b21cb8
    Fix RelationStatisticsHelper to estimate table filters correctly (duckdb/duckdb#15308)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3691"
+#define DUCKDB_PATCH_VERSION "4-dev3695"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3691"
+#define DUCKDB_VERSION "v1.1.4-dev3695"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "3613371116"
+#define DUCKDB_SOURCE_ID "7014b6aaf5"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
