227282a66b5feb3980a2db71dac394b6ccca1871
krlmlr <krlmlr@users.noreply.github.com>
2024-12-27 07:13:34 +0000

    vendor: Update vendored sources to duckdb/duckdb@23f7e0dc487902aee9ba1f35249ff855b7d167d5
    Fuzzer duckdb/duckdb#3297: Nth Value Indexing (duckdb/duckdb#14997)
    Run containerized builds requiring deprecatd ACTIONS_ALLOW_USE_UNSECURE_NODE_VERSION only on main/feature (duckdb/duckdb#14998)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2273"
+#define DUCKDB_PATCH_VERSION "4-dev2280"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2273"
+#define DUCKDB_VERSION "v1.1.4-dev2280"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "aa2fe677d6"
+#define DUCKDB_SOURCE_ID "23f7e0dc48"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
