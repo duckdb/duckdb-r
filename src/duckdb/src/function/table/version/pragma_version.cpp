bba7e2c60e2dcac318caa6f49916247cb0cedb93
Kirill Müller <kirill@cynkra.com>
2024-12-27 23:43:47 +0100

    vendor: Update vendored sources to duckdb/duckdb@902331125660f5ead08a870cbd8534572d13536c
    Correctly propagate child-types in MAP to the internal struct values, and test httpfs in LATEST_STORAGE (duckdb/duckdb#15303)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3385"
+#define DUCKDB_PATCH_VERSION "4-dev3387"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3385"
+#define DUCKDB_VERSION "v1.1.4-dev3387"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a39967c3b6"
+#define DUCKDB_SOURCE_ID "9023311256"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
