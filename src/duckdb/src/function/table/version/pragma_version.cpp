06831d19bd0f32ff25f443519dbcdd15682bfb56
Kirill Müller <kirill@cynkra.com>
2024-12-27 12:37:56 +0100

    vendor: Update vendored sources to duckdb/duckdb@ea8ad42552c8055460ae11bf3d2a882f4e9e1394
    add duckdb_append_value to C API (duckdb/duckdb#15065)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2627"
+#define DUCKDB_PATCH_VERSION "4-dev2630"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2627"
+#define DUCKDB_VERSION "v1.1.4-dev2630"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "100c581e59"
+#define DUCKDB_SOURCE_ID "ea8ad42552"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
