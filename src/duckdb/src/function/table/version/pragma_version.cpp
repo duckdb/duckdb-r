bd74947a3477f755b6faf1a7b9482f2209984a3f
krlmlr <krlmlr@users.noreply.github.com>
2024-12-27 21:32:03 +0000

    vendor: Update vendored sources to duckdb/duckdb@cd09b6e41e6efb5dd16e1317070420dfebe3ee0c
    Interrupt query on error in `ClientContext::Query` (duckdb/duckdb#15058)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2601"
+#define DUCKDB_PATCH_VERSION "4-dev2608"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2601"
+#define DUCKDB_VERSION "v1.1.4-dev2608"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "547ffc95ff"
+#define DUCKDB_SOURCE_ID "cd09b6e41e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
