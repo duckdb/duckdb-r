7ad44cf098e14d1e760154fd37ec87ea24f7b8c7
krlmlr <krlmlr@users.noreply.github.com>
2024-12-27 17:43:01 +0000

    vendor: Update vendored sources to duckdb/duckdb@9e6fd53f3dcf41b554740ed8eb62c8e41e0fdc43
    Avoid sum rewrite for hugeint/uhugeint since it could introduce overflow errors (duckdb/duckdb#15040)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2567"
+#define DUCKDB_PATCH_VERSION "4-dev2569"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2567"
+#define DUCKDB_VERSION "v1.1.4-dev2569"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "2d3ff3d571"
+#define DUCKDB_SOURCE_ID "9e6fd53f3d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
