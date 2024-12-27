51844fbbf101364f9651be637e45d9badad21f4e
Kirill Müller <kirill@cynkra.com>
2024-12-27 12:41:08 +0100

    vendor: Update vendored sources to duckdb/duckdb@f41f5fa26b05e9f705bb70fb5f7430f283a27f0f
    Internal duckdb/duckdb#861: Value Function SubFrames (duckdb/duckdb#15100)
    fix bundle-library step (duckdb/duckdb#15093)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2762"
+#define DUCKDB_PATCH_VERSION "4-dev2769"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2762"
+#define DUCKDB_VERSION "v1.1.4-dev2769"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1004fd9cfe"
+#define DUCKDB_SOURCE_ID "f41f5fa26b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
