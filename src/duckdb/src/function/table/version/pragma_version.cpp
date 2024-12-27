e1ed08e36f05190325e5d09adbb08e6f4450fadd
krlmlr <krlmlr@users.noreply.github.com>
2024-12-27 01:08:54 +0000

    vendor: Update vendored sources to duckdb/duckdb@1a3d614f0eec5a2198af8ba4ea06eb9adee9d5f8
    improve ReadAheadBuffer::AddReadHead error message (duckdb/duckdb#14940)
    Fix several CLI issues (duckdb/duckdb#14929)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2200"
+#define DUCKDB_PATCH_VERSION "4-dev2210"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2200"
+#define DUCKDB_VERSION "v1.1.4-dev2210"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "742e86dce2"
+#define DUCKDB_SOURCE_ID "1a3d614f0e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
