cd4ab64e76d1b7671cafef64d683559b34a5b434
Kirill Müller <kirill@cynkra.com>
2024-12-27 12:45:36 +0100

    vendor: Update vendored sources to duckdb/duckdb@858da0bf846a64b7adb81c337c346685b5ca3857
    FileHandle should retain the FileOpenFlags it was opened with (duckdb/duckdb#15153)
    Refactor signing linux extensions (duckdb/duckdb#15159)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2885"
+#define DUCKDB_PATCH_VERSION "4-dev2894"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2885"
+#define DUCKDB_VERSION "v1.1.4-dev2894"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "77c9f1f6ac"
+#define DUCKDB_SOURCE_ID "858da0bf84"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
