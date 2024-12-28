dfa33524bcb6e4892b5ecf4fcccd5b51c38eddd6
krlmlr <krlmlr@users.noreply.github.com>
2024-12-28 13:22:15 +0000

    vendor: Update vendored sources to duckdb/duckdb@41c78b1fb0881af9c12d5fbd30ef0385c947f128
    Operator's GetProgress to return ProgressData instead of double (duckdb/duckdb#15084)
    Linux dockerized extensions: invoke script in right folder (duckdb/duckdb#15122)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2788"
+#define DUCKDB_PATCH_VERSION "4-dev2808"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2788"
+#define DUCKDB_VERSION "v1.1.4-dev2808"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "75c349e47f"
+#define DUCKDB_SOURCE_ID "41c78b1fb0"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
