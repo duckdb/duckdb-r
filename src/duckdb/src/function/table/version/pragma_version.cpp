e739512a131f5c0c65aaab09d073262dcf5c1f53
krlmlr <krlmlr@users.noreply.github.com>
2024-12-28 06:09:41 +0000

    vendor: Update vendored sources to duckdb/duckdb@405ff1f66f60b2e87bcdc5f1ce4e302d2e9d60b2
    Ensure checkpoint tasks complete on IO exceptions (duckdb/duckdb#15089)
    new answers for some JOIN benchmarks (duckdb/duckdb#15090)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2677"
+#define DUCKDB_PATCH_VERSION "4-dev2681"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2677"
+#define DUCKDB_VERSION "v1.1.4-dev2681"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "219dc27cfc"
+#define DUCKDB_SOURCE_ID "405ff1f66f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
