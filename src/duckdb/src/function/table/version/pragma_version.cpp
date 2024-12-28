5b86bdcd6e3dae5a4341ebd32dfe059b9af63558
krlmlr <krlmlr@users.noreply.github.com>
2024-12-28 19:51:31 +0000

    vendor: Update vendored sources to duckdb/duckdb@aa3787b0c15ece12e071200e7a82a186d3d712f0
    [Compression] Add RoaringBitmap Compression (duckdb/duckdb#14878)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2924"
+#define DUCKDB_PATCH_VERSION "4-dev3037"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2924"
+#define DUCKDB_VERSION "v1.1.4-dev3037"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "974954d89e"
+#define DUCKDB_SOURCE_ID "aa3787b0c1"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
