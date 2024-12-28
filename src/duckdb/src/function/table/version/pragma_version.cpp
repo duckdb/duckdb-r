895c44432a1e662a9f29e95d71a251d89e16ff4b
krlmlr <krlmlr@users.noreply.github.com>
2024-12-28 18:11:44 +0000

    vendor: Update vendored sources to duckdb/duckdb@87021421a54e9100e5ffeaa3ca142cdd01561766
    Fix wrong type append of base vector (duckdb/duckdb#15157)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev2897"
+#define DUCKDB_PATCH_VERSION "4-dev2899"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev2897"
+#define DUCKDB_VERSION "v1.1.4-dev2899"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "46a13e5ccb"
+#define DUCKDB_SOURCE_ID "87021421a5"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
