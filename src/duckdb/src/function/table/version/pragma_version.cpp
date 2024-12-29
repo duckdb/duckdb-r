e136ce11fd11d4355168124dbba1d9a20a8660ef
krlmlr <krlmlr@users.noreply.github.com>
2024-12-29 22:03:12 +0000

    vendor: Update vendored sources to duckdb/duckdb@7e3dca3b06ab9cc99cf205fa1ae8003841631f81
    Bump Extension C API to stable (duckdb/duckdb#14992)
    Minor nightly test fixes (duckdb/duckdb#15318)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3447"
+#define DUCKDB_PATCH_VERSION "4-dev3472"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3447"
+#define DUCKDB_VERSION "v1.1.4-dev3472"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4f3a7d585d"
+#define DUCKDB_SOURCE_ID "7e3dca3b06"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
