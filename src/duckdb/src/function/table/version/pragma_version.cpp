adb4e4ca19b643d9c32ccf0723409db99958023d
krlmlr <krlmlr@users.noreply.github.com>
2024-12-30 13:01:22 +0000

    vendor: Update vendored sources to duckdb/duckdb@a04385ab02effd78ba3479b176f6f6faa01d6bc0
    [Fix] Track correct allocation size of evicted memory (duckdb/duckdb#15433)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3756"
+#define DUCKDB_PATCH_VERSION "4-dev3759"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3756"
+#define DUCKDB_VERSION "v1.1.4-dev3759"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "14d0a4ca86"
+#define DUCKDB_SOURCE_ID "a04385ab02"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
