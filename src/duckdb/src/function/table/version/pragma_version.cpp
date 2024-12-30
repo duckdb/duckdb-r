febeee5db6adb3d0d0ad06bf4aa2726d2838e52c
krlmlr <krlmlr@users.noreply.github.com>
2024-12-30 07:43:24 +0000

    vendor: Update vendored sources to duckdb/duckdb@3613371116ce1f43eeed6844aa3bc1bb556cb188
    AFL++ Fuzzer Tests and Fixes (duckdb/duckdb#15329)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3684"
+#define DUCKDB_PATCH_VERSION "4-dev3691"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3684"
+#define DUCKDB_VERSION "v1.1.4-dev3691"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "7db48386fd"
+#define DUCKDB_SOURCE_ID "3613371116"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
