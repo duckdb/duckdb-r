8d09d64d5396baad8af965b59d2eb5c5e064cb2c
krlmlr <krlmlr@users.noreply.github.com>
2024-12-30 19:12:20 +0000

    vendor: Update vendored sources to duckdb/duckdb@ec7be6f00e0dd7fa02d272f126c32aedbff13bb3
    C API header generation for Go bindings (duckdb/duckdb#14944)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3826"
+#define DUCKDB_PATCH_VERSION "4-dev3836"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3826"
+#define DUCKDB_VERSION "v1.1.4-dev3836"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "eecb1b42dc"
+#define DUCKDB_SOURCE_ID "ec7be6f00e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
