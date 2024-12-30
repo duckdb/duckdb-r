d181e447a29ab642c99aa9be7bb7519ec4659a2d
krlmlr <krlmlr@users.noreply.github.com>
2024-12-30 10:42:22 +0000

    vendor: Update vendored sources to duckdb/duckdb@70d238a7a922d0321564a5acfc3747782c42c4a4
    allow multifilereaders to delete entire chunks in FinalizeChunk (duckdb/duckdb#15401)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3733"
+#define DUCKDB_PATCH_VERSION "4-dev3736"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3733"
+#define DUCKDB_VERSION "v1.1.4-dev3736"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "dd3f65170b"
+#define DUCKDB_SOURCE_ID "70d238a7a9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
