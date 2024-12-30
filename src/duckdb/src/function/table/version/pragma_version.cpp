1fd069c0ccf6928dc18a6be011c25656c62fb2e0
krlmlr <krlmlr@users.noreply.github.com>
2024-12-30 04:32:13 +0000

    vendor: Update vendored sources to duckdb/duckdb@815f2f58e5c8c5ed8d380b378219f13e3cb604de
    [Dev] Made `reference<CompressionFunction> function` private in `ColumnSegment` (duckdb/duckdb#15347)
    [Python] Allow use of `DuckDBPyType` as child objects in implicit conversions (duckdb/duckdb#15346)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3654"
+#define DUCKDB_PATCH_VERSION "4-dev3659"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3654"
+#define DUCKDB_VERSION "v1.1.4-dev3659"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "48c9f5e796"
+#define DUCKDB_SOURCE_ID "815f2f58e5"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
