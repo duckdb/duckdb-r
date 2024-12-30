75bca9bf3e59ba2cdb8257a2fac759ba15e567b6
krlmlr <krlmlr@users.noreply.github.com>
2024-12-30 02:39:41 +0000

    vendor: Update vendored sources to duckdb/duckdb@79f36e0cffe4e3a803a6a31388c375fc538a718a
    feat: support create_on_conflict in create_table_relation (duckdb/duckdb#15245)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3627"
+#define DUCKDB_PATCH_VERSION "4-dev3631"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3627"
+#define DUCKDB_VERSION "v1.1.4-dev3631"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "373d3370c7"
+#define DUCKDB_SOURCE_ID "79f36e0cff"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
