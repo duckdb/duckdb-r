62d2ba7b51560b9fbd09d72801ccf2b534470653
krlmlr <krlmlr@users.noreply.github.com>
2025-01-03 09:21:32 +0000

    vendor: Update vendored sources to duckdb/duckdb@8869b59d17a0b0a40544f0ef9a8e129a51c6bcc1
    remove duplicate FastMem copies in binary (duckdb/duckdb#15470)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3939"
+#define DUCKDB_PATCH_VERSION "4-dev3941"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3939"
+#define DUCKDB_VERSION "v1.1.4-dev3941"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "61d1e92f27"
+#define DUCKDB_SOURCE_ID "8869b59d17"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
