222f3ccd4baeb593db5fa580473e4cef5b103127
krlmlr <krlmlr@users.noreply.github.com>
2025-01-06 15:04:28 +0000

    vendor: Update vendored sources to duckdb/duckdb@a0a828b712f538a64263dad251d20a5f91f83a80
    Make `max_temp_directory_size` round-trip (duckdb/duckdb#15549)
    Fix answers for benchmarks containing `random()` function (duckdb/duckdb#15562)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3968"
+#define DUCKDB_PATCH_VERSION "4-dev3976"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3968"
+#define DUCKDB_VERSION "v1.1.4-dev3976"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "2082b55f89"
+#define DUCKDB_SOURCE_ID "a0a828b712"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
