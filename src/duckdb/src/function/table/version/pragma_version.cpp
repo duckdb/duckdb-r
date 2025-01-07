f9dad940dd8d0ab712b6c80ee194a772910937f2
krlmlr <krlmlr@users.noreply.github.com>
2025-01-07 09:54:44 +0000

    vendor: Update vendored sources to duckdb/duckdb@a3636bdd6203f4e4c47c5191264b9209e4f2a516
    Asof join adds rows in specific case (duckdb/duckdb#15567)
    Avoid fast fail: change defaults to run all tests in more cases (duckdb/duckdb#15558)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev3995"
+#define DUCKDB_PATCH_VERSION "4-dev4003"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev3995"
+#define DUCKDB_VERSION "v1.1.4-dev4003"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "50de06cec8"
+#define DUCKDB_SOURCE_ID "a3636bdd62"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
