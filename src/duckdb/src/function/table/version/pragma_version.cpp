e61633f0e98055653c12d5f2b681aed35b37eea3
krlmlr <krlmlr@users.noreply.github.com>
2025-01-07 21:02:35 +0000

    vendor: Update vendored sources to duckdb/duckdb@2edfde3071cdc3ccc6047773e8229fb80238443d
    DuckFuzz Fix on Null parameters for both read_csv and sniff_csv (duckdb/duckdb#15565)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev4102"
+#define DUCKDB_PATCH_VERSION "4-dev4106"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev4102"
+#define DUCKDB_VERSION "v1.1.4-dev4106"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "3c0db29b2b"
+#define DUCKDB_SOURCE_ID "2edfde3071"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
