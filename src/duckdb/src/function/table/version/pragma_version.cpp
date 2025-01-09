a962e0413986bf2c22ff96574ce55481041172e1
krlmlr <krlmlr@users.noreply.github.com>
2025-01-09 11:45:06 +0000

    vendor: Update vendored sources to duckdb/duckdb@b48222edb439e42517392eaa2e918b625cafd09a
    Adjust list_reduce to use a 1-based indexing (duckdb/duckdb#15614)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev4202"
+#define DUCKDB_PATCH_VERSION "4-dev4204"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev4202"
+#define DUCKDB_VERSION "v1.1.4-dev4204"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c93ab6e817"
+#define DUCKDB_SOURCE_ID "b48222edb4"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
