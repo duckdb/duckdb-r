003381da5ab2837445964c6f138362dd103c6569
Kirill Müller <krlmlr@users.noreply.github.com>
2025-01-20 20:50:58 +0100

    chore: Move internal `duckdb.tar` dependency (#1013)
    * chore: Avoid building `symbols.rds` for speed
    
    * Move duckdb.tar dependency
    
    * Revert "chore: Avoid building `symbols.rds` for speed"
    
    This reverts commit 8f095335c9d415a923516ea4f2449928856baf3c.

--- src/include/from-tar.mk
+++ src/include/from-tar.mk
@@ -2,6 +2,7 @@
 # to an existing file.
 # We print details on the object files already extracted in the configure script
 # for diagnostic purposes.
+all: duckdb.tar
 duckdb.tar: $(DUCKDB_R_PREBUILT_ARCHIVE)
 	ls -lR
 	# Ensure the file is recreated in the next run
