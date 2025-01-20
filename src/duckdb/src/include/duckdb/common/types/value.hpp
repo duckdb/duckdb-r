1710522bbcf1ba6d52335942ffe151a29a9069c5
Kirill Müller <kirill@cynkra.com>
2025-01-20 22:44:01 +0100

    0

--- src/duckdb/src/include/duckdb/common/types/value.hpp
+++ src/duckdb/src/include/duckdb/common/types/value.hpp
@@ -159,11 +159,11 @@ public:
 	//! Create a struct value with given list of entries
 	DUCKDB_API static Value STRUCT(child_list_t<Value> values);
 	DUCKDB_API static Value STRUCT(const LogicalType &type, vector<Value> struct_values);
-	//! Create a list value with the given entries, list type is inferred from children
-	//! Cannot be called with an empty list, use either EMPTYLIST or LIST with a type instead
-	DUCKDB_API static Value LIST(vector<Value> values);
 	//! Create a list value with the given entries
 	DUCKDB_API static Value LIST(const LogicalType &child_type, vector<Value> values);
+	//! Create a list value with the given entries
+	//! The type of the first value determines the list type. The list cannot be empty.
+	DUCKDB_API static Value LIST(vector<Value> values);
 	//! Create an empty list with the specified child-type
 	DUCKDB_API static Value EMPTYLIST(const LogicalType &child_type);
 	//! Create an array value with the given entries. Array type is inferred from children
