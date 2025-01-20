721d611aec8d872da77bac44006b9773170a7aa3
Kirill Müller <kirill@cynkra.com>
2025-01-20 22:52:48 +0100

    24

--- src/utils.cpp
+++ src/utils.cpp
@@ -234,10 +234,7 @@ Value RApiTypes::SexpToValue(SEXP valsexp, R_len_t idx, bool typed_logical_null)
 			auto value = SexpToValue(ts_val, child_idx);
 			child_values.push_back(value);
 		}
-		if (child_values.empty()) {
-			return Value::EMPTYLIST(RApiTypes::LogicalTypeFromRType(child_rtype, true));
-		}
-		return Value::LIST(std::move(child_values));
+		return Value::LIST(RApiTypes::LogicalTypeFromRType(child_rtype, true), std::move(child_values));
 	}
 	case RTypeId::STRUCT: {
 		child_list_t<Value> child_values;
