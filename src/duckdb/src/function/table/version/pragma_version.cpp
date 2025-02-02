6887fa14bef3505fb9f433d0df99826f4b083ec4
Kirill Müller <kirill@cynkra.com>
2025-02-02 16:07:55 +0100

    vendor: Update vendored sources to duckdb/duckdb@09cef57892e5dc221f7eee7e6c4ab440d2ab4078
    Fix duckdb/duckdb#15659: VARCHAR parameters now count as STRING_LITERAL again (duckdb/duckdb#15724)
    Remove httpfs patch (duckdb/duckdb#15729)
    Add NATIVE_ARCH option to compile using -march=native, and in the CLI time queries that are send through "-c" (duckdb/duckdb#15726)
    [Testing] Temporarily skip tests (duckdb/duckdb#15727)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev4634"
+#define DUCKDB_PATCH_VERSION "4-dev4648"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev4634"
+#define DUCKDB_VERSION "v1.1.4-dev4648"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "677a8e7cd6"
+#define DUCKDB_SOURCE_ID "09cef57892"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
