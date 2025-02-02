ba81570a15888151a259c6d091664dc27f5bb249
Kirill Müller <kirill@cynkra.com>
2025-02-02 16:06:28 +0100

    vendor: Update vendored sources to duckdb/duckdb@945a96cd3fffc49b1522342f710b9b133f77107b
    [Fix] Make next_batch_index atomic (duckdb/duckdb#15699)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev4538"
+#define DUCKDB_PATCH_VERSION "4-dev4541"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev4538"
+#define DUCKDB_VERSION "v1.1.4-dev4541"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "2a1d5e2ab8"
+#define DUCKDB_SOURCE_ID "945a96cd3f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
