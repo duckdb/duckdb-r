0510c55a2e11786118ada26e5e446fd1998e7439
Kirill Müller <kirill@cynkra.com>
2025-02-02 12:13:52 +0100

    vendor: Update vendored sources to duckdb/duckdb@20ae352efcff50351d479e4677b2b8757810d532
    Fix sign-compare compilation warning (duckdb/duckdb#15672)
    Fix Python dictionary key is repeated (duckdb/duckdb#15663)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev4492"
+#define DUCKDB_PATCH_VERSION "4-dev4496"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev4492"
+#define DUCKDB_VERSION "v1.1.4-dev4496"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "89bcc3e2ce"
+#define DUCKDB_SOURCE_ID "20ae352efc"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
