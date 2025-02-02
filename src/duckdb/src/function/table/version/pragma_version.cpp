e630ccb29764e33343b831ec3ca138fc8e5bea07
Kirill Müller <kirill@cynkra.com>
2025-02-02 12:13:41 +0100

    vendor: Update vendored sources to duckdb/duckdb@89bcc3e2ce739b1b470afa79818ee03c8cf96fe8
    [Dev] Fix wrong result reported by Roaring Compression `FinalAnalyze` (duckdb/duckdb#15677)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev4489"
+#define DUCKDB_PATCH_VERSION "4-dev4492"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev4489"
+#define DUCKDB_VERSION "v1.1.4-dev4492"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "31cb6c5582"
+#define DUCKDB_SOURCE_ID "89bcc3e2ce"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
