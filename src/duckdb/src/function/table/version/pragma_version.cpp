c3f2826a7a33cf16bb3ac3649d0b5c8d187e80c0
Kirill Müller <kirill@cynkra.com>
2025-02-02 16:08:17 +0100

    vendor: Update vendored sources to duckdb/duckdb@0024e5d4beba0185733df68642775e3f38e089cb
    Move changes in v1.2 to main (duckdb/duckdb#15744)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev4655"
+#define DUCKDB_PATCH_VERSION "4-dev4679"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev4655"
+#define DUCKDB_VERSION "v1.1.4-dev4679"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "96d58253c4"
+#define DUCKDB_SOURCE_ID "0024e5d4be"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
