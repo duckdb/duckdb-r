362de0a7f2c4805cf17b082b3d7528f587ce3eba
Kirill Müller <kirill@cynkra.com>
2025-02-02 16:08:06 +0100

    vendor: Update vendored sources to duckdb/duckdb@a597e41cb76fd31ed7040abeffa5ee407d1a0556
    Scan validity from dictionary vectors directly, and skip scanning validity when we encounter a dictionary vector (duckdb/duckdb#15737)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev4651"
+#define DUCKDB_PATCH_VERSION "4-dev4653"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 1
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.1.4-dev4651"
+#define DUCKDB_VERSION "v1.1.4-dev4653"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e00ba6ae90"
+#define DUCKDB_SOURCE_ID "a597e41cb7"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
