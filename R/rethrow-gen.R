549708f5d324a986a1a8876ae4a5f96e8ac6f86a
Kirill Müller <kirill@cynkra.com>
2025-02-21 21:28:19 +0100

    chore: Prepare removal of `allow_materialization`

--- R/rethrow-gen.R
+++ R/rethrow-gen.R
@@ -423,9 +423,9 @@ rethrow_rapi_rel_insert <- function(rel, schema_name, table_name, call = parent.
   )
 }
 
-rethrow_rapi_rel_to_altrep <- function(rel, allow_materialization, n_rows, n_cells, call = parent.frame(2)) {
+rethrow_rapi_rel_to_altrep <- function(rel, n_rows, n_cells, call = parent.frame(2)) {
   rlang::try_fetch(
-    rapi_rel_to_altrep(rel, allow_materialization, n_rows, n_cells),
+    rapi_rel_to_altrep(rel, n_rows, n_cells),
     error = function(e) {
       rethrow_error_from_rapi(e, call)
     }
