089b721f7b67ea4c4a4d1e61ae7eb268c024ddcf
Antonov548 <antonov5551998@gmail.com>
2025-02-26 16:03:30 +0100

    vendor: Update vendored sources to duckdb/duckdb@0e0b35aebc507d7fd7edb9f2c0f2d2c7225fbc62
    Parquet Reader Cleanup: Move ColumnReaders to separate files (duckdb/duckdb#16092)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev63"
+#define DUCKDB_PATCH_VERSION "1-dev73"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev63"
+#define DUCKDB_VERSION "v1.2.1-dev73"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a35a3fba95"
+#define DUCKDB_SOURCE_ID "0e0b35aebc"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
