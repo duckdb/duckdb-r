317db6e14249f6bb71d00fb685f36c3d1ed72311
Antonov548 <antonov5551998@gmail.com>
2025-02-26 17:15:42 +0100

    vendor: Update vendored sources to duckdb/duckdb@d3de141aae35e03bd9b7a1073c16c99ee657ce4f
    Parquet Reader: Move decoding logic into separate Decoder classes (duckdb/duckdb#16100)

---
+++ src/duckdb/ub_extension_parquet_decoder.cpp
@@ -0,0 +1,10 @@
+#include "extension/parquet/decoder/byte_stream_split_decoder.cpp"
+
+#include "extension/parquet/decoder/delta_binary_packed_decoder.cpp"
+
+#include "extension/parquet/decoder/delta_byte_array_decoder.cpp"
+
+#include "extension/parquet/decoder/dictionary_decoder.cpp"
+
+#include "extension/parquet/decoder/rle_decoder.cpp"
+
