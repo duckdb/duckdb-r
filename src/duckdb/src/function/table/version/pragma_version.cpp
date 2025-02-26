47b03b21acaa3b3821d5b98b308da2bf95520ea9
Antonov548 <antonov5551998@gmail.com>
2025-02-26 16:03:17 +0100

    vendor: Update vendored sources to duckdb/duckdb@f7f11baa24fceae00cbe8438dc85b00c7676af2a
    Sniff Timestamp_TZ from CSV FIles (duckdb/duckdb#15730)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev51"
+#define DUCKDB_PATCH_VERSION "1-dev57"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev51"
+#define DUCKDB_VERSION "v1.2.1-dev57"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "670e905d11"
+#define DUCKDB_SOURCE_ID "f7f11baa24"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
