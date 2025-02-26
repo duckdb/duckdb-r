e8035c6eeff9338afb42563eff82dcae87344739
Antonov548 <antonov5551998@gmail.com>
2025-02-26 16:03:30 +0100

    vendor: Update vendored sources to duckdb/duckdb@0e0b35aebc507d7fd7edb9f2c0f2d2c7225fbc62
    Parquet Reader Cleanup: Move ColumnReaders to separate files (duckdb/duckdb#16092)

---
+++ src/duckdb/ub_extension_parquet_reader.cpp
@@ -0,0 +1,14 @@
+#include "extension/parquet/reader/cast_column_reader.cpp"
+
+#include "extension/parquet/reader/decimal_column_reader.cpp"
+
+#include "extension/parquet/reader/expression_column_reader.cpp"
+
+#include "extension/parquet/reader/list_column_reader.cpp"
+
+#include "extension/parquet/reader/row_number_column_reader.cpp"
+
+#include "extension/parquet/reader/string_column_reader.cpp"
+
+#include "extension/parquet/reader/struct_column_reader.cpp"
+
