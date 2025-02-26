7b9d9feaf4855a0ae697cb8132b0f9200183d509
Antonov548 <antonov5551998@gmail.com>
2025-02-26 11:32:44 +0100

    vendor: Update vendored sources to duckdb/duckdb@ea36eda8fbb11a77a013f4f393ac05e98637b659
    unittests: clear test directory after every test (duckdb/duckdb#16053)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev2"
+#define DUCKDB_PATCH_VERSION "1-dev5"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev2"
+#define DUCKDB_VERSION "v1.2.1-dev5"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1b8c9023d0"
+#define DUCKDB_SOURCE_ID "ea36eda8fb"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
