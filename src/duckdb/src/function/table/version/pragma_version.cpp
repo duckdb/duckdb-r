88c693ecbfa1885694d09532da47c05e44103f76
Antonov548 <antonov5551998@gmail.com>
2025-02-26 16:03:22 +0100

    vendor: Update vendored sources to duckdb/duckdb@9b8f3eb7a3e851e04126875404458e25ca35a254
    [no-op] Add documentation for filesystem read behavior (duckdb/duckdb#15937)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev57"
+#define DUCKDB_PATCH_VERSION "1-dev59"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev57"
+#define DUCKDB_VERSION "v1.2.1-dev59"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f7f11baa24"
+#define DUCKDB_SOURCE_ID "9b8f3eb7a3"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
