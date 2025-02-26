02134066a33147e91f1c3a5262309c1d57611f11
Antonov548 <antonov5551998@gmail.com>
2025-02-26 11:32:55 +0100

    vendor: Update vendored sources to duckdb/duckdb@22727b3b026715ce3d25b51f673ada23fdf0f336
    Display schema information of currently selected database only (duckdb/duckdb#15815)
    Remove extension definitions to prevent re-compilation of the entire system on commit (duckdb/duckdb#15955)
    Throw an error when unsupported commands are used in concurrentloop (duckdb/duckdb#16009)
    Benchmark runner: catch and log errors + add support for `retry load N` syntax (duckdb/duckdb#16054)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev5"
+#define DUCKDB_PATCH_VERSION "1-dev16"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev5"
+#define DUCKDB_VERSION "v1.2.1-dev16"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ea36eda8fb"
+#define DUCKDB_SOURCE_ID "22727b3b02"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
