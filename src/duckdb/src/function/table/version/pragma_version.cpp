71e0227f4a75051df7575e9164d64659340b25c8
Antonov548 <antonov5551998@gmail.com>
2025-03-04 13:28:50 +0100

    vendor: Update vendored sources to duckdb/duckdb@7ab1893992e68c2c0d850fb2b820e7dee86767a4
    Generalize `rowid` into the concept of virtual columns, and make `filename` a virtual column in the Parquet/CSV/JSON readers (duckdb/duckdb#16248)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev323"
+#define DUCKDB_PATCH_VERSION "1-dev334"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev323"
+#define DUCKDB_VERSION "v1.2.1-dev334"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "28c95be6a9"
+#define DUCKDB_SOURCE_ID "7ab1893992"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
