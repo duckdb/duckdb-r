a714ebec3add0041922cb8bbb352ac8624c07fdf
Antonov548 <antonov5551998@gmail.com>
2025-03-04 18:01:46 +0100

    vendor: Update vendored sources to duckdb/duckdb@2e15ed6f8d30a98f813928045d46b1789b7ff6db
    Improve error message on failure to install local extension (duckdb/duckdb#16371)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev650"
+#define DUCKDB_PATCH_VERSION "1-dev652"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev650"
+#define DUCKDB_VERSION "v1.2.1-dev652"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "88de9ecd79"
+#define DUCKDB_SOURCE_ID "2e15ed6f8d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
