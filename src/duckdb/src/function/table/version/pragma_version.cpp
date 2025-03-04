f96d15386f442fcfd2c3f4dbccb4baa4b03ecd89
Antonov548 <antonov5551998@gmail.com>
2025-03-04 18:01:34 +0100

    vendor: Update vendored sources to duckdb/duckdb@ef88e2bb17b1234bdc5b84c0b6ebb8e74da89bf8
    Verify UTF-8 in `DeltaLengthByteArrayDecoder` and speed it up (duckdb/duckdb#16328)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev633"
+#define DUCKDB_PATCH_VERSION "1-dev640"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev633"
+#define DUCKDB_VERSION "v1.2.1-dev640"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "134e0ae1b2"
+#define DUCKDB_SOURCE_ID "ef88e2bb17"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
