5ac91958c36906b48204576f8b599894dec0ddbc
Antonov548 <antonov5551998@gmail.com>
2025-03-04 14:55:33 +0100

    vendor: Update vendored sources to duckdb/duckdb@d9ee15f45ec6285cba14892480384611c10a8fde
    fix: add StringStats::SetMaxStringLength (duckdb/duckdb#16326)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev627"
+#define DUCKDB_PATCH_VERSION "1-dev629"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev627"
+#define DUCKDB_VERSION "v1.2.1-dev629"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "befbc739c2"
+#define DUCKDB_SOURCE_ID "d9ee15f45e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
