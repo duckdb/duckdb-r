37a1b0693b8f399c96b74265e46c59a9cbed2466
Antonov548 <antonov5551998@gmail.com>
2025-03-04 19:44:38 +0100

    vendor: Update vendored sources to duckdb/duckdb@87cf7730330dff3d1998be29915ab29bbcd6b782
    Rework CSV Reader: use the new MultiFileReaderFunction interface (duckdb/duckdb#16349)
    Revert "Linux CLI: override platform for ARM manylinux" (duckdb/duckdb#16374)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev675"
+#define DUCKDB_PATCH_VERSION "1-dev725"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev675"
+#define DUCKDB_VERSION "v1.2.1-dev725"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b33c23c1cb"
+#define DUCKDB_SOURCE_ID "87cf773033"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
