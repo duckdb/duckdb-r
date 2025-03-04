14bc3af3e3ada52fa975ed60abd07e8474454a35
Antonov548 <antonov5551998@gmail.com>
2025-03-04 19:44:41 +0100

    vendor: Update vendored sources to duckdb/duckdb@a76ff3f10b55d305c26f4e14030cc32939a54342
    Add connection and transaction identifiers (duckdb/duckdb#16296)

--- src/duckdb/ub_src_function_scalar_system.cpp
+++ src/duckdb/ub_src_function_scalar_system.cpp
@@ -2,3 +2,9 @@
 
 #include "src/function/scalar/system/write_log.cpp"
 
+#include "src/function/scalar/system/current_transaction_id.cpp"
+
+#include "src/function/scalar/system/current_connection_id.cpp"
+
+#include "src/function/scalar/system/current_query_id.cpp"
+
