463259f9bbef9866a14eae00caa48dd531ca2744
Antonov548 <antonov5551998@gmail.com>
2025-03-04 14:55:10 +0100

    vendor: Update vendored sources to duckdb/duckdb@1424181fc71f7d44a8eb43ce6c281566ef8312a8
    Multi File Reader Rework: Add `MultiFileReaderFunction` that is used to wrap a single-file reader, and use it for the Parquet reader (duckdb/duckdb#16299)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev486"
+#define DUCKDB_PATCH_VERSION "1-dev511"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev486"
+#define DUCKDB_VERSION "v1.2.1-dev511"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e249a40c8b"
+#define DUCKDB_SOURCE_ID "1424181fc7"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
