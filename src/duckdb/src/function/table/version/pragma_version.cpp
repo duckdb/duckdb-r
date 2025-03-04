084af8877b01ff354055e6ea571217e2f8dac7c4
Antonov548 <antonov5551998@gmail.com>
2025-03-04 14:55:29 +0100

    vendor: Update vendored sources to duckdb/duckdb@befbc739c299e00146c2128b7cd8e654e3c5e68b
    Add new recursive semantics (`USING KEY`) (duckdb/duckdb#12430)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev581"
+#define DUCKDB_PATCH_VERSION "1-dev627"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev581"
+#define DUCKDB_VERSION "v1.2.1-dev627"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "7a9e96d0a7"
+#define DUCKDB_SOURCE_ID "befbc739c2"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
