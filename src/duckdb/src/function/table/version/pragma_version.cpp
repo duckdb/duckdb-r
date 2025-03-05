66cf83358e39b2ec803d16f01763cd6504a3834b
Antonov548 <antonov5551998@gmail.com>
2025-03-05 12:23:49 +0100

    vendor: Update vendored sources to duckdb/duckdb@00bd91808c3a5ab318b683bf91c0ababed3cf1cc
    Internal duckdb/duckdb#4287: INTERVAL Times DOUBLE (duckdb/duckdb#16386)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev734"
+#define DUCKDB_PATCH_VERSION "1-dev737"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev734"
+#define DUCKDB_VERSION "v1.2.1-dev737"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "69caf579a4"
+#define DUCKDB_SOURCE_ID "00bd91808c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
