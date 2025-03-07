d6a47269dcda62d5bd24301f103f2211eb8460ef
Kirill Müller <kirill@cynkra.com>
2025-03-07 08:37:47 +0100

    vendor: Update vendored sources to duckdb/duckdb@b6d9eabb8f0aa61acc1b2f7a7c286640efcdd198
    Rename `DUCKDB_API` to `DUCKDB_C_API` for `duckdb.h` (duckdb/duckdb#16397)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev168"
+#define DUCKDB_PATCH_VERSION "1-dev176"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev168"
+#define DUCKDB_VERSION "v1.2.1-dev176"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "3050447575"
+#define DUCKDB_SOURCE_ID "b6d9eabb8f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
