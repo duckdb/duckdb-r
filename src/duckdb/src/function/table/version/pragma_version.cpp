59875bc0806888b3370db55803518692c5abe6fc
Kirill Müller <kirill@cynkra.com>
2025-03-07 08:37:41 +0100

    vendor: Update vendored sources to duckdb/duckdb@3050447575d18bb48bbd5177fdc5e2ea2f06b70e
    Use checkpoint bind in DuckTableEntry::Copy to avoid re-validating default values (and potentially causing issues during WAL replay) (duckdb/duckdb#16398)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev165"
+#define DUCKDB_PATCH_VERSION "1-dev168"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev165"
+#define DUCKDB_VERSION "v1.2.1-dev168"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "96b9742bc6"
+#define DUCKDB_SOURCE_ID "3050447575"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
