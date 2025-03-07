cda4e2b0fb2da2e878ff4bde8a5bd9f576c51065
Kirill Müller <kirill@cynkra.com>
2025-03-07 08:37:35 +0100

    vendor: Update vendored sources to duckdb/duckdb@96b9742bc6b3d8e931ecb8c63791182c87f5d349
    Only select options that generate more columns with null_padding, if they at least hold 50% of consistency (duckdb/duckdb#16404)
    Add twine_upload option to Python.yml to trigger upload (duckdb/duckdb#16410)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev160"
+#define DUCKDB_PATCH_VERSION "1-dev165"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev160"
+#define DUCKDB_VERSION "v1.2.1-dev165"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e9a3cec452"
+#define DUCKDB_SOURCE_ID "96b9742bc6"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
