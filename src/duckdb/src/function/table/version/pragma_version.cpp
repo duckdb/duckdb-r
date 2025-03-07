53c8a8f021f2dbef67d2f24c11fad034d8700d6d
Kirill Müller <kirill@cynkra.com>
2025-03-07 08:38:02 +0100

    vendor: Update vendored sources to duckdb/duckdb@ddee4c445f385dca7869115e5e2f3c513d73bf1a
    [Fix] Throw constraint violation for FK constraint checking (duckdb/duckdb#16399)
    Fix PyPi upload also for branches, when twine_upload is provided (duckdb/duckdb#16421)
    Internal duckdb/duckdb#4258: MODE Spooling Stability (duckdb/duckdb#16415)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev180"
+#define DUCKDB_PATCH_VERSION "1-dev189"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev180"
+#define DUCKDB_VERSION "v1.2.1-dev189"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "718e9f1396"
+#define DUCKDB_SOURCE_ID "ddee4c445f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
