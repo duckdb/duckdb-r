8b8a6e7101c468e70d45465e05a42eeb92f35dcb
Kirill Müller <kirill@cynkra.com>
2025-03-07 08:37:30 +0100

    vendor: Update vendored sources to duckdb/duckdb@e9a3cec452a5e18d018783c4af4bb54bad8ef538
    Add support for autoload and autoinstall for `ui` extension (duckdb/duckdb#16393)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev158"
+#define DUCKDB_PATCH_VERSION "1-dev160"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev158"
+#define DUCKDB_VERSION "v1.2.1-dev160"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f99871b64c"
+#define DUCKDB_SOURCE_ID "e9a3cec452"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
