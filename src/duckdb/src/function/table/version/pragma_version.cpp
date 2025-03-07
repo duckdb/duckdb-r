bf7f2ee54a9df3e0a1f33d7653dcebd0b8768fe7
Kirill Müller <kirill@cynkra.com>
2025-03-07 08:36:25 +0100

    vendor: Update vendored sources to duckdb/duckdb@cabb9db877c6f69fe5c4e814a28bb42d9a8eda27
    Fix heap buffer oveflow sampling (duckdb/duckdb#16279)
    [Fix] MinGW bundle static libs (duckdb/duckdb#16292)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev99"
+#define DUCKDB_PATCH_VERSION "1-dev112"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev99"
+#define DUCKDB_VERSION "v1.2.1-dev112"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "24d18d02e7"
+#define DUCKDB_SOURCE_ID "cabb9db877"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
