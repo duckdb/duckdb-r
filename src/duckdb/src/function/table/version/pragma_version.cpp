48040624db7f1d01500a5b7c60fd6e11af9d8525
Kirill Müller <kirill@cynkra.com>
2025-03-07 08:37:09 +0100

    vendor: Update vendored sources to duckdb/duckdb@ff41058fc983e252b773089e6075c4bebd21c6ab
    Backport ebb4dccf to v1.2-, adding missing include (duckdb/duckdb#16369)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev146"
+#define DUCKDB_PATCH_VERSION "1-dev148"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev146"
+#define DUCKDB_VERSION "v1.2.1-dev148"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d8387736a9"
+#define DUCKDB_SOURCE_ID "ff41058fc9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
