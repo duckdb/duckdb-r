1de98d48b131c358a290d7ffd5e65ccaf177497c
Kirill Müller <kirill@cynkra.com>
2025-03-07 08:39:29 +0100

    vendor: Update vendored sources (tag v1.2.1) to duckdb/duckdb@8e52ec43959ab363643d63cb78ee214577111da4
    bump spatial again (duckdb/duckdb#16518)
    LinuxRelease.yml: Pass down override git describe (duckdb/duckdb#16521)
    CI Fixes after upgrade to ubuntu 22 (duckdb/duckdb#16516)
    bump spatial and excel for v1.2 (duckdb/duckdb#16504)
    Move from ubuntu-20:04 to ubuntu-22:04, part II (duckdb/duckdb#16514)
    [chore] Build Linux releases also on PRs AND ubuntu-20 to 22 (duckdb/duckdb#16506)
    Move from ubuntu-20:04 to ubuntu-22:04, part I (duckdb/duckdb#16510)
    CodeQuality: ubuntu-20 to ubuntu-22, lock black to version 24 and trick clang_format detection (duckdb/duckdb#16513)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev248"
+#define DUCKDB_PATCH_VERSION "1"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev248"
+#define DUCKDB_VERSION "v1.2.1"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ef253eea8e"
+#define DUCKDB_SOURCE_ID "8e52ec4395"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
