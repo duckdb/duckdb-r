9acb564743a67251f100962404004eadc1cb746c
Kirill Müller <kirill@cynkra.com>
2025-03-07 08:39:04 +0100

    vendor: Update vendored sources to duckdb/duckdb@fba0ffea7e034d2601b8ec5c7afb508245bbec93
    [BugFix]: Swap join children, not left and right set (duckdb/duckdb#16487)
    bump sqlsmith extension tag (duckdb/duckdb#16488)
    Bump `postgres_scanner` and `fts` extensions (duckdb/duckdb#16492)
    [tests] Multiple FORMAT in copy, only last one matters (duckdb/duckdb#16493)
    [Python Dev] `pyproject.toml` should not use `oldest-supported-numpy` anymore (duckdb/duckdb#16486)
    Fixed reading piped JSON (duckdb/duckdb#16480)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev226"
+#define DUCKDB_PATCH_VERSION "1-dev241"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev226"
+#define DUCKDB_VERSION "v1.2.1-dev241"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "28b8a398a4"
+#define DUCKDB_SOURCE_ID "fba0ffea7e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
