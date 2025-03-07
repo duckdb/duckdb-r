3e125c271c935f81440d023e18cb5d2c5546ef0c
Kirill Müller <kirill@cynkra.com>
2025-03-07 08:36:32 +0100

    vendor: Update vendored sources to duckdb/duckdb@317dc5ba5a9cee309425f8ca770c680c9fb475d9
    Allow accessing profiler query tree under lock (duckdb/duckdb#16314)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev116"
+#define DUCKDB_PATCH_VERSION "1-dev118"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev116"
+#define DUCKDB_VERSION "v1.2.1-dev118"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "5ea07fd6a4"
+#define DUCKDB_SOURCE_ID "317dc5ba5a"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
