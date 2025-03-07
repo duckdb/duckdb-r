e98bff4f7f24a4bf384981e82caf21684bdf0c4e
Kirill Müller <kirill@cynkra.com>
2025-03-07 08:33:59 +0100

    vendor: Update vendored sources to duckdb/duckdb@f7637a9b9be4c8fb199e47d8f1011f5eee0a9b4e
    [Dev] MultiFileReader fix InternalError in CreateFilterMap (duckdb/duckdb#16114)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0"
+#define DUCKDB_PATCH_VERSION "1-dev3"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.0"
+#define DUCKDB_VERSION "v1.2.1-dev3"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "5f5512b827"
+#define DUCKDB_SOURCE_ID "f7637a9b9b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
