c4dabcee54b012838527249c6c9862c0aab1d672
Kirill Müller <kirill@cynkra.com>
2025-03-09 23:15:00 +0100

    Substance

--- inst/include/cpp11/R.hpp
+++ inst/include/cpp11/R.hpp
@@ -51,7 +51,7 @@
 namespace cpp11 {
 namespace literals {
 
-constexpr R_xlen_t operator"" _xl(unsigned long long int value) { return value; }
+constexpr R_xlen_t operator""_xl(unsigned long long int value) { return value; }
 
 }  // namespace literals
 
