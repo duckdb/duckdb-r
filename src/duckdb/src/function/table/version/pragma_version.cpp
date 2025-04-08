41d69127c55abe2a08d9434085871979473badfe
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:55:50 +0200

    vendor: Update vendored sources to duckdb/duckdb@6610bc9122f0b091bf0ff074fd8854091b33cc24
    add avro (duckdb/duckdb#16708)
    Fix duckdb/duckdb#16554: emit blobs as part of .dump (duckdb/duckdb#16693)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev72"
+#define DUCKDB_PATCH_VERSION "2-dev77"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev72"
+#define DUCKDB_VERSION "v1.2.2-dev77"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "745bc7877f"
+#define DUCKDB_SOURCE_ID "6610bc9122"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
