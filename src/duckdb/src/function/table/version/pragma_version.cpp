7bb3ec06cf47703ffe56523d41af3a381d536c2b
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:55:19 +0200

    vendor: Update vendored sources to duckdb/duckdb@d76de45caa58c69f04ff8a2e16270d324ecf3559
    Avoid UMA (duckdb/duckdb#16641)
    Add libs folder to bundled static libs (duckdb/duckdb#16655)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev50"
+#define DUCKDB_PATCH_VERSION "2-dev54"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev50"
+#define DUCKDB_VERSION "v1.2.2-dev54"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "2243dd860b"
+#define DUCKDB_SOURCE_ID "d76de45caa"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
