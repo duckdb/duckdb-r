c7c3d6e4490e737ce29cecfd2730f8a36d9bcc48
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:57:32 +0200

    vendor: Update vendored sources to duckdb/duckdb@78379ad797f8e5aa4b10f4ff0a95be3f13b4b47a
    Allow overriding the Printer::Print output destination (duckdb/duckdb#16909)
    [Python] Fix PyArrow filter pushdown for NaN (duckdb/duckdb#16952)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev166"
+#define DUCKDB_PATCH_VERSION "2-dev176"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev166"
+#define DUCKDB_VERSION "v1.2.2-dev176"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "929daf42d9"
+#define DUCKDB_SOURCE_ID "78379ad797"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
