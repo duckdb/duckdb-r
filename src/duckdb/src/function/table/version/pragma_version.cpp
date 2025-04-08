5b30cd60def93b5061f203c94151fc2b0f541263
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:57:35 +0200

    vendor: Update vendored sources to duckdb/duckdb@099fba2a4b5563dd40b5a462fd6fac0594560a4c
    bump out-of-tree extensions to v1.2.2 (duckdb/duckdb#16913)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev176"
+#define DUCKDB_PATCH_VERSION "2-dev185"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev176"
+#define DUCKDB_VERSION "v1.2.2-dev185"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "78379ad797"
+#define DUCKDB_SOURCE_ID "099fba2a4b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
