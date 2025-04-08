5b55895fb037bc06d900eff3ed5170bdf76520c8
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:56:46 +0200

    vendor: Update vendored sources to duckdb/duckdb@72cf6467725a4eaeb39c02c115a5952f800eea7d
    Add extra check in normalize for commit, rollback and abort (duckdb/duckdb#16802)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev123"
+#define DUCKDB_PATCH_VERSION "2-dev126"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev123"
+#define DUCKDB_VERSION "v1.2.2-dev126"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "882980aa42"
+#define DUCKDB_SOURCE_ID "72cf646772"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
