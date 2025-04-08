0002084ca6fe77e72119e65fff9ce8b995feabcd
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:57:04 +0200

    vendor: Update vendored sources to duckdb/duckdb@5920da2406ffa651d7c79493c9188518de5229f4
    [CSV Reader] Add check on ever quoted for candidate selection (duckdb/duckdb#16868)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev137"
+#define DUCKDB_PATCH_VERSION "2-dev140"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev137"
+#define DUCKDB_VERSION "v1.2.2-dev140"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c353171bc5"
+#define DUCKDB_SOURCE_ID "5920da2406"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
