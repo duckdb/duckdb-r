f909124fd463dfa61d6ada8426a5aa4f03246539
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:57:39 +0200

    vendor: Update vendored sources to duckdb/duckdb@5e26d7ad8277e48248f75181c5e79b44a6e178ad
    Allow SQL prepared statements to be rebound (duckdb/duckdb#16820)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev185"
+#define DUCKDB_PATCH_VERSION "2-dev187"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev185"
+#define DUCKDB_VERSION "v1.2.2-dev187"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "099fba2a4b"
+#define DUCKDB_SOURCE_ID "5e26d7ad82"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
