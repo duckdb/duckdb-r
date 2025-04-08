4491254ec2691514e6d37e2e97f23f1a2bd7f0b6
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:56:30 +0200

    vendor: Update vendored sources to duckdb/duckdb@91e67a22bf4533afb7abf9636aa8d369a4555d45
    [ADBC] Add wrapper to connection new, set options at connection init, if any. (duckdb/duckdb#16748)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev104"
+#define DUCKDB_PATCH_VERSION "2-dev112"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev104"
+#define DUCKDB_VERSION "v1.2.2-dev112"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "75fb0d7cdf"
+#define DUCKDB_SOURCE_ID "91e67a22bf"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
