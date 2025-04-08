31c6ecd6af2ec4ead2953963c3c7cbbd7a8bf27e
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:56:06 +0200

    vendor: Update vendored sources to duckdb/duckdb@0d2f28c10415550b0b1aaba0aa55618856be1d22
    Always throw the error that happens the earliest in the CSV Reader (duckdb/duckdb#16728)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev83"
+#define DUCKDB_PATCH_VERSION "2-dev85"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev83"
+#define DUCKDB_VERSION "v1.2.2-dev85"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "09bf1d736d"
+#define DUCKDB_SOURCE_ID "0d2f28c104"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
