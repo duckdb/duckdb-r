d8bc50926accb8b0d7a35343076a72db34ae05ff
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:55:38 +0200

    vendor: Update vendored sources to duckdb/duckdb@1f7c2c958097612d506f08e8e9e96554341bec42
    Several fixes for CSV fuzzer tests (duckdb/duckdb#16636)
    Avoid UMA (duckdb/duckdb#16642)
    Avoid UMA (duckdb/duckdb#16643)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev54"
+#define DUCKDB_PATCH_VERSION "2-dev68"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev54"
+#define DUCKDB_VERSION "v1.2.2-dev68"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d76de45caa"
+#define DUCKDB_SOURCE_ID "1f7c2c9580"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
