d921cc4556695dc27f17552e94e37ed7a6ea6c53
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:57:46 +0200

    vendor: Update vendored sources to duckdb/duckdb@4d7180d13e8988c73d565f97a29855ff425ed340
    Add back manylinux extensions [on v1.2-histrionicus] AND bump extensions (duckdb/duckdb#16976)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev194"
+#define DUCKDB_PATCH_VERSION "2-dev206"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev194"
+#define DUCKDB_VERSION "v1.2.2-dev206"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4de79a0fcb"
+#define DUCKDB_SOURCE_ID "4d7180d13e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
