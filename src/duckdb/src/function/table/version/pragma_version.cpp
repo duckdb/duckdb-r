e3074778b1f807db6bc16c7e8efa7fe36607500f
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:56:19 +0200

    vendor: Update vendored sources to duckdb/duckdb@d0ac2f2929e7230ac168de568d35dcde94b089c4
    Apply RLE fix to v1.2 (duckdb/duckdb#16756)
    CLI `-help`: Fix default value for -nullvalue (duckdb/duckdb#16755)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev92"
+#define DUCKDB_PATCH_VERSION "2-dev96"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev92"
+#define DUCKDB_VERSION "v1.2.2-dev96"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ede55e1564"
+#define DUCKDB_SOURCE_ID "d0ac2f2929"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
