185db7ceef5eb1991140287a6cb03aa3acd1e78e
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:56:35 +0200

    vendor: Update vendored sources to duckdb/duckdb@3fa4b872b3936ff4e373d0b4aaf1434dcc79bfa8
    `PhysicalTopN`: Buffer-allocated `StringHeap` (duckdb/duckdb#16770)
    Remove delta from extensions built on a nightly basis (vs 1.2-histrionicus) (duckdb/duckdb#16794)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev112"
+#define DUCKDB_PATCH_VERSION "2-dev117"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev112"
+#define DUCKDB_VERSION "v1.2.2-dev117"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "91e67a22bf"
+#define DUCKDB_SOURCE_ID "3fa4b872b3"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
