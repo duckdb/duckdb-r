e5eeea29f2992c21effdacb1eb15f31ae9e4bc31
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:56:02 +0200

    vendor: Update vendored sources to duckdb/duckdb@09bf1d736d031fb7c49a56201a436a390545c16e
    [Dev] Fix `EXPORT DATABASE` missing semicolons in produced `schema.sql` (duckdb/duckdb#16723)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev81"
+#define DUCKDB_PATCH_VERSION "2-dev83"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev81"
+#define DUCKDB_VERSION "v1.2.2-dev83"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "7b5e2b7373"
+#define DUCKDB_SOURCE_ID "09bf1d736d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
