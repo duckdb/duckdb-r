2cefa2f5edf73c690ecbc370eb407f93b7c0a653
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:56:57 +0200

    vendor: Update vendored sources to duckdb/duckdb@21301415e58c8a18110c80b0cc7f8d3aa4e56c16
    Fix duckdb/duckdb#16836: rewrite main column data in case of an update that only modifies the validity (duckdb/duckdb#16851)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev132"
+#define DUCKDB_PATCH_VERSION "2-dev135"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev132"
+#define DUCKDB_VERSION "v1.2.2-dev135"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a4380e7c08"
+#define DUCKDB_SOURCE_ID "21301415e5"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
