4987af2e7528771aff2e1a45ccae1b31cdb37a22
Kirill Müller <kirill@cynkra.com>
2025-04-08 05:57:12 +0200

    vendor: Update vendored sources to duckdb/duckdb@40c13c407044b6be8cbac4f0e78670a9fdb8721e
    Fix clang-tidy: add missing include (duckdb/duckdb#16920)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev142"
+#define DUCKDB_PATCH_VERSION "2-dev144"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.2-dev142"
+#define DUCKDB_VERSION "v1.2.2-dev144"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c26196e714"
+#define DUCKDB_SOURCE_ID "40c13c4070"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
