658e953dc52615e31ffe858eccf2f79253f3006d
Kirill Müller <kirill@cynkra.com>
2025-05-15 22:00:03 +0200

    vendor: Update vendored sources to duckdb/duckdb@dc4bb03fd1ccde4575e2fb3091787af42afe9741
    Add additional iterations to avoid assertion failure in `TemporaryMemoryManager` (duckdb/duckdb#16801)
    Make extensions be linked privitally into duckdb (duckdb/duckdb#16726)
    OSX.yml & Windows.yml: remove repository_dispatch, already handled by InvokeCI (duckdb/duckdb#16796)
    Remove delta from extensions built on a nightly basis (vs main branch) (duckdb/duckdb#16795)
    [chore] NotifyExternalRepositories.yml: Fix endpoint to be pinged (duckdb/duckdb#16793)
    Python docs: List all join types (duckdb/duckdb#16789)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1726"
+#define DUCKDB_PATCH_VERSION "0-dev1738"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev1726"
+#define DUCKDB_VERSION "v1.3.0-dev1738"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "242d3f78e4"
+#define DUCKDB_SOURCE_ID "dc4bb03fd1"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
