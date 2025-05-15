51eff6c8cc64bacb10dcee3795fe88cc04f59888
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:50:34 +0200

    vendor: Update vendored sources to duckdb/duckdb@b32ac647c8879f996dd0380c58a7893d87e79cd7
    feat: move GRANT from reserved to unreserved keyword (duckdb/duckdb#16546)
    Fix `test/sql/aggregate/aggregates/histogram_table_function.test` to pass the Linux CLI (arm64) CI (duckdb/duckdb#16538)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1116"
+#define DUCKDB_PATCH_VERSION "0-dev1121"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev1116"
+#define DUCKDB_VERSION "v1.3.0-dev1121"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "17c58d07e5"
+#define DUCKDB_SOURCE_ID "b32ac647c8"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
