81b2bcb535e1a0a495970724a2eac60653d4a26d
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:49:23 +0200

    vendor: Update vendored sources to duckdb/duckdb@208f1ece86fbc962088fd15a652294aaad02e8e4
    Parquet: always launch max threads if we are scanning multiple files (duckdb/duckdb#16457)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev922"
+#define DUCKDB_PATCH_VERSION "0-dev924"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev922"
+#define DUCKDB_VERSION "v1.3.0-dev924"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "68bd4a5277"
+#define DUCKDB_SOURCE_ID "208f1ece86"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
