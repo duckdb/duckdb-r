bf6d256af27b7bbd032834bfb9872a22582f6a19
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:50:29 +0200

    vendor: Update vendored sources to duckdb/duckdb@17c58d07e51e64d28e14d6f88679ae0e7719a7a1
    throw() -> noexcept in skiplist (duckdb/duckdb#16548)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1114"
+#define DUCKDB_PATCH_VERSION "0-dev1116"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev1114"
+#define DUCKDB_VERSION "v1.3.0-dev1116"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c973d00b77"
+#define DUCKDB_SOURCE_ID "17c58d07e5"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
