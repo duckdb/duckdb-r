41e475fe688e4e1625ab6d7987d63f2359ef5d43
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:59:02 +0200

    vendor: Update vendored sources to duckdb/duckdb@391593dc9ca020013a9ad14f3de3fab6d00d000a
    Add support for tracking `column_size_bytes` and `contains_nan` in RETURN_STATS (duckdb/duckdb#16731)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1578"
+#define DUCKDB_PATCH_VERSION "0-dev1587"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev1578"
+#define DUCKDB_VERSION "v1.3.0-dev1587"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "9a396a9bd7"
+#define DUCKDB_SOURCE_ID "391593dc9c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
