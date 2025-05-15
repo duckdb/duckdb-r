67155f5d3fcd6193c74b0573ae19675e2479e48a
Kirill Müller <kirill@cynkra.com>
2025-05-15 22:04:13 +0200

    vendor: Update vendored sources to duckdb/duckdb@c4d60a078ba96309ed01b3aa9c90a80e97223a48
    Allow a table to define their own row-id columns for delete/update, instead of assuming it is always COLUMN_IDENTIFIER_ROW_ID (duckdb/duckdb#17039)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2262"
+#define DUCKDB_PATCH_VERSION "0-dev2264"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2262"
+#define DUCKDB_VERSION "v1.3.0-dev2264"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "589e10ae54"
+#define DUCKDB_SOURCE_ID "c4d60a078b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
