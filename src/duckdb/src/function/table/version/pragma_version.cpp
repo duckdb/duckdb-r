ea28b8456bd6697b5346fb20967fadefa0ea7f93
Kirill Müller <kirill@cynkra.com>
2025-05-15 22:04:00 +0200

    vendor: Update vendored sources to duckdb/duckdb@b0bb04ffacd8b8c26fe02fa036d670ed2bb67d90
    Issue duckdb/duckdb#17001: AsOf memory Management (duckdb/duckdb#17028)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2249"
+#define DUCKDB_PATCH_VERSION "0-dev2252"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2249"
+#define DUCKDB_VERSION "v1.3.0-dev2252"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "8f3dc1188e"
+#define DUCKDB_SOURCE_ID "b0bb04ffac"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
