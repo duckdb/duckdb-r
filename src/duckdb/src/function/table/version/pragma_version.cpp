cbc2e3f096f8e3232456f4e9fde7fce2c087e308
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:43:44 +0200

    vendor: Update vendored sources to duckdb/duckdb@6fa3b7a5a27b07bd030baaff21ea368526355026
    Parquet Reader: Implement dedicated Skip method (duckdb/duckdb#16117)
    Using GitHub ARM runners for Linux CLI builds (duckdb/duckdb#16119)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev92"
+#define DUCKDB_PATCH_VERSION "1-dev102"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev92"
+#define DUCKDB_VERSION "v1.2.1-dev102"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1012bcfd0b"
+#define DUCKDB_SOURCE_ID "6fa3b7a5a2"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
