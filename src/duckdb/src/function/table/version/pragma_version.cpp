e21a0e470b9f00dcac723fc7a6674c1bb593ef6f
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:58:43 +0200

    vendor: Update vendored sources to duckdb/duckdb@6249beb0eb0d6558a973b1644151e53c88e04743
    In case all rows of a CSV batch are errors, we continue processing (duckdb/duckdb#16713)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1496"
+#define DUCKDB_PATCH_VERSION "0-dev1498"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev1496"
+#define DUCKDB_VERSION "v1.3.0-dev1498"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "db82e0bfcd"
+#define DUCKDB_SOURCE_ID "6249beb0eb"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
