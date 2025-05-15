40968637e8907f07ae1b66ae25179725c1881a9e
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:59:22 +0200

    vendor: Update vendored sources to duckdb/duckdb@34f5057462d6e20b917d04b730aa2fe383928450
    Add an initial value to `list_reduce` (duckdb/duckdb#16602)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1607"
+#define DUCKDB_PATCH_VERSION "0-dev1621"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev1607"
+#define DUCKDB_VERSION "v1.3.0-dev1621"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b75e8fde9d"
+#define DUCKDB_SOURCE_ID "34f5057462"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
