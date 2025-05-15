088479bed67928d3cdf3968d9b4e9618e8065820
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:42:48 +0200

    vendor: Update vendored sources to duckdb/duckdb@a35a3fba95c51fdb46663285505f73f6d18c6b01
    Accept "Auto" as date/timestamp format (duckdb/duckdb#15808)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev59"
+#define DUCKDB_PATCH_VERSION "1-dev63"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev59"
+#define DUCKDB_VERSION "v1.2.1-dev63"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "9b8f3eb7a3"
+#define DUCKDB_SOURCE_ID "a35a3fba95"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
