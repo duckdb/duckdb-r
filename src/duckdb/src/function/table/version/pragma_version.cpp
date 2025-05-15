58ffbb046d89363ae3d63f316b6999f0684fd7ab
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:43:35 +0200

    vendor: Update vendored sources to duckdb/duckdb@0a4ad94e511b2d27a43eedec9705829c8b840719
    Parquet Dictionary reader: set NULL values as the last value in the dictionary (duckdb/duckdb#16106)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev84"
+#define DUCKDB_PATCH_VERSION "1-dev87"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev84"
+#define DUCKDB_VERSION "v1.2.1-dev87"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4aed35894e"
+#define DUCKDB_SOURCE_ID "0a4ad94e51"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
