28ba43e6667d46546201835935037d554e64901f
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:47:20 +0200

    vendor: Update vendored sources to duckdb/duckdb@7a9e96d0a78d9635ba0a409bcf026974c50dfc8b
    Merge V1.2 histrionicus into main (duckdb/duckdb#16324)
    Bump Julia FixedPointDecimals dependency version (duckdb/duckdb#16323)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev520"
+#define DUCKDB_PATCH_VERSION "1-dev581"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev520"
+#define DUCKDB_VERSION "v1.2.1-dev581"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d52132a24f"
+#define DUCKDB_SOURCE_ID "7a9e96d0a7"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
