9441d6429f8432bcf6a672e33ab2dff264f50cd9
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:57:01 +0200

    vendor: Update vendored sources to duckdb/duckdb@5f7d3e02dd91c75c8913cbd7384fc4c0cb2b9698
    Add support for `RETURN_STATS` to `COPY` (duckdb/duckdb#16595)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1163"
+#define DUCKDB_PATCH_VERSION "0-dev1180"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev1163"
+#define DUCKDB_VERSION "v1.3.0-dev1180"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a61917778c"
+#define DUCKDB_SOURCE_ID "5f7d3e02dd"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
