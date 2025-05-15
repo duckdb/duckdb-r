d6a96cef1bac571b7ddb06d149a30d32cb5d53a6
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:46:43 +0200

    vendor: Update vendored sources to duckdb/duckdb@7f22b1edea37a73ee91fc3fac0d4f2bae473cc4d
    Add pragma to truncate duckdb log storage (duckdb/duckdb#16274)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev431"
+#define DUCKDB_PATCH_VERSION "1-dev433"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev431"
+#define DUCKDB_VERSION "v1.2.1-dev433"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "53edbbc9b1"
+#define DUCKDB_SOURCE_ID "7f22b1edea"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
