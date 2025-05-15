c9349b77497aa436861fd2e04aa109673469bf4a
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:50:02 +0200

    vendor: Update vendored sources to duckdb/duckdb@c486d1797ed8f90e17d7852d200c4e1b381cfab1
    Avoid throwing an exception (that is then swallowed) when computing compressed materialization over stats that are not set (duckdb/duckdb#16532)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1089"
+#define DUCKDB_PATCH_VERSION "0-dev1091"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev1089"
+#define DUCKDB_VERSION "v1.3.0-dev1091"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1df191691f"
+#define DUCKDB_SOURCE_ID "c486d1797e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
