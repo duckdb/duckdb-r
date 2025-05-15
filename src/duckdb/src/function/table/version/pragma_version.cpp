ecc57307a47b0ff86a7467ce94bb0fc52b7a95f9
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:59:18 +0200

    vendor: Update vendored sources to duckdb/duckdb@b75e8fde9de2317e741620d81b002145f35072ff
    Write UUID stats to Parquet files and support reading uuid stats (duckdb/duckdb#16744)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1602"
+#define DUCKDB_PATCH_VERSION "0-dev1607"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev1602"
+#define DUCKDB_VERSION "v1.3.0-dev1607"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "29fb829338"
+#define DUCKDB_SOURCE_ID "b75e8fde9d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
