1e073558e6520e29b61dce8fab314e6b697144d4
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:48:41 +0200

    vendor: Update vendored sources to duckdb/duckdb@5cdaf5649863733392c8448f5ce4231c71e0d51d
    Fuzzer Fix: Fix Avg for NULL cast to TIMESTAMP (duckdb/duckdb#16394)
    MAIN_BRANCH_VERSIONING: Adopt also for Python build and amalgamation (duckdb/duckdb#16400)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,17 +1,17 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev754"
+#define DUCKDB_PATCH_VERSION "0-dev758"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
-#define DUCKDB_MINOR_VERSION 2
+#define DUCKDB_MINOR_VERSION 3
 #endif
 #ifndef DUCKDB_MAJOR_VERSION
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev754"
+#define DUCKDB_VERSION "v1.3.0-dev758"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "920b39ad96"
+#define DUCKDB_SOURCE_ID "5cdaf56498"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
