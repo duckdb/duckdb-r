bcd3eb82757f9d61153220a47967cc1cda45536f
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:59:14 +0200

    vendor: Update vendored sources to duckdb/duckdb@29fb829338eadcec47e747f40b1ff2fedc2fc162
    RLE compression - memset alignment bytes to zero when aligning the counts (duckdb/duckdb#16735)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1599"
+#define DUCKDB_PATCH_VERSION "0-dev1602"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev1599"
+#define DUCKDB_VERSION "v1.3.0-dev1602"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "7c496f6dbb"
+#define DUCKDB_SOURCE_ID "29fb829338"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
