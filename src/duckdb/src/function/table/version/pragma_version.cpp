c6095cc7b9ae14cbf827de2161a5a78f54cf1b45
Kirill Müller <kirill@cynkra.com>
2025-05-15 22:01:59 +0200

    vendor: Update vendored sources to duckdb/duckdb@c66050bbda4eb60d80ba96ee3df2e97a88e36c01
    Fix several issues with vsize=2, and move vsize=2 tests to `Main.yml` (duckdb/duckdb#16918)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2054"
+#define DUCKDB_PATCH_VERSION "0-dev2066"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2054"
+#define DUCKDB_VERSION "v1.3.0-dev2066"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "643ba824aa"
+#define DUCKDB_SOURCE_ID "c66050bbda"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
