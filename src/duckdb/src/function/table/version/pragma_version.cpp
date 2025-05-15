c95a6dc8929b035f7cde5ae122c858dbfb445504
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:43:48 +0200

    vendor: Update vendored sources to duckdb/duckdb@1300137440904eca0ad683dca378b8d9ae964f58
    Use ColumnSegment::FilterSelection and SelectionVector for filtering in Parquet scans (duckdb/duckdb#16126)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev102"
+#define DUCKDB_PATCH_VERSION "1-dev104"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev102"
+#define DUCKDB_VERSION "v1.2.1-dev104"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "6fa3b7a5a2"
+#define DUCKDB_SOURCE_ID "1300137440"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
