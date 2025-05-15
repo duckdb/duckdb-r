ef80a9a1bd5ecf9f37fc3c112473fa23959f07ac
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:44:31 +0200

    vendor: Update vendored sources to duckdb/duckdb@05e95a9949ad615d54b6875c023d8f6f7b761d5b
    Parquet Reader: Split `CreateReader` into two separate stages - `ParseSchema` and `CreateReader` (duckdb/duckdb#16161)
    Removed the last CI job that used the Ubuntu 18 setup (duckdb/duckdb#16155)
    Hopefully fixing ci runs (duckdb/duckdb#16150)
    Add uniq_ptr_cast for interpreted benchmark. (duckdb/duckdb#16151)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev154"
+#define DUCKDB_PATCH_VERSION "1-dev176"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev154"
+#define DUCKDB_VERSION "v1.2.1-dev176"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c6d5179824"
+#define DUCKDB_SOURCE_ID "05e95a9949"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
