cedd09576ff6e57b5178deb29bc8eaf9e651f18c
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:57:52 +0200

    vendor: Update vendored sources to duckdb/duckdb@6729faed7cec97521025479249e85b340f301de6
    Do not create validity mask for non-null const vector (duckdb/duckdb#16669)
    Add the missing binding for json_serialize_sql (duckdb/duckdb#16666)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1301"
+#define DUCKDB_PATCH_VERSION "0-dev1305"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev1301"
+#define DUCKDB_VERSION "v1.3.0-dev1305"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "84c87b12fa"
+#define DUCKDB_SOURCE_ID "6729faed7c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
