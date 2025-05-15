8607a883f596f3f4f2746eb46b45c4c2f7047c7e
Kirill Müller <kirill@cynkra.com>
2025-05-15 22:03:32 +0200

    vendor: Update vendored sources to duckdb/duckdb@641076b4d5d21f5e6b92c6aa7a1d8aa3706c34ee
    Detect when tables have been dropped or altered, and prevent deletes in this scenario (duckdb/duckdb#17018)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2223"
+#define DUCKDB_PATCH_VERSION "0-dev2228"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2223"
+#define DUCKDB_VERSION "v1.3.0-dev2228"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "451315955f"
+#define DUCKDB_SOURCE_ID "641076b4d5"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
