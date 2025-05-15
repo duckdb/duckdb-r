db35ad9762f48f900498d187eebfa52238c50f25
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:48:05 +0200

    vendor: Update vendored sources to duckdb/duckdb@0c75ac2b362853941732812c5ac12dbef3c71a16
    Internal duckdb/duckdb#2176: SUMMARIZE Temporal Types (duckdb/duckdb#16095)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev659"
+#define DUCKDB_PATCH_VERSION "1-dev670"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev659"
+#define DUCKDB_VERSION "v1.2.1-dev670"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "244951e707"
+#define DUCKDB_SOURCE_ID "0c75ac2b36"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
