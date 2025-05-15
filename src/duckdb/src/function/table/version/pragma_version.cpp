b309e520439e8bc07f525ec20521ec14f2e6f37a
Kirill Müller <kirill@cynkra.com>
2025-05-15 21:47:32 +0200

    vendor: Update vendored sources to duckdb/duckdb@5cb575067b6f9a9544b75ca7d18cbb86fc0470b0
    Fix decorrelation of WITH USING KEY (duckdb/duckdb#16330)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev629"
+#define DUCKDB_PATCH_VERSION "1-dev631"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 2
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.2.1-dev629"
+#define DUCKDB_VERSION "v1.2.1-dev631"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d9ee15f45e"
+#define DUCKDB_SOURCE_ID "5cb575067b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
