9d60e141ae6e13f8efca55c34a3465da09739a87
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:09:34 +0200

    vendor: Update vendored sources to duckdb/duckdb@5a89e084fca32d70416955a048fcacf3b8f2351c
    BUGFIX: do not perform unused columns optimization in presence of multiple grouping sets (duckdb/duckdb#17259)
    Internal duckdb/duckdb#4772: Timestamp Error Parameter (duckdb/duckdb#17283)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2793"
+#define DUCKDB_PATCH_VERSION "0-dev2800"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2793"
+#define DUCKDB_VERSION "v1.3.0-dev2800"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "745b850a56"
+#define DUCKDB_SOURCE_ID "5a89e084fc"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
