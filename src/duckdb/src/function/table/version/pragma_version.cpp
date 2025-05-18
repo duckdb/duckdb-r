5d0e275298f4cbe889e8e620468409336c81267d
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:06:18 +0200

    vendor: Update vendored sources to duckdb/duckdb@e41c5377745f0be3a075cabcbc7a4e827fb6476a
    Add bind_operator callback to TableFunction - allowing table functions to directly emit a LogicalOperator (duckdb/duckdb#17196)
    Issue duckdb/duckdb#17049: ICU Date Cast (duckdb/duckdb#17067)
    Notify JDBC repo to run Vendor.yml workflow (duckdb/duckdb#17099)
    Python.yml: Add back logic to perform fast-fail on Python 3.10 (duckdb/duckdb#17107)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2638"
+#define DUCKDB_PATCH_VERSION "0-dev2649"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2638"
+#define DUCKDB_VERSION "v1.3.0-dev2649"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "8630414388"
+#define DUCKDB_SOURCE_ID "e41c537774"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
