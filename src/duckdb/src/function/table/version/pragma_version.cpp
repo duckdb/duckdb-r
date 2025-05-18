d5bd4a2f25f5266c927f7e7a9c0cc43dfc53234c
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:24:20 +0200

    vendor: Update vendored sources to duckdb/duckdb@93fda3591f4298414fa362c59219c09e03f718ab
    Fix TIMETZ cast in example (duckdb/duckdb#17468)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3415"
+#define DUCKDB_PATCH_VERSION "0-dev3417"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3415"
+#define DUCKDB_VERSION "v1.3.0-dev3417"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "39c4e7dceb"
+#define DUCKDB_SOURCE_ID "93fda3591f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
