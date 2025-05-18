9e575c75ddfa32c6c139ea615abbd997320b6999
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:20:48 +0200

    vendor: Update vendored sources to duckdb/duckdb@e0e4cc2cc80e59b13f792756529e36a0300365a4
    Fix duckdb/duckdb#16552: adjust join condition sequence (duckdb/duckdb#16943)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3257"
+#define DUCKDB_PATCH_VERSION "0-dev3259"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3257"
+#define DUCKDB_VERSION "v1.3.0-dev3259"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a7422f6940"
+#define DUCKDB_SOURCE_ID "e0e4cc2cc8"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
