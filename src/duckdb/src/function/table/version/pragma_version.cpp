19d5a78ca59d364f91d036395f3b03c2156454a7
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:25:44 +0200

    vendor: Update vendored sources to duckdb/duckdb@9b4d61c134df27fd0e19a9ee0e0442b0195e9967
    HTTPFS Parameters fix (duckdb/duckdb#17486)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3468"
+#define DUCKDB_PATCH_VERSION "0-dev3479"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3468"
+#define DUCKDB_VERSION "v1.3.0-dev3479"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4bf6e7689c"
+#define DUCKDB_SOURCE_ID "9b4d61c134"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
