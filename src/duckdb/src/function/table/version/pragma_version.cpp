14e17e2ea908e502f83dc0032c772fd6ec877a4c
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:08:26 +0200

    vendor: Update vendored sources to duckdb/duckdb@c7638e6d2a3ed62cf48ef550599c4a0976e8981d
    Direct IO for temp files (duckdb/duckdb#17219)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2758"
+#define DUCKDB_PATCH_VERSION "0-dev2761"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2758"
+#define DUCKDB_VERSION "v1.3.0-dev2761"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "73170dfd73"
+#define DUCKDB_SOURCE_ID "c7638e6d2a"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
