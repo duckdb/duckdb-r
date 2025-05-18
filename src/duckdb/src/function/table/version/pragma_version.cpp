f792248bc15e171289eeeead44a49b625bf1fe0d
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:14:50 +0200

    vendor: Update vendored sources to duckdb/duckdb@01dc4fb1ee3b7dd4b7635d7ff1d2f5dcc58c695e
    Unify Parquet Metadata cache invalidation logic with Cached File System cache invalidation (duckdb/duckdb#17334)
    Revert "Skip MinGW, currently failing on main" (duckdb/duckdb#17342)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2951"
+#define DUCKDB_PATCH_VERSION "0-dev2956"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2951"
+#define DUCKDB_VERSION "v1.3.0-dev2956"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e3debafdd9"
+#define DUCKDB_SOURCE_ID "01dc4fb1ee"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
