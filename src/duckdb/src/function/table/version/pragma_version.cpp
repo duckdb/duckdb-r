e04a71d5a702d40886d6dacdeaa9c1480254186b
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:16:38 +0200

    vendor: Update vendored sources to duckdb/duckdb@f6b99c83b3e1b955d091bb991c686a8c5d5535b8
    Handle both ENCRYPTION_KEY and STORAGE_VERSION passed as options (duckdb/duckdb#17357)
    Support --directories option in format.py (duckdb/duckdb#17354)
    Add support for BENCHMARK_ROOT_DIRECTORY cmake option to change benchmark runner root directory, and add support for cache_file and reload options to enable better caching for non-DuckDB databases (duckdb/duckdb#17355)
    Pass commit ID to NotifyExternalRepositories.yml (duckdb/duckdb#17333)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2991"
+#define DUCKDB_PATCH_VERSION "0-dev2999"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2991"
+#define DUCKDB_VERSION "v1.3.0-dev2999"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "6edf7abdd2"
+#define DUCKDB_SOURCE_ID "f6b99c83b3"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
