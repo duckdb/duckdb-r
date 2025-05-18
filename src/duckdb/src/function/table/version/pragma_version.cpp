38991f3cc50c759d4e65f77c1889c76c7150b20d
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:20:10 +0200

    vendor: Update vendored sources to duckdb/duckdb@4a5e610f0c20fcfa31020dfba713c2ae573555d7
    Minor fixes for the CLI (duckdb/duckdb#17405)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3229"
+#define DUCKDB_PATCH_VERSION "0-dev3235"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3229"
+#define DUCKDB_VERSION "v1.3.0-dev3235"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d4816b0428"
+#define DUCKDB_SOURCE_ID "4a5e610f0c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
