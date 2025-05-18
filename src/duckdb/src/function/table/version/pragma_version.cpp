41483d130b58a4ba866e346457a92af0b0d2f81d
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:27:14 +0200

    vendor: Update vendored sources to duckdb/duckdb@311f550603088192ec93da9b4cea47bc11a4d5a9
    Fix: Optional Probe Selection (duckdb/duckdb#17505)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3537"
+#define DUCKDB_PATCH_VERSION "0-dev3543"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3537"
+#define DUCKDB_VERSION "v1.3.0-dev3543"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e2b7d36007"
+#define DUCKDB_SOURCE_ID "311f550603"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
