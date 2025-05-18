6fdd15419eb45844941815cf0ac183ebdb3135a1
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:02:55 +0200

    vendor: Update vendored sources to duckdb/duckdb@892a03d26e5f887f63e86259257f11bc50a32269
    Fix an issue in upserts where the local append state was not correctly flushed (duckdb/duckdb#17109)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2456"
+#define DUCKDB_PATCH_VERSION "0-dev2459"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2456"
+#define DUCKDB_VERSION "v1.3.0-dev2459"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "cae7a680bc"
+#define DUCKDB_SOURCE_ID "892a03d26e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
