158a6f6eed37d739f2186c03fcedfdcd9e956bc8
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:13:08 +0200

    vendor: Update vendored sources to duckdb/duckdb@1cffd596f2329c484b590ed905b4e79d688e7ad6
    Fix `try_cast` from NaN `double` to `decimal` (duckdb/duckdb#17322)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2912"
+#define DUCKDB_PATCH_VERSION "0-dev2915"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2912"
+#define DUCKDB_VERSION "v1.3.0-dev2915"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f174813237"
+#define DUCKDB_SOURCE_ID "1cffd596f2"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
