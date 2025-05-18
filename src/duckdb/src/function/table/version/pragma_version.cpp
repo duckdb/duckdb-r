f30aea9f332f3ebe74a5331e1f579a3e9842e76d
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:19:43 +0200

    vendor: Update vendored sources to duckdb/duckdb@62d401e2da0812aa8b7a4546cb5b6d9d2dcd413e
    [Dev] Add `QualifiedName::ParseComponents`, add input to the error messages (duckdb/duckdb#17403)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3223"
+#define DUCKDB_PATCH_VERSION "0-dev3225"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3223"
+#define DUCKDB_VERSION "v1.3.0-dev3225"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "bed258be65"
+#define DUCKDB_SOURCE_ID "62d401e2da"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
