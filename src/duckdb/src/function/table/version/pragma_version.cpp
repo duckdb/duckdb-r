3616b47551eae6e87611f6f479ae214cdcffca8a
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:16:56 +0200

    vendor: Update vendored sources to duckdb/duckdb@9e9063989b6959153371ff5c2cab546dafff7951
    Fixup amalgamation: reqlen is only used with assert enabled (duckdb/duckdb#17361)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3001"
+#define DUCKDB_PATCH_VERSION "0-dev3003"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3001"
+#define DUCKDB_VERSION "v1.3.0-dev3003"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c5a1a56a43"
+#define DUCKDB_SOURCE_ID "9e9063989b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
