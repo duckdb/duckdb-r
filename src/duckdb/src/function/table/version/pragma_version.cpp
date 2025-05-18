f89abe19b9347a75f5728f64f73cd6324d267ae4
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:22:45 +0200

    vendor: Update vendored sources to duckdb/duckdb@691c73217b8570d5a4e9ca92d8aa6f46bab7213a
    On COPY TO/FROM check the format during binding. (duckdb/duckdb#17381)
    Use pytest in SQLLogic Python test runner (duckdb/duckdb#16685)
    Fix JSON extension compilation on Ubuntu 22.04 (duckdb/duckdb#17434)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3307"
+#define DUCKDB_PATCH_VERSION "0-dev3341"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3307"
+#define DUCKDB_VERSION "v1.3.0-dev3341"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a1aecb68e8"
+#define DUCKDB_SOURCE_ID "691c73217b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
