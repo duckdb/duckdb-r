a9ca3a9e00df0349654d0b252eac05a62dce91de
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:20:18 +0200

    vendor: Update vendored sources to duckdb/duckdb@bf5b8aa11c726fcf2f107badcfd4af2f2e0800c5
    Add support for CREATE OR REPLACE TYPE, CREATE TYPE IF NOT EXISTS and CREATE TEMPORARY TYPE (duckdb/duckdb#17404)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3235"
+#define DUCKDB_PATCH_VERSION "0-dev3238"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3235"
+#define DUCKDB_VERSION "v1.3.0-dev3238"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4a5e610f0c"
+#define DUCKDB_SOURCE_ID "bf5b8aa11c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
