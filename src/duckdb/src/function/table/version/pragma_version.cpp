211e4a2371e8f609da501ed708818f7e062fb57f
Kirill Müller <kirill@cynkra.com>
2025-05-18 22:57:59 +0200

    vendor: Update vendored sources to duckdb/duckdb@0eb1bab6fd67a8dfee503b0cc04dea23e19cf19c
    support large dictionary value and constant vector creation in the C API (duckdb/duckdb#17064)
    Fix sqlite3 api wrapper link + remove R-CMD-check + add more nightly tests (duckdb/duckdb#17095)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2395"
+#define DUCKDB_PATCH_VERSION "0-dev2405"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2395"
+#define DUCKDB_VERSION "v1.3.0-dev2405"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ce69651242"
+#define DUCKDB_SOURCE_ID "0eb1bab6fd"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
