39655b58d0056d13822c719fd5b6f8f165f3a107
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:26:09 +0200

    vendor: Update vendored sources to duckdb/duckdb@1281f4a6f9a7ffa954d5647764f2b94d12d6cd7b
    Add support for altering structs (drop, add, rename field) inside `LIST` and `MAP` columns. (duckdb/duckdb#17462)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3484"
+#define DUCKDB_PATCH_VERSION "0-dev3497"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3484"
+#define DUCKDB_VERSION "v1.3.0-dev3497"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "470b2078eb"
+#define DUCKDB_SOURCE_ID "1281f4a6f9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
