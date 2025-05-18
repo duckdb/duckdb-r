d90bc9e7a0e1763a1e5b9fd7a7639a306fd81cdc
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:17:26 +0200

    vendor: Update vendored sources to duckdb/duckdb@cee5e77fdba70b13aba4bcd7f7dc4f516b8c99cd
    Fix two off-by-one errors in row estimate of range and generate_series (duckdb/duckdb#17373)
    Benchmark runner: add `argument`, `include` and `load_only` options - and make ClickBench run the original benchmark instead of a subset (duckdb/duckdb#17367)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3007"
+#define DUCKDB_PATCH_VERSION "0-dev3015"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3007"
+#define DUCKDB_VERSION "v1.3.0-dev3015"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "6b1b28bff5"
+#define DUCKDB_SOURCE_ID "cee5e77fdb"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
