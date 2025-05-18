76c2274f59e0112f90f3a6c1fc366117b83dcd12
Kirill Müller <kirill@cynkra.com>
2025-05-18 22:57:48 +0200

    vendor: Update vendored sources to duckdb/duckdb@ce69651242c04ff39fa7969a682212811b94880f
    fsst: Avoid to propagate alignment information in FSST_UNALIGNED_STORE (duckdb/duckdb#17094)
    [Dev] Automatically re-execute when calling `__arrow_c_stream__` on an already-consumed-result (duckdb/duckdb#17087)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2391"
+#define DUCKDB_PATCH_VERSION "0-dev2395"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2391"
+#define DUCKDB_VERSION "v1.3.0-dev2395"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b19550535b"
+#define DUCKDB_SOURCE_ID "ce69651242"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
