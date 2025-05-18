aa6ab9965a0e447888b1f79ee0435fd397f287fe
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:16:48 +0200

    vendor: Update vendored sources to duckdb/duckdb@c5a1a56a43e2cf4cb33aa19fb3856f163c7dd255
    Fix internal exception from assigning invalid index to `optional_idx query_id;` (duckdb/duckdb#17359)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2999"
+#define DUCKDB_PATCH_VERSION "0-dev3001"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2999"
+#define DUCKDB_VERSION "v1.3.0-dev3001"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f6b99c83b3"
+#define DUCKDB_SOURCE_ID "c5a1a56a43"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
