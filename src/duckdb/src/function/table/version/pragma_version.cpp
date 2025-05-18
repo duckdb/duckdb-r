3aace10cecc7aff6e05118fcc8ae9b7ebcff823a
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:11:11 +0200

    vendor: Update vendored sources to duckdb/duckdb@4e7d0036ab970dadfff3588eeeb0d6c0f5e93fae
    Switch from Bottom-Up to Top-Down Decorrelation Strategy (duckdb/duckdb#17294)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2836"
+#define DUCKDB_PATCH_VERSION "0-dev2849"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2836"
+#define DUCKDB_VERSION "v1.3.0-dev2849"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "888f296d48"
+#define DUCKDB_SOURCE_ID "4e7d0036ab"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
