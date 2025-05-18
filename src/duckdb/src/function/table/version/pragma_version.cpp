e5c0c050405bc0ff4dc955064a628ba4182f9481
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:08:40 +0200

    vendor: Update vendored sources to duckdb/duckdb@cc5fc347f27e1246b3078e334d219310930fc3c3
    Add "thousands" option to CSV Reader (duckdb/duckdb#17220)
    Fix [InvokeCI / NotifyExternalRepository] GitHub Actions has encountered an internal error when running your job. (duckdb/duckdb#17218)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2761"
+#define DUCKDB_PATCH_VERSION "0-dev2780"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2761"
+#define DUCKDB_VERSION "v1.3.0-dev2780"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c7638e6d2a"
+#define DUCKDB_SOURCE_ID "cc5fc347f2"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
