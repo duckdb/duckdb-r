e54875b327df77044bfc68679ddd33a4e4d84d1d
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:17:48 +0200

    vendor: Update vendored sources to duckdb/duckdb@5849139fa0e1937956c30aa222bd0f0557e5ba71
    Join Hash Table Probing Optimization: Optional Probing Selection Vector (duckdb/duckdb#17062)
    fix duckdb/duckdb#17258: Allow to open database in readonly mode within cli (duckdb/duckdb#17375)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3033"
+#define DUCKDB_PATCH_VERSION "0-dev3047"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3033"
+#define DUCKDB_VERSION "v1.3.0-dev3047"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "541d3fe2e6"
+#define DUCKDB_SOURCE_ID "5849139fa0"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
