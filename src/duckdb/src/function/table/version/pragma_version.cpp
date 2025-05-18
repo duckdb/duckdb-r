fbf2c013fd97bbc30dd72d4079e7b991d241bc2b
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:24:54 +0200

    vendor: Update vendored sources to duckdb/duckdb@d4d722c1c9d84c97d3518ddba484b2679257913c
    Emit dictionary vectors with unaligned start index (duckdb/duckdb#17471)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3446"
+#define DUCKDB_PATCH_VERSION "0-dev3448"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3446"
+#define DUCKDB_VERSION "v1.3.0-dev3448"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "55da0974ac"
+#define DUCKDB_SOURCE_ID "d4d722c1c9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
