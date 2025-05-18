97c3baa379352254896908666fca3ce6bfb5cae2
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:02:44 +0200

    vendor: Update vendored sources to duckdb/duckdb@93a9d9f9b58341c3d8d72c259cddb9696a77468a
    Changing mbedtls encryption API (duckdb/duckdb#16196)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2422"
+#define DUCKDB_PATCH_VERSION "0-dev2454"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2422"
+#define DUCKDB_VERSION "v1.3.0-dev2454"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c4436d568e"
+#define DUCKDB_SOURCE_ID "93a9d9f9b5"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
