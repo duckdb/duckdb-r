94660becf0c153706c9d27148074cda1a789cf12
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:10:19 +0200

    vendor: Update vendored sources to duckdb/duckdb@ef4af3a1cbe3f2c88f27ac7cfd5b0e99724f05e5
    Add root's query_location also to TransformInterval (duckdb/duckdb#17271)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2814"
+#define DUCKDB_PATCH_VERSION "0-dev2817"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2814"
+#define DUCKDB_VERSION "v1.3.0-dev2817"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "bcf55a5c8c"
+#define DUCKDB_SOURCE_ID "ef4af3a1cb"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
