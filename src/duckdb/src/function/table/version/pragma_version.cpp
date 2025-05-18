b55055b12244523142a4151c01ce17a83ef24963
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:05:05 +0200

    vendor: Update vendored sources to duckdb/duckdb@b99d7fda5854e65c1fbb5f77f5b296bdda39ce62
    Optimize large Top N queries (duckdb/duckdb#17141)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2587"
+#define DUCKDB_PATCH_VERSION "0-dev2594"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev2587"
+#define DUCKDB_VERSION "v1.3.0-dev2594"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ad5b8d6add"
+#define DUCKDB_SOURCE_ID "b99d7fda58"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
