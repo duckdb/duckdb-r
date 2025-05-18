0a3d17cbe6c7af21529da5a509bf7845c95957ab
Kirill Müller <kirill@cynkra.com>
2025-05-18 23:19:25 +0200

    vendor: Update vendored sources to duckdb/duckdb@8caf5e78b961b55f204819fb74c585499077ec96
    Issue duckdb/duckdb#17266: Lead Lag Nulls  (duckdb/duckdb#17391)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3215"
+#define DUCKDB_PATCH_VERSION "0-dev3221"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3215"
+#define DUCKDB_VERSION "v1.3.0-dev3221"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ff3a129f34"
+#define DUCKDB_SOURCE_ID "8caf5e78b9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
