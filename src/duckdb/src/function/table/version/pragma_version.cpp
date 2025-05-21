5f51cda2098713998c25e555ea1f7b24829caf52
Kirill Müller <kirill@cynkra.com>
2025-05-21 19:38:17 +0200

    vendor: Update vendored sources to duckdb/duckdb@b501e8b1f61bcc6c395d8b8219ae45c5f37a0ecb
    [Dev] Fix Arrow fixed size binary reading (duckdb/duckdb#17573)
    Remove patch and bump httpfs (duckdb/duckdb#17558)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3675"
+#define DUCKDB_PATCH_VERSION "0-dev3682"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3675"
+#define DUCKDB_VERSION "v1.3.0-dev3682"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "eda01835de"
+#define DUCKDB_SOURCE_ID "b501e8b1f6"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
