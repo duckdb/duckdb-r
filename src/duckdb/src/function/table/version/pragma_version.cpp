78fa976483ab414a023ffbe3ee65dd29d82c648f
Kirill Müller <kirill@cynkra.com>
2025-05-21 19:38:20 +0200

    vendor: Update vendored sources (tag v1.3.0) to duckdb/duckdb@71c5c07cdd295e9409c0505885033ae9eb6b5ddd
    Fix setup.py to correctly handle OVERRIDE_GIT_DESCRIBE (duckdb/duckdb#17580)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3682"
+#define DUCKDB_PATCH_VERSION "0"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3682"
+#define DUCKDB_VERSION "v1.3.0"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b501e8b1f6"
+#define DUCKDB_SOURCE_ID "71c5c07cdd"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
