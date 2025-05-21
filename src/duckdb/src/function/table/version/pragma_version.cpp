9195f56bb7e217e8b84bac39fe64886290f66154
Kirill Müller <kirill@cynkra.com>
2025-05-21 19:37:14 +0200

    vendor: Update vendored sources to duckdb/duckdb@4075e3394fb1c412cc5908d9fb9c210da7b7a417
    Minor fixes to exception error messages (duckdb/duckdb#17528)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3618"
+#define DUCKDB_PATCH_VERSION "0-dev3621"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3618"
+#define DUCKDB_VERSION "v1.3.0-dev3621"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "3c8f023bf6"
+#define DUCKDB_SOURCE_ID "4075e3394f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
