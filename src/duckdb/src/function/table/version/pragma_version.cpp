27d589d5ee4ba3f502c31f89cbea12f0e585e106
Kirill Müller <kirill@cynkra.com>
2025-06-02 16:06:44 +0200

    vendor: Update vendored sources to duckdb/duckdb@4e0a953228ab377e47fa3a59583d0fd3579ad325
    Bugfixes (duckdb/duckdb#17695)
    Fix version detection for sdist builds without git info (duckdb/duckdb#17605)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev60"
+#define DUCKDB_PATCH_VERSION "1-dev65"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.1-dev60"
+#define DUCKDB_VERSION "v1.3.1-dev65"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a2e752670d"
+#define DUCKDB_SOURCE_ID "4e0a953228"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
