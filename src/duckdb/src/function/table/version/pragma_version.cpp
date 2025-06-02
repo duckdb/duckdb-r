02689889da1f17b6788cee5cec96a4c6f39a76f2
Kirill Müller <kirill@cynkra.com>
2025-06-02 16:04:48 +0200

    vendor: Update vendored sources to duckdb/duckdb@406888577e0e56a7d5784a189bca73f87e2ea9c2
    MultiFileReader: Fix for handling nested list/map default values (duckdb/duckdb#17589)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,17 +1,17 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0"
+#define DUCKDB_PATCH_VERSION "0-dev5"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
-#define DUCKDB_MINOR_VERSION 3
+#define DUCKDB_MINOR_VERSION 4
 #endif
 #ifndef DUCKDB_MAJOR_VERSION
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0"
+#define DUCKDB_VERSION "v1.4.0-dev5"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "71c5c07cdd"
+#define DUCKDB_SOURCE_ID "406888577e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
