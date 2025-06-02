9ced60b10b41dfd888e35bc0f83dd3c501ad09fd
Kirill Müller <kirill@cynkra.com>
2025-06-02 16:06:11 +0200

    vendor: Update vendored sources to duckdb/duckdb@ca7af7ffdcc86b3cd91606c1e634e7cbee601e38
    Don't bail on TopN optimization if we don't have a cardinality (duckdb/duckdb#17654)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev48"
+#define DUCKDB_PATCH_VERSION "1-dev51"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.1-dev48"
+#define DUCKDB_VERSION "v1.3.1-dev51"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "243eb89447"
+#define DUCKDB_SOURCE_ID "ca7af7ffdc"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
