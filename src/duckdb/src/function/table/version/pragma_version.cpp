053ae09ea5b10f697905afebd9147cf5c9fdb385
Kirill Müller <kirill@cynkra.com>
2025-06-02 16:05:50 +0200

    vendor: Update vendored sources to duckdb/duckdb@199b61b7db984efa5bb4b75f387800761b674f91
    [Dev] Throw if `db` is not available yet in setting certain configuration options (duckdb/duckdb#17659)
    [Python][Dev] Ignore `DYNAMIC_FILTER` TableFilters in filter pushdown (duckdb/duckdb#17657)
    CLI: Print codename for '-version' (duckdb/duckdb#17637)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev34"
+#define DUCKDB_PATCH_VERSION "1-dev40"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.1-dev34"
+#define DUCKDB_VERSION "v1.3.1-dev40"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "66c2f1e558"
+#define DUCKDB_SOURCE_ID "199b61b7db"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
