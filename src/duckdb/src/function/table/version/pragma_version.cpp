207774507ede45b0d24d84be984edb95b02bf550
Kirill Müller <kirill@cynkra.com>
2025-06-02 16:05:34 +0200

    vendor: Update vendored sources to duckdb/duckdb@66c2f1e558dfe5ceb032fab809532a3378f791ec
    Issue duckdb/duckdb#17621: Streaming Window Reset (duckdb/duckdb#17649)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev30"
+#define DUCKDB_PATCH_VERSION "1-dev34"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.1-dev30"
+#define DUCKDB_VERSION "v1.3.1-dev34"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ae17830a85"
+#define DUCKDB_SOURCE_ID "66c2f1e558"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
