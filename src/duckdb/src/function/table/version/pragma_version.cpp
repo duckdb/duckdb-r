33f576b050e3aa53b325f5706d6eada6759e5989
Kirill Müller <kirill@cynkra.com>
2025-06-02 16:06:48 +0200

    vendor: Update vendored sources to duckdb/duckdb@95cc51601b441b884ec98a7e5114a8f3ae525c33
    Do not get file handle unnecessarily.  (duckdb/duckdb#17698)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev65"
+#define DUCKDB_PATCH_VERSION "1-dev67"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.1-dev65"
+#define DUCKDB_VERSION "v1.3.1-dev67"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4e0a953228"
+#define DUCKDB_SOURCE_ID "95cc51601b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
