8996ee778772e0740899acd2ba303eba4d5f7d5c
Kirill Müller <kirill@cynkra.com>
2025-06-02 16:05:28 +0200

    vendor: Update vendored sources to duckdb/duckdb@ae17830a858358329a0dc9849ddda7834ae193d0
    Actually initialize in batch copy to file (duckdb/duckdb#17627)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev28"
+#define DUCKDB_PATCH_VERSION "1-dev30"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.1-dev28"
+#define DUCKDB_VERSION "v1.3.1-dev30"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "bbf0c28eac"
+#define DUCKDB_SOURCE_ID "ae17830a85"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
