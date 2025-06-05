34a2f4e8ce2f536e3bb3086d20ffcff367636c12
Kirill Müller <kirill@cynkra.com>
2025-06-05 07:39:57 +0200

    vendor: Update vendored sources to duckdb/duckdb@2c3480d8d84bbcc1c17c5f59e101c4eb0fcf731b
    C API tidying (duckdb/duckdb#17623)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3653"
+#define DUCKDB_PATCH_VERSION "0-dev3655"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3653"
+#define DUCKDB_VERSION "v1.3.0-dev3655"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1f8b6839ea"
+#define DUCKDB_SOURCE_ID "2c3480d8d8"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
