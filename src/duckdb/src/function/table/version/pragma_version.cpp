2c6fa8f6e6ed1daf4afd627772589ad8f299b65f
Kirill Müller <kirill@cynkra.com>
2025-06-05 07:41:08 +0200

    vendor: Update vendored sources to duckdb/duckdb@ad27205412dcb0e4742ba92ed5c3237823d6452a
    Merge v1.3-ossivalis into main (duckdb/duckdb#17690)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3746"
+#define DUCKDB_PATCH_VERSION "1-dev193"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.0-dev3746"
+#define DUCKDB_VERSION "v1.3.1-dev193"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "47599043f0"
+#define DUCKDB_SOURCE_ID "ad27205412"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
