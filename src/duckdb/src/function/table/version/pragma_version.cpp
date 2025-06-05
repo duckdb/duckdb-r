2559e33dadea879324890a8bffaa78c6a8e74f45
krlmlr <krlmlr@users.noreply.github.com>
2025-06-05 15:07:45 +0000

    vendor: Update vendored sources to duckdb/duckdb@fe3c08f299ab446b6e9b2fbb1290404e2269a2c8
    Check page filtered out flag before reading it (duckdb/duckdb#17786)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev140"
+#define DUCKDB_PATCH_VERSION "1-dev142"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.1-dev140"
+#define DUCKDB_VERSION "v1.3.1-dev142"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a8d649362c"
+#define DUCKDB_SOURCE_ID "fe3c08f299"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
