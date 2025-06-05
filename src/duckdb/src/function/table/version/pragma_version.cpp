c4c0b30b859ea7669857abe690affcd2b69b9ea1
krlmlr <krlmlr@users.noreply.github.com>
2025-06-05 06:56:30 +0000

    vendor: Update vendored sources to duckdb/duckdb@d0fe0698f44f644a1d55477fe1da036e3eb91d86
    Fix wrong assertion in Parquet DBP encoder (duckdb/duckdb#17746)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev92"
+#define DUCKDB_PATCH_VERSION "1-dev95"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.1-dev92"
+#define DUCKDB_VERSION "v1.3.1-dev95"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ff7ad850eb"
+#define DUCKDB_SOURCE_ID "d0fe0698f4"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
