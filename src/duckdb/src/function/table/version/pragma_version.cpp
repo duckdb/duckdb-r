47c41e23b79593ed16c3ac376dac675da43dac1b
krlmlr <krlmlr@users.noreply.github.com>
2025-06-05 11:30:29 +0000

    vendor: Update vendored sources to duckdb/duckdb@d81605edd39a308ab02bce50177ed82bbeb38b64
    Take string size into account in `GetRowSize` in `ParquetWriter` (duckdb/duckdb#17793)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev126"
+#define DUCKDB_PATCH_VERSION "1-dev128"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.1-dev126"
+#define DUCKDB_VERSION "v1.3.1-dev128"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "843ea85218"
+#define DUCKDB_SOURCE_ID "d81605edd3"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
