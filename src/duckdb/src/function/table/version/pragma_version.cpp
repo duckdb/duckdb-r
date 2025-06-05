f612e9944f72ed40e9efd31c0d8f1264a12e1dfa
krlmlr <krlmlr@users.noreply.github.com>
2025-06-05 05:16:17 +0000

    vendor: Update vendored sources to duckdb/duckdb@e1d3536dc946b84e1a3f8a649506303a23ced241
    Issue duckdb/duckdb#17725: Quantile NaN Compare (duckdb/duckdb#17761)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev79"
+#define DUCKDB_PATCH_VERSION "1-dev81"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.1-dev79"
+#define DUCKDB_VERSION "v1.3.1-dev81"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "0de6960b0e"
+#define DUCKDB_SOURCE_ID "e1d3536dc9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
