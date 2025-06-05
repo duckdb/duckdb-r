cf6ec3b53b0057580df559c4a457784fb2337e5a
krlmlr <krlmlr@users.noreply.github.com>
2025-06-05 17:23:09 +0000

    vendor: Update vendored sources to duckdb/duckdb@6d6324a1bdce93aeadb6c5ada8031a4859e2bf4d
    arrow_output_version option to produce arrow depending on a format version. (duckdb/duckdb#17791)
    Support glibc 2.28 environments in 1.3.x (duckdb/duckdb#17814)
    Avoid saving ccache on pull_requests (duckdb/duckdb#17810)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev142"
+#define DUCKDB_PATCH_VERSION "1-dev155"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.1-dev142"
+#define DUCKDB_VERSION "v1.3.1-dev155"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "fe3c08f299"
+#define DUCKDB_SOURCE_ID "6d6324a1bd"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
