ddb55a5929ab2a7106ca3ec7d1210fda5c19ba09
krlmlr <krlmlr@users.noreply.github.com>
2025-06-05 10:58:02 +0000

    vendor: Update vendored sources to duckdb/duckdb@32009cdbaa8e32a01fc9ad083a375e60885a85f6
    Adding additional authenticated data for encryption (duckdb/duckdb#17508)
    [SQLLogicTester] Introduce `reset label <query label>` in the tester (duckdb/duckdb#17729)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev235"
+#define DUCKDB_PATCH_VERSION "0-dev241"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev235"
+#define DUCKDB_VERSION "v1.4.0-dev241"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "2e7b62dada"
+#define DUCKDB_SOURCE_ID "32009cdbaa"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
