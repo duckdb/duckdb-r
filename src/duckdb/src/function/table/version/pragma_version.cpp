425ccf493fce09aa0735cdedbdc0dc9ec0f99cbf
krlmlr <krlmlr@users.noreply.github.com>
2025-06-13 20:04:49 +0000

    vendor: Update vendored sources to duckdb/duckdb@db3f895ce83dd731f333cebc6e3f23a94f35f1c7
    Add v1.3.1 to version_map.json (duckdb/duckdb#17922)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev256"
+#define DUCKDB_PATCH_VERSION "1-dev259"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.1-dev256"
+#define DUCKDB_VERSION "v1.3.1-dev259"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "07661de910"
+#define DUCKDB_SOURCE_ID "db3f895ce8"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
