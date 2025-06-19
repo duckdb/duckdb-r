f894c52e0418f93153151f581b4e412215e3002c
Kirill Müller <kirill@cynkra.com>
2025-06-19 09:37:54 +0200

    Strip

--- tests/testthat/test-config_keyvalue.R
+++ tests/testthat/test-config_keyvalue.R
@@ -1,4 +1,13 @@
 test_that("configuration key value pairs work as expected", {
-  # but we should throw an error on non-existent options
-  expect_error(duckdb(config = list(a = "a")))
+  # but setting a legal option is fine
+  drv <- duckdb(config = list("default_order" = "DESC"))
+
+  # the option actually does something
+  con <- dbConnect(drv)
+  dbExecute(con, "create table a (i integer)")
+  dbExecute(con, "insert into a values (44), (42)")
+  res <- dbGetQuery(con, "select i from a order by i")
+  dbDisconnect(con)
+  expect_equal(res$i, c(44, 42))
+  duckdb_shutdown(drv)
 })
