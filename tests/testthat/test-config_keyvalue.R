6dc42cfa32edba064ca8d637fb52ca4fda7e37b9
Kirill Müller <kirill@cynkra.com>
2025-06-19 06:46:10 +0200

    Strip

--- tests/testthat/test-config_keyvalue.R
+++ tests/testthat/test-config_keyvalue.R
@@ -2,7 +2,4 @@ test_that("configuration key value pairs work as expected", {
   # setting nothing or empty list should work
   drv <- duckdb()
   duckdb_shutdown(drv)
-
-  drv <- duckdb(config = list())
-  duckdb_shutdown(drv)
 })
