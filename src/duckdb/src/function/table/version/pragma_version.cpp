35c823acf9b9d02c36667c60667fe9a28faea2f0
krlmlr <krlmlr@users.noreply.github.com>
2025-06-19 15:19:25 +0000

    vendor: Update vendored sources to duckdb/duckdb@e4f6023b219c33382c5be14caa31a08c8ddb7809
    Grab lock before finalizing dynamic filters (duckdb/duckdb#17964)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev8"
+#define DUCKDB_PATCH_VERSION "2-dev11"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.2-dev8"
+#define DUCKDB_VERSION "v1.3.2-dev11"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "8c40a99d4d"
+#define DUCKDB_SOURCE_ID "e4f6023b21"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
