1fa605862a8d02886143250eabf7448d11850e2b
Kirill Müller <kirill@cynkra.com>
2025-06-19 06:45:33 +0200

    Strip

--- tests/testthat/test-config_keyvalue.R
+++ tests/testthat/test-config_keyvalue.R
@@ -23,5 +23,4 @@ test_that("configuration key value pairs work as expected", {
 
   # setting a configuration option to a non-string is an error
   expect_error(duckdb(config = list("default_order" = 42)))
-  expect_error(duckdb(config = list("default_order" = c("a", "b"))))
 })
