fd18758c0609d62184f22e39049a21bf48d86e9f
krlmlr <krlmlr@users.noreply.github.com>
2025-06-20 21:04:07 +0000

    vendor: Update vendored sources to duckdb/duckdb@550eb857c76b621116864196c5eab4e61e903195
    [Profiling] Propagate the ClientContext into file handle writes (duckdb/duckdb#17754)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev304"
+#define DUCKDB_PATCH_VERSION "0-dev312"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev304"
+#define DUCKDB_VERSION "v1.4.0-dev312"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e99cf223af"
+#define DUCKDB_SOURCE_ID "550eb857c7"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
