7ddb0434e83227ead481fe28cca6eb9c3e55069a
krlmlr <krlmlr@users.noreply.github.com>
2025-06-21 00:17:07 +0000

    vendor: Update vendored sources to duckdb/duckdb@f85436e220b8257279f4598b1089ba92c053ce0f
    Merge v1.3 into main (duckdb/duckdb#17806)

--- src/duckdb/ub_src_logging.cpp
+++ src/duckdb/ub_src_logging.cpp
@@ -1,10 +1,10 @@
-#include "src/logging/logger.cpp"
-
 #include "src/logging/log_manager.cpp"
 
 #include "src/logging/log_storage.cpp"
 
-#include "src/logging/logging.cpp"
-
 #include "src/logging/log_types.cpp"
+
+#include "src/logging/logger.cpp"
+
+#include "src/logging/logging.cpp"
 
