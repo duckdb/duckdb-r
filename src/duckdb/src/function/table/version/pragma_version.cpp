72b8a75a8799e97784caa254d2758d85f961f7fd
krlmlr <krlmlr@users.noreply.github.com>
2025-06-21 19:03:35 +0000

    vendor: Update vendored sources to duckdb/duckdb@fd1b726c1cea7ef33ca2334bc19a6f76cb317bb1
    Leverage `VectorType` in `ColumnDataCollection` (duckdb/duckdb#17881)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev635"
+#define DUCKDB_PATCH_VERSION "0-dev642"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev635"
+#define DUCKDB_VERSION "v1.4.0-dev642"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "32f2206ceb"
+#define DUCKDB_SOURCE_ID "fd1b726c1c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
