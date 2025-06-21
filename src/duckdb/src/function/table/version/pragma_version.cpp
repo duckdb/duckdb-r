931934da22edf8b8e276974873e8de105939d25a
krlmlr <krlmlr@users.noreply.github.com>
2025-06-21 12:06:07 +0000

    vendor: Update vendored sources to duckdb/duckdb@5a997783350ab4ee617d4e07fd154f28727d949a
    Add STRUCT to MAP cast function (duckdb/duckdb#17799)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev500"
+#define DUCKDB_PATCH_VERSION "0-dev504"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev500"
+#define DUCKDB_VERSION "v1.4.0-dev504"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "405e5799df"
+#define DUCKDB_SOURCE_ID "5a99778335"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
