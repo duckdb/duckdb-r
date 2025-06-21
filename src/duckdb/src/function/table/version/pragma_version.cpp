65a3e4422ee549d7bb1cb93fc6c21b7d30a6266e
krlmlr <krlmlr@users.noreply.github.com>
2025-06-21 17:03:46 +0000

    vendor: Update vendored sources to duckdb/duckdb@e00dfa4b49fbd2374efe5fe703b9fa643cf78500
    Make CTE Materialization the Default Instead of Inlining (duckdb/duckdb#17459)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev542"
+#define DUCKDB_PATCH_VERSION "0-dev586"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev542"
+#define DUCKDB_VERSION "v1.4.0-dev586"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "94324054e6"
+#define DUCKDB_SOURCE_ID "e00dfa4b49"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
