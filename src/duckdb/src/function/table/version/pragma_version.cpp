56fff54dd79ee9251ebe0acf1ac4d2034d60a345
krlmlr <krlmlr@users.noreply.github.com>
2025-06-22 07:04:44 +0000

    vendor: Update vendored sources to duckdb/duckdb@8be6ed418aff9e8b2c6a5faa549e93e142adf297
    Issue duckdb/duckdb#5123: make_timestamp_ms (duckdb/duckdb#17908)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev846"
+#define DUCKDB_PATCH_VERSION "0-dev854"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev846"
+#define DUCKDB_VERSION "v1.4.0-dev854"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a600bca9b8"
+#define DUCKDB_SOURCE_ID "8be6ed418a"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
