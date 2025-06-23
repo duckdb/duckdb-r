362f91ed2801c6ca154ad4c5478976ce43f14e2a
krlmlr <krlmlr@users.noreply.github.com>
2025-06-23 12:56:54 +0000

    vendor: Update vendored sources to duckdb/duckdb@522c28af3ea597a70ba9cbb76bb13091f18d159c
    Implement consumption and production of Arrow Binary View (duckdb/duckdb#17975)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev861"
+#define DUCKDB_PATCH_VERSION "0-dev870"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev861"
+#define DUCKDB_VERSION "v1.4.0-dev870"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "0cbb56d14c"
+#define DUCKDB_SOURCE_ID "522c28af3e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
