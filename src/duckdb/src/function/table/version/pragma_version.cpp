843bd2ba14a41f549166172e5f74ad8f71a9d014
krlmlr <krlmlr@users.noreply.github.com>
2025-06-28 00:16:36 +0000

    vendor: Update vendored sources to duckdb/duckdb@24bb8debdf99bbae48829f8b6b31eae15bbe7709
    Implement bulk enqueue for non-concurrent queue (duckdb/duckdb#18032)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev43"
+#define DUCKDB_PATCH_VERSION "2-dev46"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.2-dev43"
+#define DUCKDB_VERSION "v1.3.2-dev46"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "3cb90b8994"
+#define DUCKDB_SOURCE_ID "24bb8debdf"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
