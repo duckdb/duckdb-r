f1e3d35c9d0f4c08f5d725d7ec2b7b55d3c9b55e
krlmlr <krlmlr@users.noreply.github.com>
2025-06-28 01:41:45 +0000

    vendor: Update vendored sources to duckdb/duckdb@8147b4391571d7d50609a9666e5d461ae42d513b
    Issue duckdb/duckdb#18035: Zero Fill TIMESTAMP_NS (duckdb/duckdb#18045)
    Bring back libduckdb-src.zip as release artifact (duckdb/duckdb#18019)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev46"
+#define DUCKDB_PATCH_VERSION "2-dev51"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.2-dev46"
+#define DUCKDB_VERSION "v1.3.2-dev51"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "24bb8debdf"
+#define DUCKDB_SOURCE_ID "8147b43915"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
