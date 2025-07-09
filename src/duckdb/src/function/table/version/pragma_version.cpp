d71fd8d32c7da2e5aac0bdee27871eb137377584
Kirill Müller <kirill@cynkra.com>
2025-07-09 08:06:07 +0200

    vendor: Update vendored sources to duckdb/duckdb@3d5f3d0ed163efe29a0e3c092f30631ffb5a5d2a
    Improve sort key comparison performance (duckdb/duckdb#18131)
    set ::error:: annotations for test runners (duckdb/duckdb#18072)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1254"
+#define DUCKDB_PATCH_VERSION "0-dev1267"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev1254"
+#define DUCKDB_VERSION "v1.4.0-dev1267"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "bc4e384cea"
+#define DUCKDB_SOURCE_ID "3d5f3d0ed1"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
