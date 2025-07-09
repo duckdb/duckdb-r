9f6f7a253859c63059eede2fbb97e01ec7d821dc
Kirill Müller <kirill@cynkra.com>
2025-07-09 08:04:19 +0200

    vendor: Update vendored sources to duckdb/duckdb@5175b7d33c7f1fe8852ffc6eb2a61bfd76928803
    Merge ossivalis into main (duckdb/duckdb#18036)
    Add ppc64le spin-wait instruction (duckdb/duckdb#17837)
    Unittest: Add skip_compiled option that can be used to skip built-in C++ tests (duckdb/duckdb#18034)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev914"
+#define DUCKDB_PATCH_VERSION "0-dev1023"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev914"
+#define DUCKDB_VERSION "v1.4.0-dev1023"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "894822942c"
+#define DUCKDB_SOURCE_ID "5175b7d33c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
