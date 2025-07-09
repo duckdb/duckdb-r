39428034affd1f87866bf50b484703ea2b5737a2
Kirill Müller <kirill@cynkra.com>
2025-07-09 07:38:27 +0200

    vendor: Update vendored sources to duckdb/duckdb@8e92841aa0e9351e17d4e1988e56df5e459f3b41
    bump spatial (duckdb/duckdb#18161)
    Bump for wasm fixes (excel and httpfs) and test fixes (iceberg) (duckdb/duckdb#18167)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev141"
+#define DUCKDB_PATCH_VERSION "2-dev147"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.2-dev141"
+#define DUCKDB_VERSION "v1.3.2-dev147"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b9b1e449fe"
+#define DUCKDB_SOURCE_ID "8e92841aa0"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
