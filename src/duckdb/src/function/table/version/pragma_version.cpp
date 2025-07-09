a0abfdad983ea54ed4ec04e134965f4d7a95d2f9
Kirill Müller <kirill@cynkra.com>
2025-07-09 08:06:39 +0200

    vendor: Update vendored sources to duckdb/duckdb@c677fa112c5597c16cd83ed24739129696d38d53
    Reduce lock contention for the instance cache (duckdb/duckdb#18079)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1337"
+#define DUCKDB_PATCH_VERSION "0-dev1354"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev1337"
+#define DUCKDB_VERSION "v1.4.0-dev1354"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "97d22c2a22"
+#define DUCKDB_SOURCE_ID "c677fa112c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
