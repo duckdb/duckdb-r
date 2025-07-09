fb7c19b64aede77e4247e0582e87ee81f60f2d18
Kirill Müller <kirill@cynkra.com>
2025-07-09 07:37:47 +0200

    vendor: Update vendored sources to duckdb/duckdb@98066e1a3d9091403400df47560f0fdebb78309e
    Add v1.3.2 to version_map.json and generate storage_info.cpp (duckdb/duckdb#18112)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev104"
+#define DUCKDB_PATCH_VERSION "2-dev106"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.2-dev104"
+#define DUCKDB_VERSION "v1.3.2-dev106"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f5903393c1"
+#define DUCKDB_SOURCE_ID "98066e1a3d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
