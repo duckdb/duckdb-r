26b801793275f0d4a6fac2d386998f6808c9e7f0
Kirill Müller <kirill@cynkra.com>
2025-07-09 07:38:00 +0200

    vendor: Update vendored sources to duckdb/duckdb@dc5dc2b115e46684d31cdb9790ad4e063c9c2a6d
    Allow .tsv as an accepted db file (duckdb/duckdb#18133)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev114"
+#define DUCKDB_PATCH_VERSION "2-dev116"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.2-dev114"
+#define DUCKDB_VERSION "v1.3.2-dev116"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "59a35ee41f"
+#define DUCKDB_SOURCE_ID "dc5dc2b115"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
