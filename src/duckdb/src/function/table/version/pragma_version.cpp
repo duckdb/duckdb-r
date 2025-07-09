414dc0c3a3f36f4f29745f6dc04e187a34f09184
Kirill Müller <kirill@cynkra.com>
2025-07-09 08:05:45 +0200

    vendor: Update vendored sources to duckdb/duckdb@b398a30f4e0bd5993953f7ea9c0f224aef144936
    Fix  correlated subquery unnest fail (duckdb/duckdb#18092)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1167"
+#define DUCKDB_PATCH_VERSION "0-dev1169"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev1167"
+#define DUCKDB_VERSION "v1.4.0-dev1169"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "2b6f68a5d0"
+#define DUCKDB_SOURCE_ID "b398a30f4e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
