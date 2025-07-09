e8400dcf4da02fe93b570eb0ec6c051a584e48af
Kirill Müller <kirill@cynkra.com>
2025-07-09 07:37:43 +0200

    vendor: Update vendored sources to duckdb/duckdb@f5903393c1d99284a3906fb1997720000000486d
    Refactor extracting expressions for dynamic index scans (duckdb/duckdb#18095)
    Cleanup on correct branch (1.3-ossivalis) instead of v1.2-histrionicus (duckdb/duckdb#18111)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev100"
+#define DUCKDB_PATCH_VERSION "2-dev104"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.2-dev100"
+#define DUCKDB_VERSION "v1.3.2-dev104"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "03a2ddf09e"
+#define DUCKDB_SOURCE_ID "f5903393c1"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
