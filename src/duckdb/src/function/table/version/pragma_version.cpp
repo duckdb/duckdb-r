9461531f439b59390885a982e0e22b7c85c750c5
krlmlr <krlmlr@users.noreply.github.com>
2025-07-21 16:04:52 +0000

    vendor: Update vendored sources to duckdb/duckdb@35391cb7f32572e45fd202513af4a1a63ae9daa3
    [Fix] Block verification for add and drop field info (duckdb/duckdb#18302)
    Python external dispatch param fixes (duckdb/duckdb#18343)
    Backport duckdb/duckdb#18254 (duckdb/duckdb#18306)
    Skip test/sql/copy/s3/url_encode.test due to httpfs update (duckdb/duckdb#18317)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev88"
+#define DUCKDB_PATCH_VERSION "3-dev101"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.3-dev88"
+#define DUCKDB_VERSION "v1.3.3-dev101"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "473fd24cf2"
+#define DUCKDB_SOURCE_ID "35391cb7f3"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
