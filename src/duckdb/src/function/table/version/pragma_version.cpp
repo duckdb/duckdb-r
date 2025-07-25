b3640e9a2386f95c0072676a52968b509490d2b7
krlmlr <krlmlr@users.noreply.github.com>
2025-07-25 03:32:16 +0000

    vendor: Update vendored sources to duckdb/duckdb@102731c0331f1a248092bb18fc11e3640bbd00e2
    Dictionary functions (duckdb/duckdb#18127)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1587"
+#define DUCKDB_PATCH_VERSION "0-dev1611"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev1587"
+#define DUCKDB_VERSION "v1.4.0-dev1611"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1b6c85a228"
+#define DUCKDB_SOURCE_ID "102731c033"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
