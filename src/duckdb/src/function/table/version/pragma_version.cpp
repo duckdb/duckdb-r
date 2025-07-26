114a47c1f0998dac0bbbb651f9d50ec453518b4a
Kirill Müller <kirill@cynkra.com>
2025-07-26 10:31:31 +0200

    vendor: Update vendored sources to duckdb/duckdb@31e9e29125305bb18c90d975738d336019e0e57b
    feat: making Parquet write RowGroup.total_compressed_size (duckdb/duckdb#18307)
    Make storage-version a test parameter (duckdb/duckdb#18324)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1877"
+#define DUCKDB_PATCH_VERSION "0-dev1881"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev1877"
+#define DUCKDB_VERSION "v1.4.0-dev1881"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e958b053a8"
+#define DUCKDB_SOURCE_ID "31e9e29125"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
