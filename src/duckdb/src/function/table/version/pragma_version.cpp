8c9e3647fd552872816d971ab8b85752d6c09d0e
Kirill Müller <kirill@cynkra.com>
2025-07-26 10:33:24 +0200

    vendor: Update vendored sources to duckdb/duckdb@1c928021f3e1c610028371e43b426d0aa2e58828
    Remove incorrect assertion (duckdb/duckdb#18404)
    [ Python SQLLogic Tester ] Add `MERGE_INTO` statement to duckdb python (duckdb/duckdb#18402)
    CI: Add separate job for discussion mirroring (duckdb/duckdb#18407)
    Wrap runner.ExecuteFile, otherwise cleanup is not properly performed (duckdb/duckdb#18400)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2246"
+#define DUCKDB_PATCH_VERSION "0-dev2256"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2246"
+#define DUCKDB_VERSION "v1.4.0-dev2256"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "77abe23a68"
+#define DUCKDB_SOURCE_ID "1c928021f3"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
