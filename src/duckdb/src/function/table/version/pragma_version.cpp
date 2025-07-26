cf03dc3673195d8513d06d59ab917c4ec63fcb82
Kirill Müller <kirill@cynkra.com>
2025-07-26 10:32:11 +0200

    vendor: Update vendored sources to duckdb/duckdb@7e7e1dae5d6b94aaab033d4c5bde6101d9cbb92c
    Pass `AttachOptions` to `attach` method, and turn `StorageExtensionInfo` into an `optional_ptr` (duckdb/duckdb#18368)
    Split up out-of-tree extensions into separate files, and allow out-of-tree extensions to be built using BUILD_EXTENSIONS={ext_name} (duckdb/duckdb#18357)
    Python external dispatch param fixes (duckdb/duckdb#18359)
    Revert "[unittest] - fix doubled error headers on `Unexpected failure`" (duckdb/duckdb#18355)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2079"
+#define DUCKDB_PATCH_VERSION "0-dev2092"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2079"
+#define DUCKDB_VERSION "v1.4.0-dev2092"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ed126743a2"
+#define DUCKDB_SOURCE_ID "7e7e1dae5d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
