0a75883d905c376caa7b9e758cc8a0be261a1505
Kirill Müller <kirill@cynkra.com>
2025-07-26 10:30:36 +0200

    vendor: Update vendored sources to duckdb/duckdb@6d74aa8e5f51d53c8c656d60e17d5c4f6c758663
    Expanded autocomplete suggestions (duckdb/duckdb#18243)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1695"
+#define DUCKDB_PATCH_VERSION "0-dev1713"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev1695"
+#define DUCKDB_VERSION "v1.4.0-dev1713"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d7f955fd3c"
+#define DUCKDB_SOURCE_ID "6d74aa8e5f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
