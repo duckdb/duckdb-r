5ae87b63abdf169cdfb6f714005db756d48cb0df
Kirill Müller <kirill@cynkra.com>
2025-07-26 10:30:54 +0200

    vendor: Update vendored sources to duckdb/duckdb@a6df1bbc8e41f7d4dc4c221662b5101d0954f541
    Use DuckDB cast infrastructure in fmt for new uhugeint/hugeint code (duckdb/duckdb#18275)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1727"
+#define DUCKDB_PATCH_VERSION "0-dev1732"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev1727"
+#define DUCKDB_VERSION "v1.4.0-dev1732"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "92a6f1e4d4"
+#define DUCKDB_SOURCE_ID "a6df1bbc8e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
