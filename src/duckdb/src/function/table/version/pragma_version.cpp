4690c4cadaf46caf84fe72ea1cb407da61ac76d1
Kirill Müller <kirill@cynkra.com>
2025-07-26 10:33:35 +0200

    vendor: Update vendored sources to duckdb/duckdb@3de8d76ba83a82fb4337ca7cb8e4de2eab748561
    Add DuckLake back in (duckdb/duckdb#18405)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2260"
+#define DUCKDB_PATCH_VERSION "0-dev2263"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2260"
+#define DUCKDB_VERSION "v1.4.0-dev2263"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c1062e494b"
+#define DUCKDB_SOURCE_ID "3de8d76ba8"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
