ff312d1fa919a6152a3492174dc211db1347882a
Kirill Müller <kirill@cynkra.com>
2025-07-26 10:31:35 +0200

    vendor: Update vendored sources to duckdb/duckdb@61f07c3221e01674d8fe40b4a25364ba2f3159a7
    add the from-table-function as parameter to copy-from-bind (duckdb/duckdb#18004)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1881"
+#define DUCKDB_PATCH_VERSION "0-dev1890"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev1881"
+#define DUCKDB_VERSION "v1.4.0-dev1890"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "31e9e29125"
+#define DUCKDB_SOURCE_ID "61f07c3221"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
