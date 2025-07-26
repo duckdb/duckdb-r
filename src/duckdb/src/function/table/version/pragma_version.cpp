64fcb8eeefffa37bddfaa6e85b7c0618474afd4e
Kirill Müller <kirill@cynkra.com>
2025-07-26 10:30:45 +0200

    vendor: Update vendored sources to duckdb/duckdb@3fbd759cff938b65c02ca22d806af5d02ed99957
    Add support for RETURNING to MERGE INTO (duckdb/duckdb#18271)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1719"
+#define DUCKDB_PATCH_VERSION "0-dev1723"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev1719"
+#define DUCKDB_VERSION "v1.4.0-dev1723"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f5c4098dc3"
+#define DUCKDB_SOURCE_ID "3fbd759cff"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
