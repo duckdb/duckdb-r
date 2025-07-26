ed5fb99592724539c7991c6b1a950d09e6fe684a
Kirill Müller <kirill@cynkra.com>
2025-07-26 10:30:49 +0200

    vendor: Update vendored sources to duckdb/duckdb@92a6f1e4d4ea04790a22a5ab3e07be534526f250
    Use set for row ID scanning during index scans (duckdb/duckdb#18274)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1723"
+#define DUCKDB_PATCH_VERSION "0-dev1727"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev1723"
+#define DUCKDB_VERSION "v1.4.0-dev1727"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "3fbd759cff"
+#define DUCKDB_SOURCE_ID "92a6f1e4d4"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
