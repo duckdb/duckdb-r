a68c093dda982311e784c23b7b236b942166293a
Kirill Müller <kirill@cynkra.com>
2025-07-26 10:27:41 +0200

    vendor: Update vendored sources to duckdb/duckdb@fc8e22066a7ff8f6aa4e138e22df9dfb0f8acc7e
    Fixes for gcc 15 (duckdb/duckdb#18261)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1659"
+#define DUCKDB_PATCH_VERSION "0-dev1661"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev1659"
+#define DUCKDB_VERSION "v1.4.0-dev1661"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4e4b421a41"
+#define DUCKDB_SOURCE_ID "fc8e22066a"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
