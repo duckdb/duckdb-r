cee7a382f39a72e3e31b96c9cbc5eb05b92b43be
krlmlr <krlmlr@users.noreply.github.com>
2025-07-28 14:06:13 +0000

    vendor: Update vendored sources to duckdb/duckdb@ac98720e11fe1194375d996c957e5837affbfb68
    Correctly fetch only base column data in ColumnData::FetchUpdateData (duckdb/duckdb#18423)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2291"
+#define DUCKDB_PATCH_VERSION "0-dev2293"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2291"
+#define DUCKDB_VERSION "v1.4.0-dev2293"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "101ff9df5d"
+#define DUCKDB_SOURCE_ID "ac98720e11"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
