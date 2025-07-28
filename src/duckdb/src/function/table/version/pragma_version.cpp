511f6751ac954620d183904c7e72a440226dd8c0
krlmlr <krlmlr@users.noreply.github.com>
2025-07-28 08:06:30 +0000

    vendor: Update vendored sources to duckdb/duckdb@6955f9d77f44b34beaf5734c12b66c94d88193b3
    Add support for table_constraints of AdbcConnectionGetObjects() (duckdb/duckdb#18181)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2263"
+#define DUCKDB_PATCH_VERSION "0-dev2268"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2263"
+#define DUCKDB_VERSION "v1.4.0-dev2268"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "3de8d76ba8"
+#define DUCKDB_SOURCE_ID "6955f9d77f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
