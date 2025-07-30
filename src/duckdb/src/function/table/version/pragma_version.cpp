18a6a31921e66e5b89cfb66818009ded422b9787
krlmlr <krlmlr@users.noreply.github.com>
2025-07-30 12:07:17 +0000

    vendor: Update vendored sources to duckdb/duckdb@2b974a241bfed0c3d823286445595419a3b599ce
    Add support for "template" types (duckdb/duckdb#18410)
    Update pyodide build to 0.28.0 (duckdb/duckdb#18446)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2333"
+#define DUCKDB_PATCH_VERSION "0-dev2356"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2333"
+#define DUCKDB_VERSION "v1.4.0-dev2356"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "041b68304f"
+#define DUCKDB_SOURCE_ID "2b974a241b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
