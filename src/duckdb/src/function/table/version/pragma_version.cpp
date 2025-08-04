d085e5a0e1568f802c9b992e5ed0a74ec04641fa
krlmlr <krlmlr@users.noreply.github.com>
2025-08-04 11:05:08 +0000

    vendor: Update vendored sources to duckdb/duckdb@67cbce34e13c7b6c9178d13b3886428b3f6f7485
    More insights around dict_fsst compression failure (duckdb/duckdb#18475)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev140"
+#define DUCKDB_PATCH_VERSION "3-dev144"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.3-dev140"
+#define DUCKDB_VERSION "v1.3.3-dev144"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "0e258ecaaf"
+#define DUCKDB_SOURCE_ID "67cbce34e1"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
