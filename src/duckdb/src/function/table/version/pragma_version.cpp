efe38541fcbbe2e49921393db3dafbaa385ca1e0
Kirill Müller <kirill@cynkra.com>
2025-08-25 06:03:12 +0200

    vendor: Update vendored sources to duckdb/duckdb@39636ee1bc986a842c59f9d4e94ef8eb9461bab3
    [Varint] Negation, Subtraction and Over/under-flow checking (duckdb/duckdb#18477)
    ALP test: skip TPC-DS 67 - it is not consistent with floating point numbers (duckdb/duckdb#18528)
    Consistently detect JSON schema indepent of number of threads (duckdb/duckdb#18522)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2720"
+#define DUCKDB_PATCH_VERSION "0-dev2737"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2720"
+#define DUCKDB_VERSION "v1.4.0-dev2737"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "26e810fc91"
+#define DUCKDB_SOURCE_ID "39636ee1bc"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
