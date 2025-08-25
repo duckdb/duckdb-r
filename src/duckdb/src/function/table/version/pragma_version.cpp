efdf7e8bc37478336ed76f9a60b6802a4bebb144
Kirill Müller <kirill@cynkra.com>
2025-08-25 07:19:29 +0200

    vendor: Update vendored sources to duckdb/duckdb@dc67dd251d90fb2960d530c97fb330575c479d33
    Correctly set weights in reservoir sample when switch to slow sampling (duckdb/duckdb#18563)
    [Dev] Add script to create patch from changes in an extension repository (duckdb/duckdb#18620)
    Python test runner: Fix hash comparison error output (duckdb/duckdb#18626)
    [CI] skip building encodings extension in InvokeCI (duckdb/duckdb#18655)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3028"
+#define DUCKDB_PATCH_VERSION "0-dev3043"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev3028"
+#define DUCKDB_VERSION "v1.4.0-dev3043"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a8206a211f"
+#define DUCKDB_SOURCE_ID "dc67dd251d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
