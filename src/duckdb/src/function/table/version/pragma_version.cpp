6ae2cefa005233d7805e9dd4698d36cc42d14755
Kirill Müller <kirill@cynkra.com>
2025-08-25 06:04:35 +0200

    vendor: Update vendored sources to duckdb/duckdb@d500c5793dc9a5e67eebc2f4d9738d547f86154c
    Check if `heap_block_ids` is empty before getting start/end when destroying chunks in `TupleDataCollection` (duckdb/duckdb#18556)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2786"
+#define DUCKDB_PATCH_VERSION "0-dev2788"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2786"
+#define DUCKDB_VERSION "v1.4.0-dev2788"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f1c61de813"
+#define DUCKDB_SOURCE_ID "d500c5793d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
