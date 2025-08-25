07dc1e52d3b77ae1256c50487512b371fce2f54c
Kirill Müller <kirill@cynkra.com>
2025-08-25 06:02:23 +0200

    vendor: Update vendored sources to duckdb/duckdb@22b928b4d998648c8f44c0b5b24ecf37b8a622a1
    Allow expressions to be used in ATTACH / COPY options (duckdb/duckdb#18515)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2580"
+#define DUCKDB_PATCH_VERSION "0-dev2591"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2580"
+#define DUCKDB_VERSION "v1.4.0-dev2591"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "8f8c57241c"
+#define DUCKDB_SOURCE_ID "22b928b4d9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
