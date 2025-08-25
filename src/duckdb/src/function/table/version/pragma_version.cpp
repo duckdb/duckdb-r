cd1cb99d51c8152b9c984afacd612b7565a8d4b8
Kirill Müller <kirill@cynkra.com>
2025-08-25 07:18:47 +0200

    vendor: Update vendored sources to duckdb/duckdb@b6fbb6cbb25f68e831d87df9cf9a7eb34c893ef1
    bump spatial (on main) (duckdb/duckdb#18197)
    bump avro to v1.4 (duckdb/duckdb#18434)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2965"
+#define DUCKDB_PATCH_VERSION "0-dev2977"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2965"
+#define DUCKDB_VERSION "v1.4.0-dev2977"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "56adde892c"
+#define DUCKDB_SOURCE_ID "b6fbb6cbb2"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
