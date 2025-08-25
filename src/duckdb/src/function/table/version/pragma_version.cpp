9a153e7beb4e9f4f9a39502427a06e3ec1e36a8b
Kirill Müller <kirill@cynkra.com>
2025-08-25 06:04:05 +0200

    vendor: Update vendored sources to duckdb/duckdb@582bf198c49a9345465d5af5b4a1fc4568dc9465
    [Fix] Follow-up PR to only delete unique row IDs (duckdb/duckdb#18545)
    Restore missing `test/configs/small_block_size.json` file (duckdb/duckdb#18507)
    Unittester: Add the `--sort-style` parameter that allows a fallback comparison where results are sorted according to a given sort-style (duckdb/duckdb#18542)
    Allow overriding openssl version for FIPS compliance (duckdb/duckdb#18499)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2761"
+#define DUCKDB_PATCH_VERSION "0-dev2778"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2761"
+#define DUCKDB_VERSION "v1.4.0-dev2778"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b699c72915"
+#define DUCKDB_SOURCE_ID "582bf198c4"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
