7aa4c3e38a2518e9ef84215511855e85b69178a9
Kirill Müller <kirill@cynkra.com>
2025-08-25 07:16:00 +0200

    vendor: Update vendored sources to duckdb/duckdb@bab40635d872c3ec5ca45dd893f05602b0d78468
    fix: add formatting to explain row counts (duckdb/duckdb#18566)
    Delete FUNDING.json
    Update FUNDING.json
    Create FUNDING.json

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2812"
+#define DUCKDB_PATCH_VERSION "0-dev2819"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2812"
+#define DUCKDB_VERSION "v1.4.0-dev2819"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "37f3606699"
+#define DUCKDB_SOURCE_ID "bab40635d8"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
