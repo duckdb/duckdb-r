d864c1e6a41d896a0301720cb4bb729eb4ae1410
Kirill Müller <kirill@cynkra.com>
2025-08-25 07:16:59 +0200

    vendor: Update vendored sources to duckdb/duckdb@318850d46c728550048e93ba4e155ffb82802cb2
    Adds a function for updating and adding values in a struct (duckdb/duckdb#15533)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2857"
+#define DUCKDB_PATCH_VERSION "0-dev2879"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2857"
+#define DUCKDB_VERSION "v1.4.0-dev2879"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "84bd10b122"
+#define DUCKDB_SOURCE_ID "318850d46c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
