deebb3580765abae122a20d6e1891689bbb611eb
Kirill Müller <kirill@cynkra.com>
2025-08-25 07:17:35 +0200

    vendor: Update vendored sources to duckdb/duckdb@bf50f75e34cbc92d06c62f05ad7ee9bed1b85615
    Issue duckdb/duckdb#18631: Streaming Windowed Quantile (duckdb/duckdb#18636)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2912"
+#define DUCKDB_PATCH_VERSION "0-dev2914"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2912"
+#define DUCKDB_VERSION "v1.4.0-dev2914"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "2ed9bf887f"
+#define DUCKDB_SOURCE_ID "bf50f75e34"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
