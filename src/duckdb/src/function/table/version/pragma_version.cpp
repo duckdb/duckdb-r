db724f2b8a039d2ebb879f82fb679c3c0c0a8a15
Kirill Müller <kirill@cynkra.com>
2025-08-25 07:20:22 +0200

    vendor: Update vendored sources to duckdb/duckdb@a2b026bdd644802304b2910006db16debe00913f
    Add `date_trunc()` simplification rules (duckdb/duckdb#18457)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3060"
+#define DUCKDB_PATCH_VERSION "0-dev3085"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev3060"
+#define DUCKDB_VERSION "v1.4.0-dev3085"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "85f1888941"
+#define DUCKDB_SOURCE_ID "a2b026bdd6"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
