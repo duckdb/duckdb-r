b4e9d02cd1c9612cd4b9aa8ae94817b80ae3148a
Kirill Müller <kirill@cynkra.com>
2025-08-25 06:01:42 +0200

    vendor: Update vendored sources to duckdb/duckdb@405e363605667319660ed3d80dc12472d83054b7
    [Profiling] Add client context into read functions (duckdb/duckdb#18438)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2515"
+#define DUCKDB_PATCH_VERSION "0-dev2520"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2515"
+#define DUCKDB_VERSION "v1.4.0-dev2520"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "13bef88cae"
+#define DUCKDB_SOURCE_ID "405e363605"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
