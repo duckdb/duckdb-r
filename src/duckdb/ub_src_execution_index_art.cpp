e426136508578bc62fccb8e16b9127fe2f6708b4
Kirill Müller <kirill@cynkra.com>
2025-08-25 07:21:28 +0200

    vendor: Update vendored sources to duckdb/duckdb@22e6d1e3751829d2d029636b75ff931710ca7cbc
    Make ART construction iterative via ARTBuilder (duckdb/duckdb#18702)

--- src/duckdb/ub_src_execution_index_art.cpp
+++ src/duckdb/ub_src_execution_index_art.cpp
@@ -1,26 +1,28 @@
+#include "src/execution/index/art/art_builder.cpp"
+
+#include "src/execution/index/art/art.cpp"
+
 #include "src/execution/index/art/art_key.cpp"
 
 #include "src/execution/index/art/art_merger.cpp"
 
-#include "src/execution/index/art/node.cpp"
+#include "src/execution/index/art/base_leaf.cpp"
+
+#include "src/execution/index/art/base_node.cpp"
 
 #include "src/execution/index/art/iterator.cpp"
 
 #include "src/execution/index/art/leaf.cpp"
 
-#include "src/execution/index/art/base_node.cpp"
-
-#include "src/execution/index/art/node48.cpp"
+#include "src/execution/index/art/node.cpp"
 
 #include "src/execution/index/art/node256.cpp"
 
-#include "src/execution/index/art/prefix.cpp"
-
-#include "src/execution/index/art/base_leaf.cpp"
-
 #include "src/execution/index/art/node256_leaf.cpp"
 
-#include "src/execution/index/art/art.cpp"
+#include "src/execution/index/art/node48.cpp"
 
 #include "src/execution/index/art/plan_art.cpp"
+
+#include "src/execution/index/art/prefix.cpp"
 
