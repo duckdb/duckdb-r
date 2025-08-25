02a5e4dcd1cff79990b1e13c3f0b42d8f2be82ab
Kirill Müller <kirill@cynkra.com>
2025-08-25 07:21:06 +0200

    vendor: Update vendored sources to duckdb/duckdb@18d0c636b012b6822f1003bfa5f75b8941b38384
    Hold row group lock for entire call of MoveToCollection (duckdb/duckdb#18694)
    Expected errors 2053 (duckdb/duckdb#18695)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3114"
+#define DUCKDB_PATCH_VERSION "0-dev3133"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev3114"
+#define DUCKDB_VERSION "v1.4.0-dev3133"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "aca23dcd26"
+#define DUCKDB_SOURCE_ID "18d0c636b0"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
