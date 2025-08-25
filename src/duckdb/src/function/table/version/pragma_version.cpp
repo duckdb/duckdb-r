fa928467397c1408f43c4c46f75b5bcbfe5fd421
Kirill Müller <kirill@cynkra.com>
2025-08-25 06:02:53 +0200

    vendor: Update vendored sources to duckdb/duckdb@408ebe591cea7c24d46b2e7dec987da7344952c6
    Fix incorrect character encoding in GetLastErrorAsString on Windows (duckdb/duckdb#18431)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2710"
+#define DUCKDB_PATCH_VERSION "0-dev2714"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2710"
+#define DUCKDB_VERSION "v1.4.0-dev2714"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "37dcc7317d"
+#define DUCKDB_SOURCE_ID "408ebe591c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
