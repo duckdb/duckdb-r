330fcb4245b861db39ad133ee2850c66c5fccf94
Kirill Müller <kirill@cynkra.com>
2025-08-25 07:18:02 +0200

    vendor: Update vendored sources to duckdb/duckdb@110f45e7a8d15c77fe7bbd9863678f2d4a411e54
    [CSV Sniffer] Fix type detection issue with union and empty columns (duckdb/duckdb#18606)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2926"
+#define DUCKDB_PATCH_VERSION "0-dev2930"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2926"
+#define DUCKDB_VERSION "v1.4.0-dev2930"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a518f9a332"
+#define DUCKDB_SOURCE_ID "110f45e7a8"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
