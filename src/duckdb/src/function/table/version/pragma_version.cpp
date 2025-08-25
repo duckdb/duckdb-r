17bbfa9783fdb570a7910aa197c607afd611d679
Kirill Müller <kirill@cynkra.com>
2025-08-25 07:20:42 +0200

    vendor: Update vendored sources to duckdb/duckdb@d229d97f4028e153234647b5a2b65682b321e77d
    Make sure parse errors are wrapped in ErrorData (duckdb/duckdb#18682)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3107"
+#define DUCKDB_PATCH_VERSION "0-dev3109"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev3107"
+#define DUCKDB_VERSION "v1.4.0-dev3109"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ceccfd13a2"
+#define DUCKDB_SOURCE_ID "d229d97f40"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
