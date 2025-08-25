939502f9c3a45dc53454829e61961a1166b3a14a
Kirill Müller <kirill@cynkra.com>
2025-08-25 06:02:57 +0200

    vendor: Update vendored sources to duckdb/duckdb@28733bfdddaa7760d8ef73b2611e2015c07bfbe2
    Dynamically determine dictionary size limit in Parquet writer (if unset) (duckdb/duckdb#18356)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2714"
+#define DUCKDB_PATCH_VERSION "0-dev2718"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2714"
+#define DUCKDB_VERSION "v1.4.0-dev2718"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "408ebe591c"
+#define DUCKDB_SOURCE_ID "28733bfddd"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
