1777fd3ef5d926e4090e402952cd394704dd5c90
Kirill Müller <kirill@cynkra.com>
2025-08-25 06:58:35 +0200

    vendor: Update vendored sources to duckdb/duckdb@ce8d52c023c76723f92e8fcb0d0a60ef31a424f4
    Fixing compilation with -std=cpp23 (duckdb/duckdb#18557)

--- src/duckdb/third_party/httplib/httplib.hpp
+++ src/duckdb/third_party/httplib/httplib.hpp
@@ -15,6 +15,9 @@
 
 #include "duckdb/original/std/memory.hpp"
 #include "duckdb/common/string.hpp"
+#include <exception>
+#include <stdexcept>
+
 
 /*
  * Configuration
@@ -281,6 +284,7 @@ using socket_t = int;
 
 #include <iostream>
 #include <sstream>
+
 
 // Disabled OpenSSL version check for CI
 //#if OPENSSL_VERSION_NUMBER < 0x1010100fL
