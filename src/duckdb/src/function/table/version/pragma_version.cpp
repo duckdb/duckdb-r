6b496c114289c47ae14aab37378c7d2e734a6452
Kirill Müller <kirill@cynkra.com>
2025-08-25 07:17:23 +0200

    vendor: Update vendored sources to duckdb/duckdb@f1ed071dd2661b440a361f676d39ae4d29150744
    [Profiling] Add client context into more read functions (duckdb/duckdb#18514)
    [CI] Don't zip and upload Code Coverage tests results when Code Coverage got cancelled (duckdb/duckdb#18607)
    [Test] Fix test case and a benchmark (duckdb/duckdb#18610)
    Update README.md (duckdb/duckdb#18614)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2887"
+#define DUCKDB_PATCH_VERSION "0-dev2909"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2887"
+#define DUCKDB_VERSION "v1.4.0-dev2909"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "61efd4a830"
+#define DUCKDB_SOURCE_ID "f1ed071dd2"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
