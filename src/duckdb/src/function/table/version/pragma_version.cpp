7376cd197f4753a37990bbc9d2a02b21543764cc
Kirill Müller <kirill@cynkra.com>
2025-08-25 07:20:53 +0200

    vendor: Update vendored sources to duckdb/duckdb@aca23dcd266cdca3854efa77fdceff471f39d3c3
    Issue duckdb/duckdb#18457: DateTrunc Simplification Warnings (duckdb/duckdb#18687)
    [Python SQLLogicTest] Add `test/sql/pragma/profiling/test_profiling_all.test` to the SKIPPED_TESTS set (duckdb/duckdb#18689)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3109"
+#define DUCKDB_PATCH_VERSION "0-dev3114"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev3109"
+#define DUCKDB_VERSION "v1.4.0-dev3114"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d229d97f40"
+#define DUCKDB_SOURCE_ID "aca23dcd26"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
