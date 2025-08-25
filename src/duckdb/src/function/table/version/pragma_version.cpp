3181037828e55c92738ca0b498a29d36d95e0b68
Kirill Müller <kirill@cynkra.com>
2025-08-25 07:18:08 +0200

    vendor: Update vendored sources to duckdb/duckdb@dd86cdd88749f263f6e069008a88f6c62374e99f
    [ART] ART::Erase refactoring (duckdb/duckdb#18595)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2930"
+#define DUCKDB_PATCH_VERSION "0-dev2937"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2930"
+#define DUCKDB_VERSION "v1.4.0-dev2937"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "110f45e7a8"
+#define DUCKDB_SOURCE_ID "dd86cdd887"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
