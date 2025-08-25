0c7611b43bb2f94692560f1af801640bde1ccb35
Kirill Müller <kirill@cynkra.com>
2025-08-25 07:17:29 +0200

    vendor: Update vendored sources to duckdb/duckdb@2ed9bf887f61a0ac226ab8c8f1164601d985d607
    Fix serialization backwards compatability for varargs functions (duckdb/duckdb#18596)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2909"
+#define DUCKDB_PATCH_VERSION "0-dev2912"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev2909"
+#define DUCKDB_VERSION "v1.4.0-dev2912"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f1ed071dd2"
+#define DUCKDB_SOURCE_ID "2ed9bf887f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
