84e6070cccb211b04ac106780a4339a1d89cbe83
Kirill Müller <kirill@cynkra.com>
2025-08-26 17:04:27 +0200

    vendor: Update vendored sources to duckdb/duckdb@61f797f0e2d17d16430e7ca6f52ce720ebd156c3
    Python-style positional/named arguments for macro's (duckdb/duckdb#18684)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3146"
+#define DUCKDB_PATCH_VERSION "0-dev3162"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev3146"
+#define DUCKDB_VERSION "v1.4.0-dev3162"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f6dfad0f1a"
+#define DUCKDB_SOURCE_ID "61f797f0e2"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
