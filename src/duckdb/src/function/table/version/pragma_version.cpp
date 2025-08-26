69f46fb60e08af6e8cc7c5f6e7bac9fa2ae4510b
Kirill Müller <kirill@cynkra.com>
2025-08-26 17:04:36 +0200

    vendor: Update vendored sources to duckdb/duckdb@71af4dca9105830a3f666689c940ba2cc3db7f4e
    Add Option to Allocate Using an Arena in `string_t` (duckdb/duckdb#17992)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3165"
+#define DUCKDB_PATCH_VERSION "0-dev3187"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev3165"
+#define DUCKDB_VERSION "v1.4.0-dev3187"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f8886f66ac"
+#define DUCKDB_SOURCE_ID "71af4dca91"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
