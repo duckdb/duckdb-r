4d4ac14f059a31f8013a340b23a5698ff96f0c5b
Kirill Müller <kirill@cynkra.com>
2025-08-26 17:17:21 +0200

    vendor: Update vendored sources to duckdb/duckdb@ba40c75d57e18fe143234a9b9015fd462c43f5b3
    Use 1-based indexing for SQL-based JSON array extraction (duckdb/duckdb#18735)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3268"
+#define DUCKDB_PATCH_VERSION "0-dev3271"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev3268"
+#define DUCKDB_VERSION "v1.4.0-dev3271"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4101338ad0"
+#define DUCKDB_SOURCE_ID "ba40c75d57"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
