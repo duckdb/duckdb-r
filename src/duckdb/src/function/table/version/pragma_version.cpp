5d8ed495eb2f17fb16927cd1010b76d310cda87f
krlmlr <krlmlr@users.noreply.github.com>
2025-08-27 16:55:11 +0000

    vendor: Update vendored sources to duckdb/duckdb@f40ac6e5653dc7bc83fa03a5021d8aa09a938cef
    Backport duckdb/duckdb#18374 to `v1.3-ossivalis` (duckdb/duckdb#18752)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev227"
+#define DUCKDB_PATCH_VERSION "3-dev231"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 3
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.3.3-dev227"
+#define DUCKDB_VERSION "v1.3.3-dev231"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "df0a3de744"
+#define DUCKDB_SOURCE_ID "f40ac6e565"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
