f992c48d59415aa9feca3c5a1a4509763aacf625
krlmlr <krlmlr@users.noreply.github.com>
2025-08-28 11:08:36 +0000

    vendor: Update vendored sources to duckdb/duckdb@f4a269a077ebadae11cdc2e43ae76f2141b43e89
    Minor logging fixes and more benchmarking (duckdb/duckdb#18755)
    Extensions.yml should also check converted_to_draft (duckdb/duckdb#18754)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3404"
+#define DUCKDB_PATCH_VERSION "0-dev3408"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev3404"
+#define DUCKDB_VERSION "v1.4.0-dev3408"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1b955e2313"
+#define DUCKDB_SOURCE_ID "f4a269a077"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
