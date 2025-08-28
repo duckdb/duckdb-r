6c9bc89f3d87a74781655fd14b01c6d50e9d1a46
krlmlr <krlmlr@users.noreply.github.com>
2025-08-28 16:08:59 +0000

    vendor: Update vendored sources to duckdb/duckdb@3a45b5f17f3d4d83148c7d30765a0b38cde96f96
    Make `duckdb_log` return a TIMESTAMP_TZ (duckdb/duckdb#18768)
    Fix Path Typo in Extension's CMake Warning Message (duckdb/duckdb#18766)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3456"
+#define DUCKDB_PATCH_VERSION "0-dev3462"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev3456"
+#define DUCKDB_VERSION "v1.4.0-dev3462"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ebe58746a0"
+#define DUCKDB_SOURCE_ID "3a45b5f17f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
