60f5b72f3be9492d5a70ec6b87fa1d44183610cd
krlmlr <krlmlr@users.noreply.github.com>
2025-08-28 08:06:39 +0000

    vendor: Update vendored sources to duckdb/duckdb@ea1524cdccd853728a7881c85a92ba3a5495d136
    Remove separate WAL encryption flag (duckdb/duckdb#18750)
    Add leak suppressions to nightly runs (duckdb/duckdb#18748)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3390"
+#define DUCKDB_PATCH_VERSION "0-dev3395"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev3390"
+#define DUCKDB_VERSION "v1.4.0-dev3395"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1b88c2b433"
+#define DUCKDB_SOURCE_ID "ea1524cdcc"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
