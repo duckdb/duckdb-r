0f9c108db558e99eaf3865462750573d1598aa8d
krlmlr <krlmlr@users.noreply.github.com>
2025-08-28 06:06:06 +0000

    vendor: Update vendored sources to duckdb/duckdb@fe92c476fcdaead3ae90f89bdc1b3e59b9844369
    Use correct type for pushing collations in subqueries (duckdb/duckdb#18744)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3349"
+#define DUCKDB_PATCH_VERSION "0-dev3351"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev3349"
+#define DUCKDB_VERSION "v1.4.0-dev3351"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "050ba71c81"
+#define DUCKDB_SOURCE_ID "fe92c476fc"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
