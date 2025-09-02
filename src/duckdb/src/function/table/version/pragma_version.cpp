5f948939b3f76f85a42fb2b67e7392d0d89d68a7
krlmlr <krlmlr@users.noreply.github.com>
2025-09-02 10:12:27 +0000

    vendor: Update vendored sources to duckdb/duckdb@da1eb68203a47dff51d30bbf7ca76fbfcfe9597a
    Add callback to get a list of copy options, use this to provide suggestions and to erase options from import that are only used during exporting (duckdb/duckdb#18812)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3605"
+#define DUCKDB_PATCH_VERSION "0-dev3612"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev3605"
+#define DUCKDB_VERSION "v1.4.0-dev3612"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1736683b86"
+#define DUCKDB_SOURCE_ID "da1eb68203"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
