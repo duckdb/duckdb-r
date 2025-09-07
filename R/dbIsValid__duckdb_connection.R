58f3a1575b26061536ab03da9c1cc343e673d9cb
Kirill Müller <kirill@cynkra.com>
2025-09-07 20:14:18 +0200

    ref

--- R/dbIsValid__duckdb_connection.R
+++ R/dbIsValid__duckdb_connection.R
@@ -5,7 +5,7 @@ dbIsValid__duckdb_connection <- function(dbObj, ...) {
   # Use fast C++ connection validity check instead of executing SELECT 1
   # This avoids deadlock issues when called from progress bar handlers
   # that already have ScopedInterruptHandler protection
-  rethrow_rapi_connection_valid(dbObj@conn)
+  rethrow_rapi_connection_valid(dbObj@conn_ref)
 }
 
 #' @rdname duckdb_connection-class
