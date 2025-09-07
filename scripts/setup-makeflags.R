818f690e79ba18450467fb52b10e2a215527ff85
Kirill Müller <kirill@cynkra.com>
2025-09-07 17:55:33 +0200

    Tweak

--- scripts/setup-makeflags.R
+++ scripts/setup-makeflags.R
@@ -10,15 +10,15 @@ makeflags <- Sys.getenv("MAKEFLAGS", unset = "")
 if (makeflags == "") {
   # Detect number of cores
   ncores <- parallel::detectCores()
-  
+
   # CRAN policy: don't use more than 2 cores during checks
   # Use NOT_CRAN environment variable to allow more cores locally
   not_cran <- tolower(Sys.getenv("NOT_CRAN", "false")) %in% c("true", "1", "yes")
-  
+
   if (!not_cran) {
     ncores <- min(ncores, 2)
   }
-  
+
   # Output the MAKEFLAGS value for the shell to export
   makeflags_value <- sprintf("-j%s", ncores)
   cat(makeflags_value)
