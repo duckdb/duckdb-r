05d8638008aff74bc76f3074ecc57db6142bda73
Kirill Müller <kirill@cynkra.com>
2025-09-09 19:52:21 +0200

    Color

--- tests/testthat/test-error-handling.R
+++ tests/testthat/test-error-handling.R
@@ -1,5 +1,6 @@
 test_that("rapi_error functions accept additional parameters", {
   local_edition(3)
+  rlang::local_options(cli.num_colors = 1)
 
   # Test that our updated function signatures work with default parameters
   expect_snapshot(error = TRUE, {
