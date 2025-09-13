28343f5b8b83e160e8c4ba6bbd568eabf6fa417d
krlmlr <krlmlr@users.noreply.github.com>
2025-09-13 09:04:14 +0000

    vendor: Update vendored sources to duckdb/duckdb@c151565eb3a7b010385d155abcc2006256dea2f9
    Cannot create table from variant yet (duckdb/duckdb#18912)
    [chore] Fix amalgamation build in progress_bar (duckdb/duckdb#18910)
    Bump inet & aws (duckdb/duckdb#18899)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev4008"
+#define DUCKDB_PATCH_VERSION "0-dev4015"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev4008"
+#define DUCKDB_VERSION "v1.4.0-dev4015"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "81547d76b8"
+#define DUCKDB_SOURCE_ID "c151565eb3"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
