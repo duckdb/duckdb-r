d6eee2f03959d103856c70bdea938347241b6d0a
krlmlr <krlmlr@users.noreply.github.com>
2025-09-18 08:05:44 +0000

    vendor: Update vendored sources to duckdb/duckdb@0dcf633f603a629981d089202f93b9080cb1a3e9
    PEG grammar updates (duckdb/duckdb#19031)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev4273"
+#define DUCKDB_PATCH_VERSION "0-dev4292"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.0-dev4273"
+#define DUCKDB_VERSION "v1.4.0-dev4292"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d8e291c16a"
+#define DUCKDB_SOURCE_ID "0dcf633f60"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
