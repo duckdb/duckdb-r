ace74df9023d2a560411338659fc5a6fd924e9ee
Kirill Müller <kirill@cynkra.com>
2025-09-29 08:11:10 +0200

    vendor: Update vendored sources to duckdb/duckdb@af47f945b01f79fa3becad9c4ed269dba725ef5a
    Correctly re-align all child column segments of the ColumnData on Deserialize, and add logging to checkpoints (duckdb/duckdb#19055)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev29"
+#define DUCKDB_PATCH_VERSION "1-dev33"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.1-dev29"
+#define DUCKDB_VERSION "v1.4.1-dev33"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4039239f10"
+#define DUCKDB_SOURCE_ID "af47f945b0"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
