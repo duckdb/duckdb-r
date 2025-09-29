245ee36eb405dc4670c8484be3dee668dcec7d6f
Kirill Müller <kirill@cynkra.com>
2025-09-29 08:10:52 +0200

    vendor: Update vendored sources to duckdb/duckdb@e5025153a95572bf8417d59d2b04e2b8f620d6fc
    Throw if we detect a quoted new line with the null padding set in parallel mode (duckdb/duckdb#19012)
    Issue duckdb/duckdb#19016: ICU Offset Parsing (duckdb/duckdb#19029)
    ComplexJSON: parse all valid JSON correctly (duckdb/duckdb#19024)
    set default value of MAIN_BRANCH_VERSIONING to false (duckdb/duckdb#19014)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,17 +1,17 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2"
+#define DUCKDB_PATCH_VERSION "1-dev12"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
-#define DUCKDB_MINOR_VERSION 5
+#define DUCKDB_MINOR_VERSION 4
 #endif
 #ifndef DUCKDB_MAJOR_VERSION
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev2"
+#define DUCKDB_VERSION "v1.4.1-dev12"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d19df69e14"
+#define DUCKDB_SOURCE_ID "e5025153a9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
