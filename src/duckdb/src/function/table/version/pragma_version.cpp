bb010bb5827661c2bb4c9d22e0ce9a4edd7c8758
Kirill Müller <kirill@cynkra.com>
2025-09-29 08:11:06 +0200

    vendor: Update vendored sources to duckdb/duckdb@4039239f1024468b2c6fa38143080ee1a5a9af4e
    Fix threading issues in metadata manager, and expand concurrent attach / detach fuzz test (duckdb/duckdb#19054)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev22"
+#define DUCKDB_PATCH_VERSION "1-dev29"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.1-dev22"
+#define DUCKDB_VERSION "v1.4.1-dev29"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "93b8281d69"
+#define DUCKDB_SOURCE_ID "4039239f10"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
