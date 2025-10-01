5ec3902a262c88805b5d3918bf6836f1a5a40e1c
krlmlr <krlmlr@users.noreply.github.com>
2025-10-01 15:08:43 +0000

    vendor: Update vendored sources to duckdb/duckdb@9eccb88e54dbed8f9d8059f84acb7ee3ae0ac5e9
    Check for unresolved parameters when binding `CREATE MACRO ... AS TABLE` (duckdb/duckdb#19196)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev111"
+#define DUCKDB_PATCH_VERSION "1-dev114"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.1-dev111"
+#define DUCKDB_VERSION "v1.4.1-dev114"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "3a557fe42f"
+#define DUCKDB_SOURCE_ID "9eccb88e54"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
