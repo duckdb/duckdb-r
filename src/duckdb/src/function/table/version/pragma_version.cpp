c1b9b4214b076718fe5cf119e681bcbac4e0403e
krlmlr <krlmlr@users.noreply.github.com>
2025-10-03 10:05:18 +0000

    vendor: Update vendored sources to duckdb/duckdb@dabebf30c877c5c9665a231211425e9e04a19ceb
    Don't write parquet-native `GEOMETRY` by default, add option to control GeoParquet version (duckdb/duckdb#19244)
    Bump mysql and sqlite (duckdb/duckdb#19240)
    Bump DuckLake to latest of V1.4 (duckdb/duckdb#19237)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev178"
+#define DUCKDB_PATCH_VERSION "1-dev186"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.1-dev178"
+#define DUCKDB_VERSION "v1.4.1-dev186"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "24c295a9dd"
+#define DUCKDB_SOURCE_ID "dabebf30c8"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
