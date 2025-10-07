0f066246602364c97eed54b765607920ad887ce2
krlmlr <krlmlr@users.noreply.github.com>
2025-10-07 11:04:15 +0000

    vendor: Update vendored sources (tag v1.4.1) to duckdb/duckdb@b390a7c3760bd95926fe8aefde20d04b349b472e
    Bump: avro, httpfs (duckdb/duckdb#19248)
    Bump: iceberg (duckdb/duckdb#19250)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev191"
+#define DUCKDB_PATCH_VERSION "1"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.1-dev191"
+#define DUCKDB_VERSION "v1.4.1"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "8a74cb9212"
+#define DUCKDB_SOURCE_ID "b390a7c376"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
