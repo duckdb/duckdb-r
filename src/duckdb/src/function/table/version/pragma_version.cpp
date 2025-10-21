122b49990677e56d255f4c8bcff367fa70db8080
krlmlr <krlmlr@users.noreply.github.com>
2025-10-21 10:05:10 +0000

    vendor: Update vendored sources to duckdb/duckdb@fabc49d076becde4d2429616c02bdce841fca952
    Fix StringDecompress for hugeint_t values (duckdb/duckdb#19403)
    Fixup Kalman fiter: bound it's (0, scale_factor), not (0, 1) (duckdb/duckdb#19395)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev93"
+#define DUCKDB_PATCH_VERSION "2-dev97"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.2-dev93"
+#define DUCKDB_VERSION "v1.4.2-dev97"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "dff9856570"
+#define DUCKDB_SOURCE_ID "fabc49d076"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
