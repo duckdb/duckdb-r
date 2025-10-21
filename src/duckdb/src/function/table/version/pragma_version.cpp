45f5c2cf60f9c8018ef52b2e622e1d822487f414
krlmlr <krlmlr@users.noreply.github.com>
2025-10-21 08:07:04 +0000

    vendor: Update vendored sources to duckdb/duckdb@d8ae68f883fd79a064471dc2b2a2fb03aa7129d8
    Fixes for CTE (de)serialization compatibility with older versions (duckdb/duckdb#19393)
    BUGFIX: Silent failure to write row groups with large lists (duckdb/duckdb#19376)
    Throw if non-`VARCHAR` key is passed to `json_object` (duckdb/duckdb#19365)
    add test tag support [vfs integration tests p1] (duckdb/duckdb#19331)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev67"
+#define DUCKDB_PATCH_VERSION "2-dev89"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.2-dev67"
+#define DUCKDB_VERSION "v1.4.2-dev89"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d1eaf5e700"
+#define DUCKDB_SOURCE_ID "d8ae68f883"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
