0f39843b5d18a60d2f603afa9418bab9b1d46078
krlmlr <krlmlr@users.noreply.github.com>
2025-10-28 08:06:05 +0000

    vendor: Update vendored sources to duckdb/duckdb@b62b03c4b3610e82493a4da1b45cfbbbed4eae77
    CI Fix: clear benchmark cache between runs (duckdb/duckdb#19508)
    vfs integration tests p2 - update (env) vars and tests (duckdb/duckdb#19428)
    [Compression] Fix issue in ZSTD decompression related to mis-interpreted segment offsets (duckdb/duckdb#19475)
    Add tests for nested lambda bindings (duckdb/duckdb#19453)
    WASM duckdb/duckdb#1897: UTC Offset Support (duckdb/duckdb#19464)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev123"
+#define DUCKDB_PATCH_VERSION "2-dev149"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.2-dev123"
+#define DUCKDB_VERSION "v1.4.2-dev149"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4248409fc2"
+#define DUCKDB_SOURCE_ID "b62b03c4b3"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
