05ae6a3d3cefdd2ef1e1ae1d4f5cac5811be0216
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:18:28 +0100

    vendor: Update vendored sources to duckdb/duckdb@8704368343af72bfb8326b4273fed1a2caaa2427
    [Dev] Add a `VARIANT` visitor class (duckdb/duckdb#19387)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1092"
+#define DUCKDB_PATCH_VERSION "0-dev1098"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev1092"
+#define DUCKDB_VERSION "v1.5.0-dev1098"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4fdf341c83"
+#define DUCKDB_SOURCE_ID "8704368343"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
