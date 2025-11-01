923862c9f5b2dd160c0a4d313189132404f92dcf
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:17:39 +0100

    vendor: Update vendored sources to duckdb/duckdb@9b8604e509ff0c216a2ee14437714846630ad0b2
    [C API] Improve documentation on list vectors (duckdb/duckdb#19349)
    Allow * NOT SIMILAR TO 'pattern' (duckdb/duckdb#19232)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1040"
+#define DUCKDB_PATCH_VERSION "0-dev1058"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev1040"
+#define DUCKDB_VERSION "v1.5.0-dev1058"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4a221c652a"
+#define DUCKDB_SOURCE_ID "9b8604e509"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
