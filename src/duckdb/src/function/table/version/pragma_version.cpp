5e1688c50f43c142744b311839f3d0ee5ddffbb5
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:16:55 +0100

    vendor: Update vendored sources to duckdb/duckdb@fc2da14954a4a1ba0800cbcc7b9254374167db1f
    Merge andium into main (duckdb/duckdb#19325)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev883"
+#define DUCKDB_PATCH_VERSION "0-dev962"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev883"
+#define DUCKDB_VERSION "v1.5.0-dev962"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b83205f805"
+#define DUCKDB_SOURCE_ID "fc2da14954"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
