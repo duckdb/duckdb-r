fdc931ea5e0ab75c01d1c41d683417e2f97b94b2
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:03:47 +0100

    vendor: Update vendored sources to duckdb/duckdb@f793ea27c6ca1ee2f1ca22aff86ba6586575d085
    Partially remove BoundQueryNode - have Bind directly emit a BoundStatement (duckdb/duckdb#19289)
    [shell] Fix tilde parsing for .once statement (duckdb/duckdb#19254)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev720"
+#define DUCKDB_PATCH_VERSION "0-dev732"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev720"
+#define DUCKDB_VERSION "v1.5.0-dev732"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "5657cbdc0b"
+#define DUCKDB_SOURCE_ID "f793ea27c6"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
--- src/duckdb/ub_src_planner_binder_query_node.cpp
+++ src/duckdb/ub_src_planner_binder_query_node.cpp
@@ -6,6 +6,8 @@
 
 #include "src/planner/binder/query_node/bind_cte_node.cpp"
 
+#include "src/planner/binder/query_node/bind_statement_node.cpp"
+
 #include "src/planner/binder/query_node/bind_table_macro_node.cpp"
 
 #include "src/planner/binder/query_node/plan_query_node.cpp"
