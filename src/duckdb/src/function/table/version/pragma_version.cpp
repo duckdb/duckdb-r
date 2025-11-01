e1583a7a72fc4e7065042c763e7c87bc83317e57
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:17:14 +0100

    vendor: Update vendored sources to duckdb/duckdb@cb636bc87433541aa326373012baf4afe05f651f
    Rework CTE binding: remove CTENode, and bind CommonTableExpressionMap directly instead (duckdb/duckdb#19351)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev973"
+#define DUCKDB_PATCH_VERSION "0-dev986"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev973"
+#define DUCKDB_VERSION "v1.5.0-dev986"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "0e5bf48abf"
+#define DUCKDB_SOURCE_ID "cb636bc874"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
