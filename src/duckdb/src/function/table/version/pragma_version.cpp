9ead06399c73508fac52a5160c190b57564d8660
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:02:35 +0100

    vendor: Update vendored sources to duckdb/duckdb@664366d25a225ae3b4893b45b2917e917911c4df
    Add support for late materialization and filter pushdown to read_duckdb (duckdb/duckdb#19145)
    Remove unused code (duckdb/duckdb#19134)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev352"
+#define DUCKDB_PATCH_VERSION "0-dev358"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev352"
+#define DUCKDB_VERSION "v1.5.0-dev358"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "7992e70b4d"
+#define DUCKDB_SOURCE_ID "664366d25a"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
