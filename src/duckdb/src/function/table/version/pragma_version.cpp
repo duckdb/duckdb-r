4d7dec883d841e0595a54acbab67cfdbf8a3d835
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:19:39 +0100

    vendor: Update vendored sources to duckdb/duckdb@1b30f728eacca2e57f0ed15de319dd79f57b92e8
    Add DuckDB::String to operators (duckdb/duckdb#19423)
    SET statement for PEG Transformer (duckdb/duckdb#19320)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1298"
+#define DUCKDB_PATCH_VERSION "0-dev1326"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev1298"
+#define DUCKDB_VERSION "v1.5.0-dev1326"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d921f4d6ba"
+#define DUCKDB_SOURCE_ID "1b30f728ea"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
