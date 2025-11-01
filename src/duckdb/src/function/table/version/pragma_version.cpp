f8d8196e2ae497ec3274f3d8c4a39867628a2170
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:17:48 +0100

    vendor: Update vendored sources to duckdb/duckdb@bc61f690d3552807a1615ce86055010952dbf920
    Internal duckdb/duckdb#6210: IEJoin Threading (duckdb/duckdb#19361)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1067"
+#define DUCKDB_PATCH_VERSION "0-dev1070"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev1067"
+#define DUCKDB_VERSION "v1.5.0-dev1070"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "604e5ce24e"
+#define DUCKDB_SOURCE_ID "bc61f690d3"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
