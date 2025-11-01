ed0d1e8b8679f12eb8f422301ac4eca858ac0f95
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:02:53 +0100

    vendor: Update vendored sources to duckdb/duckdb@07d170f87ea42a1bfd8e4a71d2911eb0e0500e44
    Fix missing replacement scan code for CTEs (duckdb/duckdb#19159)
    Validate filesystem registration (duckdb/duckdb#19168)
    Print help to stdout (duckdb/duckdb#19170)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev372"
+#define DUCKDB_PATCH_VERSION "0-dev383"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev372"
+#define DUCKDB_VERSION "v1.5.0-dev383"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "2259ad7316"
+#define DUCKDB_SOURCE_ID "07d170f87e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
