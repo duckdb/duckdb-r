92da936f875ade7bad654cc64309b584881c4041
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:09:35 +0100

    vendor: Update vendored sources to duckdb/duckdb@00dda2d0cc4ec963626d7ce6232a404a267ec7d2
    Remove BoundTableRef and make binding a TableRef directly return a BoundStatement (duckdb/duckdb#19294)

--- src/duckdb/ub_src_planner_binder_tableref.cpp
+++ src/duckdb/ub_src_planner_binder_tableref.cpp
@@ -22,23 +22,5 @@
 
 #include "src/planner/binder/tableref/bind_named_parameters.cpp"
 
-#include "src/planner/binder/tableref/plan_basetableref.cpp"
-
-#include "src/planner/binder/tableref/plan_delimgetref.cpp"
-
-#include "src/planner/binder/tableref/plan_dummytableref.cpp"
-
-#include "src/planner/binder/tableref/plan_expressionlistref.cpp"
-
-#include "src/planner/binder/tableref/plan_column_data_ref.cpp"
-
 #include "src/planner/binder/tableref/plan_joinref.cpp"
-
-#include "src/planner/binder/tableref/plan_subqueryref.cpp"
-
-#include "src/planner/binder/tableref/plan_table_function.cpp"
-
-#include "src/planner/binder/tableref/plan_cteref.cpp"
-
-#include "src/planner/binder/tableref/plan_pivotref.cpp"
 
