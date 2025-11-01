3ca138782d262525ed17678aac2fdc2be8f96861
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:02:13 +0100

    vendor: Update vendored sources to duckdb/duckdb@e3b2c2219fe738ac990186c3d8193d58a9c11e40
    Rework `UNION (ALL)` operators (`SetOperationNode`/`LogicalSetOperation`/`PhysicalUnion`) to have multiple children (duckdb/duckdb#19109)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev283"
+#define DUCKDB_PATCH_VERSION "0-dev290"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev283"
+#define DUCKDB_VERSION "v1.5.0-dev290"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "8d72b0ef61"
+#define DUCKDB_SOURCE_ID "e3b2c2219f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
