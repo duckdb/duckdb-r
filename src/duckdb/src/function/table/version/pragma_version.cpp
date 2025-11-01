8c26e9808814bc345fa8981ba6d0715e3492bb32
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:20:22 +0100

    vendor: Update vendored sources to duckdb/duckdb@bfc1fc8800aa17ee7bbc16f4f0eab5b58f2166a0
    Internal duckdb/duckdb#6196: AsOf Prefix Comparisons (duckdb/duckdb#19466)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1375"
+#define DUCKDB_PATCH_VERSION "0-dev1378"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev1375"
+#define DUCKDB_VERSION "v1.5.0-dev1378"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "6a144a5b7e"
+#define DUCKDB_SOURCE_ID "bfc1fc8800"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
