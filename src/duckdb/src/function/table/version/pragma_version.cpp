0759f4942b42ff3d6648756f63ae27b6d36bbce5
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:02:19 +0100

    vendor: Update vendored sources to duckdb/duckdb@7992e70b4d548ca604e34e55dfc69f960e7500c9
    Fix unholy interaction between `CREATE VIEW` and `CTE` definitions (duckdb/duckdb#19116)
    pushdown filter when doing cross or lateral join with unnest (duckdb/duckdb#19085)
    Add support for `read_duckdb` - a function that allows reading / globbing DuckDB database files  (duckdb/duckdb#19108)
    During optimistic writing, gather N row groups and write them at the same time column-at-a-time (duckdb/duckdb#19118)
    fix: check tree depth and flatten dep join for custom operator extension (duckdb/duckdb#19094)
    Fix typo (duckdb/duckdb#19130)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev290"
+#define DUCKDB_PATCH_VERSION "0-dev352"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev290"
+#define DUCKDB_VERSION "v1.5.0-dev352"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e3b2c2219f"
+#define DUCKDB_SOURCE_ID "7992e70b4d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
