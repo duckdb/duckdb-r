458a0e6f4e4a01559af90afd62119321fcc4be3d
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:20:56 +0100

    vendor: Update vendored sources to duckdb/duckdb@f8913ffd9afe9259d576da58e64184bf9b028207
    [Optimizer] Support constant order normalization (duckdb/duckdb#19494)
    Fix issue duckdb/duckdb#17941: Cryptic error message when dropping a column part of a unique constraint.  (duckdb/duckdb#19465)
    [minor] Address test compilation warnings (duckdb/duckdb#19492)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1406"
+#define DUCKDB_PATCH_VERSION "0-dev1415"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev1406"
+#define DUCKDB_VERSION "v1.5.0-dev1415"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f1f9fc1bec"
+#define DUCKDB_SOURCE_ID "f8913ffd9a"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
