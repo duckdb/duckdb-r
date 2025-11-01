14c4ab7b0a0b0c1893ae4790d9d976be6565af68
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:03:03 +0100

    vendor: Update vendored sources to duckdb/duckdb@cdb1995d71ccb37a870aa0c2b6a4e2049122ded9
    Internal duckdb/duckdb#5380: AsOf Join Pipeline  (duckdb/duckdb#19169)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev391"
+#define DUCKDB_PATCH_VERSION "0-dev401"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev391"
+#define DUCKDB_VERSION "v1.5.0-dev401"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "3e00750a54"
+#define DUCKDB_SOURCE_ID "cdb1995d71"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
