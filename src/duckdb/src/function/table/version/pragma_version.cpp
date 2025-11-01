87f1d0234bf16c1e68bb892867a57f82c6030f1d
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:01:51 +0100

    vendor: Update vendored sources to duckdb/duckdb@74f64b65d43261269577645c0d6dc8966e629137
    Destroy row groups in parallel (duckdb/duckdb#19053)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev205"
+#define DUCKDB_PATCH_VERSION "1-dev209"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.1-dev205"
+#define DUCKDB_VERSION "v1.4.1-dev209"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d52dd4e3df"
+#define DUCKDB_SOURCE_ID "74f64b65d4"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
