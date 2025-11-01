f1c505c62ae7198a21987bd4c9045e9308f0dd5f
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:22:59 +0100

    vendor: Update vendored sources to duckdb/duckdb@ead3ca87f790fd7e0b1e3efdaf74c727aae47473
    Custom Order Row Group Scans (duckdb/duckdb#19537)
    CLI: Add support for dynamic prompts (duckdb/duckdb#19579)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1779"
+#define DUCKDB_PATCH_VERSION "0-dev1813"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev1779"
+#define DUCKDB_VERSION "v1.5.0-dev1813"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "36c5ef1d66"
+#define DUCKDB_SOURCE_ID "ead3ca87f7"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
