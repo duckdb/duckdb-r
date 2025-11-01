a9d5f23c67b2261774063dcc6c0d189f1300bf69
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:18:53 +0100

    vendor: Update vendored sources to duckdb/duckdb@7977b60c54ef05e87d58ec3fa9a26e52ee260a82
    Add Null Support for TopNWindowElimination Rule (duckdb/duckdb#19370)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1137"
+#define DUCKDB_PATCH_VERSION "0-dev1149"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev1137"
+#define DUCKDB_VERSION "v1.5.0-dev1149"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a8303e69dd"
+#define DUCKDB_SOURCE_ID "7977b60c54"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
