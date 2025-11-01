fa4edeb59e870f40b537332adff87164499760ac
Kirill Müller <kirill@cynkra.com>
2025-11-02 00:22:42 +0100

    vendor: Update vendored sources to duckdb/duckdb@b1cbd31c8ff4784cd86548fff9a7786e64bde147
    Fix: list_concat(NULL) returns NULL (duckdb/duckdb#19565)
    Change `format_version` to `2` when specify `V2` for `PARQUET_VERSION` (duckdb/duckdb#18855)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev1756"
+#define DUCKDB_PATCH_VERSION "0-dev1762"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev1756"
+#define DUCKDB_VERSION "v1.5.0-dev1762"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "02e0f47cbb"
+#define DUCKDB_SOURCE_ID "b1cbd31c8f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
