32ad996c06f5c14df45cbe93428f19caf03c2d5d
krlmlr <krlmlr@users.noreply.github.com>
2025-11-02 14:04:00 +0000

    vendor: Update vendored sources to duckdb/duckdb@407720a34804f0da61d5ba6645c3c44ec6ddf0d8
    Wal index deletes (duckdb/duckdb#19477)
    Skip compiling remote optimizer test when TSAN Is enabled (duckdb/duckdb#19590)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev199"
+#define DUCKDB_PATCH_VERSION "2-dev224"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.2-dev199"
+#define DUCKDB_VERSION "v1.4.2-dev224"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "9ea6e07a29"
+#define DUCKDB_SOURCE_ID "407720a348"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
