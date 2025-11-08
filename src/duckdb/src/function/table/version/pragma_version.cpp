393ecdcea8aaa0d870404d5c20b9bfb9bd534a4a
krlmlr <krlmlr@users.noreply.github.com>
2025-11-08 17:04:04 +0000

    vendor: Update vendored sources to duckdb/duckdb@e3d242509e5710314921a0d7debd0bedb4d10a3e
    Add request timing to HTTP log (duckdb/duckdb#19691)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev284"
+#define DUCKDB_PATCH_VERSION "2-dev288"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.2-dev284"
+#define DUCKDB_VERSION "v1.4.2-dev288"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "7ce99bc041"
+#define DUCKDB_SOURCE_ID "e3d242509e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
