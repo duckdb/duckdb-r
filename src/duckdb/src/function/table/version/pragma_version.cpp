818ab167c987bb77375cf215316e7b0259051f6f
krlmlr <krlmlr@users.noreply.github.com>
2025-11-10 08:06:13 +0000

    vendor: Update vendored sources to duckdb/duckdb@3645e8d4b92c23efdd2d485e3303d7c33a0e4fe3
    Fix LENGTH(NULL) with GROUP BY NULL binding issue (duckdb/duckdb#19269)
    Internal duckdb/duckdb#6519: HashedSort Memory Footprint (duckdb/duckdb#19702)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2368"
+#define DUCKDB_PATCH_VERSION "0-dev2374"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev2368"
+#define DUCKDB_VERSION "v1.5.0-dev2374"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "44b706b2b7"
+#define DUCKDB_SOURCE_ID "3645e8d4b9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
