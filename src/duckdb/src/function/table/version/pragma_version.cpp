aaef6ae0cabb580134a7eca1843b4881ccf3c184
krlmlr <krlmlr@users.noreply.github.com>
2025-11-10 10:05:56 +0000

    vendor: Update vendored sources to duckdb/duckdb@95fcb8f18819b1a77df079a7fcb753a8c2f52844
    Bump: aws, ducklake, httpfs, iceberg (duckdb/duckdb#19654)
    Add missing query location to blob cast (duckdb/duckdb#19689)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev288"
+#define DUCKDB_PATCH_VERSION "2-dev295"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.2-dev288"
+#define DUCKDB_VERSION "v1.4.2-dev295"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e3d242509e"
+#define DUCKDB_SOURCE_ID "95fcb8f188"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
