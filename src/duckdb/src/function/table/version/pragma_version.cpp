28de44bf869dab6d30a95ff1bc3b1cf060b66d5c
krlmlr <krlmlr@users.noreply.github.com>
2025-11-11 09:05:33 +0000

    vendor: Update vendored sources to duckdb/duckdb@287ac426ad072cab320f1eda69bad05e95b02439
    search activer binders in table function binder during binding column… (duckdb/duckdb#14486)
    Fix empty result pullup for ANTI joins (duckdb/duckdb#19701)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2448"
+#define DUCKDB_PATCH_VERSION "0-dev2455"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev2448"
+#define DUCKDB_VERSION "v1.5.0-dev2455"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c95fd2678a"
+#define DUCKDB_SOURCE_ID "287ac426ad"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
