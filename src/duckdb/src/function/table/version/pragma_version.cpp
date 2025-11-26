8fefd028adcaa2e2f76bbc31409f8e197f21c9e3
krlmlr <krlmlr@users.noreply.github.com>
2025-11-26 20:05:10 +0000

    vendor: Update vendored sources to duckdb/duckdb@a2ae69278e36a919880c521d18789dca9fb6f15a
    Add CacheBehavior::AUTOMATIC to DBInstanceCache that automatically does the right thing™ (duckdb/duckdb#19841)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev37"
+#define DUCKDB_PATCH_VERSION "3-dev39"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.3-dev37"
+#define DUCKDB_VERSION "v1.4.3-dev39"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ff2930fb30"
+#define DUCKDB_SOURCE_ID "a2ae69278e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
