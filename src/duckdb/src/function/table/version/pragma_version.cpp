3f15d61796179238f0db57a9e1f3535eb97673d7
krlmlr <krlmlr@users.noreply.github.com>
2025-11-26 14:05:08 +0000

    vendor: Update vendored sources to duckdb/duckdb@f68f1a555b5384a5b467bc466a71e5026adcab03
    Correctly use a lock when accessing the EncryptionKeyManager (duckdb/duckdb#19772)
    Remove httpfs patches and bump (duckdb/duckdb#19763)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev8"
+#define DUCKDB_PATCH_VERSION "3-dev12"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.3-dev8"
+#define DUCKDB_VERSION "v1.4.3-dev12"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "5f0c38c5eb"
+#define DUCKDB_SOURCE_ID "f68f1a555b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
