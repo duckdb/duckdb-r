16f50b1ec21747705f37c9cedc2acc318a08248e
krlmlr <krlmlr@users.noreply.github.com>
2025-12-01 21:04:25 +0000

    vendor: Update vendored sources to duckdb/duckdb@61e8c5b06d74802db643c0cea504bf4224c2ae56
    Take in consideration if the sniffer used unstrictness while selecting candidates (duckdb/duckdb#20005)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev174"
+#define DUCKDB_PATCH_VERSION "3-dev177"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.3-dev174"
+#define DUCKDB_VERSION "v1.4.3-dev177"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e5552390e9"
+#define DUCKDB_SOURCE_ID "61e8c5b06d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
