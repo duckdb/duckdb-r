3d85022207229a6ab3a5812814e00354cc3ae53c
krlmlr <krlmlr@users.noreply.github.com>
2025-12-01 04:23:30 +0000

    vendor: Update vendored sources to duckdb/duckdb@8fc3f2d6d3362f8e6a334db28b0ef7625ac4ab83
    Add `Stats` API to filesystem interfaces (duckdb/duckdb#19743)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2548"
+#define DUCKDB_PATCH_VERSION "0-dev2555"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev2548"
+#define DUCKDB_VERSION "v1.5.0-dev2555"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "737209ed29"
+#define DUCKDB_SOURCE_ID "8fc3f2d6d3"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
