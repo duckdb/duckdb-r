a6fac65b81b1b6f34d1af57a2b54156c95e69b28
krlmlr <krlmlr@users.noreply.github.com>
2025-12-10 14:11:36 +0000

    vendor: Update vendored sources to duckdb/duckdb@e92f41065ffbb8039282fd2397c542db71720a76
    [Storage] Fix NULL filter check for constant segments (duckdb/duckdb#20103)
    Cast Fix: Correctly handle negative exponent with a number with a decimal in VARCHAR -> INTEGER cast (duckdb/duckdb#20098)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3"
+#define DUCKDB_PATCH_VERSION "4-dev6"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.3"
+#define DUCKDB_VERSION "v1.4.4-dev6"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d1dc88f950"
+#define DUCKDB_SOURCE_ID "e92f41065f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
