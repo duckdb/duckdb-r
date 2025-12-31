61ca9b9c2d39ce2100f77494804a8ecb21b1cf0c
krlmlr <krlmlr@users.noreply.github.com>
2025-12-31 15:04:59 +0000

    vendor: Update vendored sources to duckdb/duckdb@2606b638022d09bb9dadbcdf1a53ff675b1d087b
    Add `struct_keys` method to StructVector (duckdb/duckdb#19673)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2941"
+#define DUCKDB_PATCH_VERSION "0-dev2961"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev2941"
+#define DUCKDB_VERSION "v1.5.0-dev2961"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "14c00e7194"
+#define DUCKDB_SOURCE_ID "2606b63802"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
