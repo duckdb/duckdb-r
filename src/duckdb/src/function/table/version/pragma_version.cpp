5ae2892d7077f852ee171e50caef06ec8a2e574a
krlmlr <krlmlr@users.noreply.github.com>
2025-12-31 08:06:11 +0000

    vendor: Update vendored sources to duckdb/duckdb@4fa6c74d24e997faafb317f28d03bec75dd1e39d
    CLI: Add table metadata rendering, and use this rendering for `.tables` and `DESCRIBE` queries (duckdb/duckdb#19792)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev2746"
+#define DUCKDB_PATCH_VERSION "0-dev2764"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev2746"
+#define DUCKDB_VERSION "v1.5.0-dev2764"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "8521dd47d7"
+#define DUCKDB_SOURCE_ID "4fa6c74d24"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
