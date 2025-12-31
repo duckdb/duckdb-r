08525c4c97095718ccdc7b9bdd9c4c3d7e6b4061
krlmlr <krlmlr@users.noreply.github.com>
2025-12-31 21:04:20 +0000

    vendor: Update vendored sources to duckdb/duckdb@f17e4aafe2dee8b4ffc0172622be4e7397f1b746
    duckdb/duckdb#6429 Roaring Booleans (duckdb/duckdb#19785)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3007"
+#define DUCKDB_PATCH_VERSION "0-dev3045"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev3007"
+#define DUCKDB_VERSION "v1.5.0-dev3045"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b9aed20677"
+#define DUCKDB_SOURCE_ID "f17e4aafe2"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
