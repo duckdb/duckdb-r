e33ff45c49ef924cddff159320867d09495e7588
krlmlr <krlmlr@users.noreply.github.com>
2025-12-31 15:05:00 +0000

    vendor: Update vendored sources to duckdb/duckdb@bacda75d6cbd87f303d112f5c611a629b55faeb3
    Fix view resolution not being stable if the referenced table lives in a different schema (duckdb/duckdb#20260)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev76"
+#define DUCKDB_PATCH_VERSION "4-dev85"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.4-dev76"
+#define DUCKDB_VERSION "v1.4.4-dev85"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "410ffa007b"
+#define DUCKDB_SOURCE_ID "bacda75d6c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
