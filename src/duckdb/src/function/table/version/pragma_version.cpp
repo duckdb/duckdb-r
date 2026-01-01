983bcc7e77f29d30efdff92866f7d47831b5458c
krlmlr <krlmlr@users.noreply.github.com>
2026-01-01 04:22:59 +0000

    vendor: Update vendored sources to duckdb/duckdb@9b445d1a25493b2073e56249fcd37017ed4a4c57
    [Parquet] Implement automatic shredding for `VARIANT` columns in COPY TO parquet (duckdb/duckdb#19336)
    [swift] decode boolean primitive type (duckdb/duckdb#19890)
    [Testing][Dev] Extend tests covered by the `storage_compatibility` test config (duckdb/duckdb#19860)
    PEG Transformer template script (duckdb/duckdb#19812)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3152"
+#define DUCKDB_PATCH_VERSION "0-dev3205"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev3152"
+#define DUCKDB_VERSION "v1.5.0-dev3205"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d2cbbf369a"
+#define DUCKDB_SOURCE_ID "9b445d1a25"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
