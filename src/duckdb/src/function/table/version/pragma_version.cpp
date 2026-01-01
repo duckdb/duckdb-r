3040b21994b8a3770a99b73bbbd524751f2cf341
krlmlr <krlmlr@users.noreply.github.com>
2026-01-01 09:07:07 +0000

    vendor: Update vendored sources to duckdb/duckdb@5246f9858655690cd4aeee8e4ad01ee7301e79f4
    [Dev] Fix assertion failure in `AlterType` (duckdb/duckdb#19850)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3245"
+#define DUCKDB_PATCH_VERSION "0-dev3249"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev3245"
+#define DUCKDB_VERSION "v1.5.0-dev3249"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "142b128bfb"
+#define DUCKDB_SOURCE_ID "5246f98586"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
