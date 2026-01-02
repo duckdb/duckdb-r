7f145aa6dc2091e357e7d44d3947c0b080711c93
krlmlr <krlmlr@users.noreply.github.com>
2026-01-02 07:06:59 +0000

    vendor: Update vendored sources to duckdb/duckdb@cf1cf35a4c9617ab05c49a73715cd1aeaa772218
    fix: allow table in/out functions to indicate if they support parallelism (duckdb/duckdb#19951)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3778"
+#define DUCKDB_PATCH_VERSION "0-dev3782"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev3778"
+#define DUCKDB_VERSION "v1.5.0-dev3782"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "79e7080cf3"
+#define DUCKDB_SOURCE_ID "cf1cf35a4c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
