580214f8a2f8f15f6a56d74bc7b785ceeccc67ff
krlmlr <krlmlr@users.noreply.github.com>
2026-01-02 22:04:23 +0000

    vendor: Update vendored sources to duckdb/duckdb@a40c629f0488a8576a747978a30bbfffcfdad39a
    Enable Parquet reader support for uniformly encrypted Parquet files written by Arrow C++ API (duckdb/duckdb#20046)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev4096"
+#define DUCKDB_PATCH_VERSION "0-dev4120"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev4096"
+#define DUCKDB_VERSION "v1.5.0-dev4120"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a5114158b7"
+#define DUCKDB_SOURCE_ID "a40c629f04"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
