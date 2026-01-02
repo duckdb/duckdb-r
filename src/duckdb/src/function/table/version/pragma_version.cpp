ed5bfcba891cf89bd213fc12d801665bc8742fb3
krlmlr <krlmlr@users.noreply.github.com>
2026-01-02 17:04:46 +0000

    vendor: Update vendored sources to duckdb/duckdb@07d41f48b92408c594c6bcc4bc330619fc92151d
    Eagerly Execute Min/Max Aggregates on Statistics (duckdb/duckdb#19906)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3984"
+#define DUCKDB_PATCH_VERSION "0-dev4007"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev3984"
+#define DUCKDB_VERSION "v1.5.0-dev4007"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "104c8d0842"
+#define DUCKDB_SOURCE_ID "07d41f48b9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
