739f2fe2b5987f6be2d2ffb123597472ae3809f2
krlmlr <krlmlr@users.noreply.github.com>
2026-01-02 10:05:15 +0000

    vendor: Update vendored sources to duckdb/duckdb@26437d973411bdda2611369d2d01a246fe92adbd
    fix alter table rename / alter on temp table losing temporary flag (duckdb/duckdb#20011)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev3822"
+#define DUCKDB_PATCH_VERSION "0-dev3826"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev3822"
+#define DUCKDB_VERSION "v1.5.0-dev3826"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "026109d42c"
+#define DUCKDB_SOURCE_ID "26437d9734"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
