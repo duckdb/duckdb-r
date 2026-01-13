65f95fbaef77091305f212ac7450ad1ed875ec41
krlmlr <krlmlr@users.noreply.github.com>
2026-01-13 16:07:33 +0000

    vendor: Update vendored sources to duckdb/duckdb@6ef04d6ec477cd8fb0c6c65018d44987c2aa7869
    Reset cached dictionaries in `TRY` expression (duckdb/duckdb#20452)
    Issue duckdb/duckdb#20470: TIMESTAMPTZ to DATE (duckdb/duckdb#20498)
    rewrite unaligned scan (duckdb/duckdb#20474)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev210"
+#define DUCKDB_PATCH_VERSION "4-dev222"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.4-dev210"
+#define DUCKDB_VERSION "v1.4.4-dev222"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "6f3381c403"
+#define DUCKDB_SOURCE_ID "6ef04d6ec4"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
