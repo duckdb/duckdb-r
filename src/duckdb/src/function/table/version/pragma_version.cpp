6eacd602e16587517f013c780e1f42d419be4364
krlmlr <krlmlr@users.noreply.github.com>
2026-01-22 09:10:40 +0000

    vendor: Update vendored sources to duckdb/duckdb@6ddac802ffa9bcfbcc3f5f0d71de5dff9b0bc250
    Fail fast extensions (duckdb/duckdb#20605)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev256"
+#define DUCKDB_PATCH_VERSION "4-dev272"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.4-dev256"
+#define DUCKDB_VERSION "v1.4.4-dev272"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e3b14d4d3b"
+#define DUCKDB_SOURCE_ID "6ddac802ff"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
