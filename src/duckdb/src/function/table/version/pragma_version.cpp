d93c107e5bd2a1b67dbf719ea99b8a7cfc650f0d
Kirill Müller <kirill@cynkra.com>
2026-01-26 16:46:43 +0100

    vendor: Update vendored sources (tag v1.4.4) to duckdb/duckdb@6ddac802ffa9bcfbcc3f5f0d71de5dff9b0bc250

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev272"
+#define DUCKDB_PATCH_VERSION "4"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,7 +8,7 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.4-dev272"
+#define DUCKDB_VERSION "v1.4.4"
 #endif
 #ifndef DUCKDB_SOURCE_ID
 #define DUCKDB_SOURCE_ID "6ddac802ff"
