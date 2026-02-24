5e12f063798aa5680af795dfbd7ba5f223d52634
krlmlr <krlmlr@users.noreply.github.com>
2026-02-24 12:26:11 +0000

    vendor: Update vendored sources to duckdb/duckdb@ebad5254ea8ce0750afac939d52ab81951b857a8
    Fix try cast for variant to incompatible struct types (duckdb/duckdb#20211)
    Add days_in_month  internal macro for retrieving number of days in a month (duckdb/duckdb#20180)
    fix: simplify platform detection in CMakeLists.txt (duckdb/duckdb#20183)
    fix: simplify logic by removing empty else branch (duckdb/duckdb#20184)
    fix ci run - 'no space left on device' (duckdb/duckdb#20203)
    Link MSVC runtime library statically (duckdb/duckdb#19415)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev4588"
+#define DUCKDB_PATCH_VERSION "0-dev4600"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev4588"
+#define DUCKDB_VERSION "v1.5.0-dev4600"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "19e969f163"
+#define DUCKDB_SOURCE_ID "ebad5254ea"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
