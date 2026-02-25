b6ad7ab6cb51be35b0b1066cb8d04be69739aaa8
krlmlr <krlmlr@users.noreply.github.com>
2026-02-25 13:56:16 +0000

    vendor: Update vendored sources to duckdb/duckdb@2370bf6fd7418606943ea54a3a52aa0019f10aef
    Big-endian patches (FSST, arrow, types, md5) (duckdb/duckdb#20237)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev4906"
+#define DUCKDB_PATCH_VERSION "0-dev4913"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev4906"
+#define DUCKDB_VERSION "v1.5.0-dev4913"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e6cbdcaa65"
+#define DUCKDB_SOURCE_ID "2370bf6fd7"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
