13e481e20d4558b03234f1185a7ce92843255d47
krlmlr <krlmlr@users.noreply.github.com>
2026-02-25 16:41:51 +0000

    vendor: Update vendored sources to duckdb/duckdb@210fbd853b3e0b8e3d25d2591470bf39293585ef
    Allow concurrent deletes while checkpointing (duckdb/duckdb#20286)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev4962"
+#define DUCKDB_PATCH_VERSION "0-dev4995"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev4962"
+#define DUCKDB_VERSION "v1.5.0-dev4995"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e1878adb0d"
+#define DUCKDB_SOURCE_ID "210fbd853b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
