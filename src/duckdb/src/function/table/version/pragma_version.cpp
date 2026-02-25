cd5f39624b1e00102924101908f8fd6ca9b6248e
krlmlr <krlmlr@users.noreply.github.com>
2026-02-25 09:19:19 +0000

    vendor: Update vendored sources to duckdb/duckdb@7f6b657e32fb8a5be83157f2853e1dbc80d1749f
    feat: preserve ATTACH options and return via duckdb_databases() (duckdb/duckdb#20186)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev4865"
+#define DUCKDB_PATCH_VERSION "0-dev4868"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev4865"
+#define DUCKDB_VERSION "v1.5.0-dev4868"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1e5f6c8932"
+#define DUCKDB_SOURCE_ID "7f6b657e32"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
