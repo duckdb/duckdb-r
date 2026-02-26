113d67f715b2dd1b1b255db3b23d66bcafcc1103
krlmlr <krlmlr@users.noreply.github.com>
2026-02-26 09:18:27 +0000

    vendor: Update vendored sources to duckdb/duckdb@6b9c556c5326497471774ebf220b4d4899ac2232
    Parquet: Populate the column_orders field and expose it in parquet_file_metadata (duckdb/duckdb#20241)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev5159"
+#define DUCKDB_PATCH_VERSION "0-dev5163"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev5159"
+#define DUCKDB_VERSION "v1.5.0-dev5163"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4b7a6b7bd0"
+#define DUCKDB_SOURCE_ID "6b9c556c53"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
