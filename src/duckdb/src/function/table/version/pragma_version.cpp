0762a5bb4e924595480b208bd05d84ae6646cdb5
krlmlr <krlmlr@users.noreply.github.com>
2026-02-27 18:13:56 +0000

    vendor: Update vendored sources to duckdb/duckdb@5438deaaac9860b37985be4943326e390fa5087a
    Expose ArenaAllocator from PlanGenerator (duckdb/duckdb#20447)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev5308"
+#define DUCKDB_PATCH_VERSION "0-dev5310"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev5308"
+#define DUCKDB_VERSION "v1.5.0-dev5310"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "48bdbbebe4"
+#define DUCKDB_SOURCE_ID "5438deaaac"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
