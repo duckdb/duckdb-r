ba67bd91285b729cda242b46bb47da71815a0c92
krlmlr <krlmlr@users.noreply.github.com>
2026-02-27 13:57:07 +0000

    vendor: Update vendored sources to duckdb/duckdb@1f37984c7318b5a9c6cf864bc8ba75c4f4969cd5
    feat(adbc): support the experimental `adbc.ingest.target_catalog` option and improve ingest name resolution (duckdb/duckdb#20369)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev5296"
+#define DUCKDB_PATCH_VERSION "0-dev5302"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev5296"
+#define DUCKDB_VERSION "v1.5.0-dev5302"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ee0b06f6f0"
+#define DUCKDB_SOURCE_ID "1f37984c73"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
