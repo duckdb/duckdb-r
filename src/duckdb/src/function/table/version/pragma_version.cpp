ed644a288200156cec973ff3037ed0912c7ed51f
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:27:07 +0100

    vendor: Update vendored sources to duckdb/duckdb@ec482fd35127e605be37ebf9e23440840818af80
    Date: 2026-01-24 13:36:47 +0100
    
    Fix position in unique constraint variegata (duckdb/duckdb#20651)
    Add missing import of type_visitor.hpp (duckdb/duckdb#20654)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6077"
+#define DUCKDB_PATCH_VERSION "0-dev6082"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6077"
+#define DUCKDB_VERSION "v1.5.0-dev6082"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "56dbca7f17c"
+#define DUCKDB_SOURCE_ID "ec482fd3512"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
