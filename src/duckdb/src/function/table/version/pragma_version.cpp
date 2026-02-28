d4cd7515717d40c1e4f5644f30cd2b9e76e63ea9
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:55:54 +0100

    vendor: Update vendored sources to duckdb/duckdb@8f1be00cd598886697fcb94e906e4dfb52f1e50b
    Date: 2026-02-06 09:09:58 +0100
    
    Internal duckdb/duckdb#7326: IEJoin NextValid Fencepost (duckdb/duckdb#20815)
    Prevent warnings being printed multiple times in the CLI (duckdb/duckdb#20806)
    Remove index list assertion (duckdb/duckdb#20827)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6963"
+#define DUCKDB_PATCH_VERSION "0-dev6971"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6963"
+#define DUCKDB_VERSION "v1.5.0-dev6971"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "6bbef69c8a6"
+#define DUCKDB_SOURCE_ID "8f1be00cd59"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
