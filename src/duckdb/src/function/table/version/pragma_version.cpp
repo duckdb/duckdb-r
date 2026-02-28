5de5d465f74bc692a3f2e2f5be8b9c5090f23955
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:52:16 +0100

    vendor: Update vendored sources to duckdb/duckdb@6f5082b998d10c4e84806d606b1c4b913f8074e9
    Date: 2026-01-27 11:05:28 +0100
    
    UnalignedScan CI failure fix (duckdb/duckdb#20671)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6258"
+#define DUCKDB_PATCH_VERSION "0-dev6262"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6258"
+#define DUCKDB_VERSION "v1.5.0-dev6262"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "bb8ff0eb5dd"
+#define DUCKDB_SOURCE_ID "6f5082b998d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
