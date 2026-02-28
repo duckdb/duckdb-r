db4aacd63a6c3603e7e52d0b14f3de80fa69b447
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:52:50 +0100

    vendor: Update vendored sources to duckdb/duckdb@04d9758c8a5c87e591351cb7abf69833d327f7c5
    Date: 2026-01-29 10:13:04 +0100
    
    rename uc_catalog to unity_catalog (duckdb/duckdb#20712)
    avro.cmake: don't DONT_LINK (duckdb/duckdb#20711)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6354"
+#define DUCKDB_PATCH_VERSION "0-dev6360"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6354"
+#define DUCKDB_VERSION "v1.5.0-dev6360"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c7a379d5908"
+#define DUCKDB_SOURCE_ID "04d9758c8a5"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
