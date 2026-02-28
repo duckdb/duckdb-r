0a1f19e7fc1d387a45598e4e08fc48d1f9da6887
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:56:04 +0100

    vendor: Update vendored sources to duckdb/duckdb@22c7bd339cf90a74068fbb38fb97eec4f4279963
    Date: 2026-02-07 12:03:17 +0100
    
    Reintroduce debug code for UnalignedScan (duckdb/duckdb#20738)
    Start CLI search with what is already in buffer [Retarget to v1.5-variegata] (duckdb/duckdb#20835)
    Fix CI variegata (duckdb/duckdb#20836)
    Bump: httplib (duckdb/duckdb#20299)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7005"
+#define DUCKDB_PATCH_VERSION "0-dev7024"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev7005"
+#define DUCKDB_VERSION "v1.5.0-dev7024"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ebf0f8fde42"
+#define DUCKDB_SOURCE_ID "22c7bd339cf"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
