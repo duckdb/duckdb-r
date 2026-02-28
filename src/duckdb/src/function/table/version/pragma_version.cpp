63892bbd3a129af088d8e4b9030383abea6ddc1c
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:55:18 +0100

    vendor: Update vendored sources to duckdb/duckdb@bb01275c0a89b2cea65eb20c2477f09952672875
    Date: 2026-02-03 18:09:36 +0100
    
    Block handle refactor to support eviction queue operations while detaching (duckdb/duckdb#20724)
    Revert "[chore] Skip building spatial, needs a patch, needs to be rev… (duckdb/duckdb#20786)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6722"
+#define DUCKDB_PATCH_VERSION "0-dev6739"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6722"
+#define DUCKDB_VERSION "v1.5.0-dev6739"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "da4266e3b0f"
+#define DUCKDB_SOURCE_ID "bb01275c0a8"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
