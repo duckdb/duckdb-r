f6edfe8207e00e9be26c40a7fe45fb5bed33e5b4
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:55:22 +0100

    vendor: Update vendored sources to duckdb/duckdb@b6d84909c3b42312afe3802943e7852ac0f189e0
    Date: 2026-02-04 01:51:56 -0600
    
    `SELECT` statement for the PEG Transformer (duckdb/duckdb#20776)
    Bump storage version to `v1.5` (duckdb/duckdb#20780)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6739"
+#define DUCKDB_PATCH_VERSION "0-dev6845"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6739"
+#define DUCKDB_VERSION "v1.5.0-dev6845"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "bb01275c0a8"
+#define DUCKDB_SOURCE_ID "b6d84909c3b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
