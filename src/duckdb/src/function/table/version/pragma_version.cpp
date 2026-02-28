7403231ce53499a62dc4f00ce65ead07e8e2d2bf
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:52:43 +0100

    vendor: Update vendored sources to duckdb/duckdb@cc7c620e58625f6b5f53f724b6a9645f4ee0be6f
    Date: 2026-01-28 16:34:24 +0100
    
    Pass down optional_ptr<ClientContext> into LazyMultiFileList, and if available use to check interrupts (duckdb/duckdb#20661)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6340"
+#define DUCKDB_PATCH_VERSION "0-dev6344"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6340"
+#define DUCKDB_VERSION "v1.5.0-dev6344"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b62c70b08bf"
+#define DUCKDB_SOURCE_ID "cc7c620e586"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
