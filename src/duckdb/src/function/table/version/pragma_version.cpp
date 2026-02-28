74409dad02ebc101ea4339fffbb595c5b0ce40c2
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:52:55 +0100

    vendor: Update vendored sources to duckdb/duckdb@12944d904cb14ed142c6fedda8f7dd6876d53f1d
    Date: 2026-01-29 11:31:15 +0100
    
    Allow lazy binding of views, and no longer require views to rigidly have the same schema (return types / names) (duckdb/duckdb#20696)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6384"
+#define DUCKDB_PATCH_VERSION "0-dev6414"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6384"
+#define DUCKDB_VERSION "v1.5.0-dev6414"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "46e0a2756d7"
+#define DUCKDB_SOURCE_ID "12944d904cb"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
