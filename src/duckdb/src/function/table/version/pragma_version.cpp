0c51e2021c2b855d8c426c826d7f82177062389f
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:53:30 +0100

    vendor: Update vendored sources to duckdb/duckdb@a12b16c5c1b5b6d1a3f678450d58c63ee07e1cce
    Date: 2026-02-02 13:44:38 +0100
    
    Integrate caching filesystem into json reader (duckdb/duckdb#20575)
    [chore] Skip building spatial, needs a patch, needs to be reverted (duckdb/duckdb#20772)
    Add back spatial to V1.5, patch ducklake (duckdb/duckdb#20734)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6616"
+#define DUCKDB_PATCH_VERSION "0-dev6632"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6616"
+#define DUCKDB_VERSION "v1.5.0-dev6632"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "cd19175d109"
+#define DUCKDB_SOURCE_ID "a12b16c5c1b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
