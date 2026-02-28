3f6ee0eceb29689f3bf9e5624597566d5e0b1544
Kirill Müller <kirill@cynkra.com>
2026-02-28 15:51:00 +0100

    vendor: Update vendored sources to duckdb/duckdb@ada11940c2aea3e2d21ef620a0cfa2697914a47b
    Add max_execution_time setting for per-connection query time limit (duckdb/duckdb#20585)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6077"
+#define DUCKDB_PATCH_VERSION "0-dev6080"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6077"
+#define DUCKDB_VERSION "v1.5.0-dev6080"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1c27c54f27b"
+#define DUCKDB_SOURCE_ID "ada11940c2a"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
