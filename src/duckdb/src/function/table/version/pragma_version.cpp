8fee29c1def5856934b0d8fd9d078591c9771ea7
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:53:00 +0100

    vendor: Update vendored sources to duckdb/duckdb@23e49b3312b6f9cc59080521ac1fc4f5a9495de1
    Date: 2026-01-29 14:33:11 +0100
    
    [Parquet] Add VARIANT stats + various fixes for reading/writing stats (duckdb/duckdb#20623)
    Fix Index Reuse in List Comprehension (2) (duckdb/duckdb#20581)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6416"
+#define DUCKDB_PATCH_VERSION "0-dev6464"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6416"
+#define DUCKDB_VERSION "v1.5.0-dev6464"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b39d7247332"
+#define DUCKDB_SOURCE_ID "23e49b3312b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
