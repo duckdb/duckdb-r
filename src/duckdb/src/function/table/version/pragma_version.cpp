169158da4de6cc60eacf5dad728abd0c511a10ce
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:47:53 +0100

    vendor: Update vendored sources to duckdb/duckdb@af8e16bcd391a7969ecc1042593cea8ec088884d
    Date: 2026-01-24 13:49:14 +0100
    
    Respect __HIVE_DEFAULT_PARTITION__ for null values per Hive standard (duckdb/duckdb#20512)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6227"
+#define DUCKDB_PATCH_VERSION "0-dev6230"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6227"
+#define DUCKDB_VERSION "v1.5.0-dev6230"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "dc2bbb453e1"
+#define DUCKDB_SOURCE_ID "af8e16bcd39"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
