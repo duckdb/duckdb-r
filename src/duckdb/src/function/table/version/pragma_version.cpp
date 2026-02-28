68f9fad7a4f7b242fe4b838339f5cce7793a4e2a
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:51:49 +0100

    vendor: Update vendored sources to duckdb/duckdb@c5e243b5ac32c7bda2d79d0ff69b443345fb28e4
    Date: 2026-01-27 08:23:00 +0100
    
    Run `TransformStringToLogicalType` in transaction (duckdb/duckdb#20666)
    Split path in test correctly (duckdb/duckdb#20668)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6240"
+#define DUCKDB_PATCH_VERSION "0-dev6248"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6240"
+#define DUCKDB_VERSION "v1.5.0-dev6248"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ef79b3937f1"
+#define DUCKDB_SOURCE_ID "c5e243b5ac3"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
