6944e7cb9ccf658f5f17e32f6f535d73ca99c0c8
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:56:40 +0100

    vendor: Update vendored sources to duckdb/duckdb@1c3aa53f4af83ce3af98ba784f778f8f4dd88198
    Date: 2026-02-09 13:26:43 +0100
    
    IndexStorageInfo Lifetimes (duckdb/duckdb#20611)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7047"
+#define DUCKDB_PATCH_VERSION "0-dev7053"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev7047"
+#define DUCKDB_VERSION "v1.5.0-dev7053"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4a7aee98bc4"
+#define DUCKDB_SOURCE_ID "1c3aa53f4af"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
