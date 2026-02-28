665d40e30e7b394bccb16042aeeb53b5ac1bedcc
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:52:24 +0100

    vendor: Update vendored sources to duckdb/duckdb@9a22f042c26da9fa1aacf2781758eb0d1ae42a1e
    Date: 2026-01-28 08:45:13 +0100
    
    Add back `UnregisterSubSystem` and make `ExtractSubSystem` work with `OpenerFileSystem` (duckdb/duckdb#20694)
    Rewrite many older concurrent tests to SQLLogicTests (duckdb/duckdb#20693)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6291"
+#define DUCKDB_PATCH_VERSION "0-dev6315"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6291"
+#define DUCKDB_VERSION "v1.5.0-dev6315"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "5b798a76e5a"
+#define DUCKDB_SOURCE_ID "9a22f042c26"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
