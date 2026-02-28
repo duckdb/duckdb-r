c636b28e8a2ad1f28c55e758482fa7f4d0c05e20
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:19:03 +0100

    vendor: Update vendored sources to duckdb/duckdb@56dbca7f17cc306a570dbb301950b2caa38ee2c8
    Fix group column binding order in TopNWindowElimination optimizer (duckdb/duckdb#20653)
    test/issues/general/test_1248.test: remove require vector_size 2048 (duckdb/duckdb#20647)
    Issue duckdb/duckdb#20608: EXCEPT Window Collations (duckdb/duckdb#20645)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6071"
+#define DUCKDB_PATCH_VERSION "0-dev6077"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6071"
+#define DUCKDB_VERSION "v1.5.0-dev6077"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "32940e6025a"
+#define DUCKDB_SOURCE_ID "56dbca7f17c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
