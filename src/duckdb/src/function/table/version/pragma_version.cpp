4517e10c8047e8a79219b8715b5678ad3caf9457
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:53:09 +0100

    vendor: Update vendored sources to duckdb/duckdb@09d90410682b32ffe6e9a7b03cdfae0e9f6e1e2d
    Date: 2026-01-30 12:55:41 +0100
    
    Avoid triggering optimistic writes during WAL replay in read-only mode (duckdb/duckdb#20731)
    Upgrade ducklake (duckdb/duckdb#20740)
    Bump httpfs: now with support for bulk deletes (duckdb/duckdb#20735)
    V1.4 -> v1.5 (duckdb/duckdb#20736)
    Bump httpfs, remove patches, fix Main.yml (duckdb/duckdb#20723)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6464"
+#define DUCKDB_PATCH_VERSION "0-dev6477"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6464"
+#define DUCKDB_VERSION "v1.5.0-dev6477"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "23e49b3312b"
+#define DUCKDB_SOURCE_ID "09d90410682"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
