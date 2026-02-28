5f8c4af3aca1afe29a91f39ad67161275a99a90c
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:54:00 +0100

    vendor: Update vendored sources to duckdb/duckdb@75a25b5404700262f965129571a1995fb802242d
    Date: 2026-02-02 19:33:40 +0100
    
    Pass client context to index constructor (duckdb/duckdb#20778)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6677"
+#define DUCKDB_PATCH_VERSION "0-dev6679"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6677"
+#define DUCKDB_VERSION "v1.5.0-dev6679"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "992df353dfd"
+#define DUCKDB_SOURCE_ID "75a25b54047"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
