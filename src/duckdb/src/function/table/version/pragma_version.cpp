9f41e62a32164038dc087aa5539eb9c2ffacded2
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:52:52 +0100

    vendor: Update vendored sources to duckdb/duckdb@dcf48cc2be96c3af7ccd8e626b06366d10bebd48
    Date: 2026-01-29 10:28:11 +0100
    
    [C API] unsafe utf8 string functions, valid_ut8_check, and tests (duckdb/duckdb#20692)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6360"
+#define DUCKDB_PATCH_VERSION "0-dev6378"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6360"
+#define DUCKDB_VERSION "v1.5.0-dev6378"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "04d9758c8a5"
+#define DUCKDB_SOURCE_ID "dcf48cc2be9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
