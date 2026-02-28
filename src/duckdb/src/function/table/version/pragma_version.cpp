79819db7bcc3fbfcb146e8eadbb5cf5fbfb83b77
Kirill Müller <kirill@cynkra.com>
2026-02-28 16:51:53 +0100

    vendor: Update vendored sources to duckdb/duckdb@bb8ff0eb5dd74df0560c4605ca5fa58855ee0571
    Date: 2026-01-27 10:43:32 +0100
    
    [chore] Move appender query logic into the appender (base) class (duckdb/duckdb#20640)
    bump httpfs (duckdb/duckdb#20677)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev6248"
+#define DUCKDB_PATCH_VERSION "0-dev6258"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev6248"
+#define DUCKDB_VERSION "v1.5.0-dev6258"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c5e243b5ac3"
+#define DUCKDB_SOURCE_ID "bb8ff0eb5dd"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
