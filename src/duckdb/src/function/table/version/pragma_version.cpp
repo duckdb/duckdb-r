9e92f4522294d3af5d66323cd112fab4586f9056
Kirill Müller <kirill@cynkra.com>
2026-03-01 08:54:03 +0100

    vendor: Update vendored sources to duckdb/duckdb@c2fb9b2e5817bfae2ddb82ce199f7b2dcc16c2a5
    Date: 2026-02-18 08:27:16 +0100
    
    MetricType enum generation backwards compatibility for 1.4+ (duckdb/duckdb#20988)
    Bump avro (duckdb/duckdb#20985)
    Bump sqlite_scanner (duckdb/duckdb#20990)
    Bump vss (duckdb/duckdb#20993)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7279"
+#define DUCKDB_PATCH_VERSION "0-dev7300"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev7279"
+#define DUCKDB_VERSION "v1.5.0-dev7300"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "0c6fc7a36a3"
+#define DUCKDB_SOURCE_ID "c2fb9b2e581"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
