e76746bf0f8306342e3ba8c942960e08a89bfad6
Kirill Müller <kirill@cynkra.com>
2026-03-01 08:55:04 +0100

    vendor: Update vendored sources to duckdb/duckdb@e79a6b6c075c57afd134c6d8c08cbb66ba404ae9
    Date: 2026-02-25 17:08:11 +0100
    
    [Fix] WAL corruption when reusing optimistically written blocks too optimistically ;) (duckdb/duckdb#21067)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7479"
+#define DUCKDB_PATCH_VERSION "0-dev7487"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev7479"
+#define DUCKDB_VERSION "v1.5.0-dev7487"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "7baa30c39b8"
+#define DUCKDB_SOURCE_ID "e79a6b6c075"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
