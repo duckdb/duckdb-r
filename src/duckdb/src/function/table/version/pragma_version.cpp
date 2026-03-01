afdcb2d3c9aa7e4c5390579f799e63f4a9e1be84
Kirill Müller <kirill@cynkra.com>
2026-03-01 08:52:47 +0100

    vendor: Update vendored sources to duckdb/duckdb@895a60bdbe000bbe839418f21c63c4a6c1a0f73e
    Date: 2026-02-11 08:31:50 +0100
    
    feat(adbc): implement cancellation of ADBC 1.1.0 (duckdb/duckdb#20511)
    Fix ASAN Violation container-overflow (duckdb/duckdb#20894)
    Don't automatically enable pager for `.mode [ascii|markdown|html|insert|...]` (duckdb/duckdb#20896)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7109"
+#define DUCKDB_PATCH_VERSION "0-dev7137"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev7109"
+#define DUCKDB_VERSION "v1.5.0-dev7137"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "628931ddb7b"
+#define DUCKDB_SOURCE_ID "895a60bdbe0"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
