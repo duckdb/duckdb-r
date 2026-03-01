30c6e788afe8edf338bbebadde5fe6bcc04fba21
Kirill Müller <kirill@cynkra.com>
2026-03-01 08:54:52 +0100

    vendor: Update vendored sources to duckdb/duckdb@4786ad8f5ad4c8fda50c71ba16953b9c7333a0ee
    Date: 2026-02-24 08:30:45 +0100
    
    Better debug error message for missing compression functions (duckdb/duckdb#21050)
    skip encryption fallback test (duckdb/duckdb#21047)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7420"
+#define DUCKDB_PATCH_VERSION "0-dev7424"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev7420"
+#define DUCKDB_VERSION "v1.5.0-dev7424"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "966952524a6"
+#define DUCKDB_SOURCE_ID "4786ad8f5ad"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
