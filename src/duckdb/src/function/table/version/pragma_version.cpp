eeb3d2b81c7fd07e7d2144dbe2b5afd008a5bc82
Kirill Müller <kirill@cynkra.com>
2026-03-01 08:54:05 +0100

    vendor: Update vendored sources to duckdb/duckdb@7f2b2c53cfc49bac42ef2e708fbd0a8fc796efa1
    Date: 2026-02-18 08:27:33 +0100
    
    Internal duckdb/duckdb#7480: Nth_Value Zero-Based Confusion (duckdb/duckdb#20980)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7300"
+#define DUCKDB_PATCH_VERSION "0-dev7305"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev7300"
+#define DUCKDB_VERSION "v1.5.0-dev7305"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c2fb9b2e581"
+#define DUCKDB_SOURCE_ID "7f2b2c53cfc"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
