05c51b60d852a1308c0264013992e067dc001e19
Kirill Müller <kirill@cynkra.com>
2026-03-01 08:53:25 +0100

    vendor: Update vendored sources to duckdb/duckdb@a2ff7341bad9d04b4378bfce6b631d4dec317b87
    Date: 2026-02-16 08:38:06 +0100
    
    Add HTTPClient::Cleanup, currently nop but can be implemented to perform post-usage cleanup (duckdb/duckdb#20954)
    [v1.5-variegata] Bump httpfs further (duckdb/duckdb#20948)
    Re-enable Wasm builds for iceberg for v1.5-variegata (duckdb/duckdb#20946)
    Add opt-in for musl libc extensions build (duckdb/duckdb#20931)
    Update odbc_scanner to fix CMake build race (duckdb/duckdb#20930)
    Add option `enable_peg_parser` (duckdb/duckdb#20837)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7214"
+#define DUCKDB_PATCH_VERSION "0-dev7230"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev7214"
+#define DUCKDB_VERSION "v1.5.0-dev7230"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "23a8774245b"
+#define DUCKDB_SOURCE_ID "a2ff7341bad"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
