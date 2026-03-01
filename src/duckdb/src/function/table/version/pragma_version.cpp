63d18b1986cee962549f95dc3d86294e04eec57b
Kirill Müller <kirill@cynkra.com>
2026-03-01 08:53:14 +0100

    vendor: Update vendored sources to duckdb/duckdb@23a8774245bf03fe641879c55bab83256ef79dec
    Date: 2026-02-14 07:40:52 +0100
    
    Make untyped value vector optional in shredded variants, and allow primitive types to be inlined (duckdb/duckdb#20951)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7207"
+#define DUCKDB_PATCH_VERSION "0-dev7214"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev7207"
+#define DUCKDB_VERSION "v1.5.0-dev7214"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f480e781694"
+#define DUCKDB_SOURCE_ID "23a8774245b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
