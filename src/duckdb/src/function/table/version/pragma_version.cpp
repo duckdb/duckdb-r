1124262c24e598dc1a4d9929eb3d617acb193980
Kirill Müller <kirill@cynkra.com>
2026-03-01 08:55:18 +0100

    vendor: Update vendored sources to duckdb/duckdb@9d65d875decde94dd8fe15cabc28324d3ab25be2
    Date: 2026-02-27 14:05:10 +0100
    
    fix(adbc): ADBC GetObjects should return empty list instead of null (duckdb/duckdb#21018)
    Internal duckdb/duckdb#7585: AsOf Simple Joins (duckdb/duckdb#21091)
    [JSON] Fix problem that was causing bogus UBIGINT values to be emitted from JSON "null" values (duckdb/duckdb#21098)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7504"
+#define DUCKDB_PATCH_VERSION "0-dev7510"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev7504"
+#define DUCKDB_VERSION "v1.5.0-dev7510"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "9475963fd98"
+#define DUCKDB_SOURCE_ID "9d65d875dec"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
