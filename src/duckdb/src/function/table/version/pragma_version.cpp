4ef9cc7c43ff06d9c54716b2c68bfb2331369b80
Kirill Müller <kirill@cynkra.com>
2026-03-01 08:53:33 +0100

    vendor: Update vendored sources to duckdb/duckdb@e10e37bc42eaefb4421a55746a4c6c767db07c6f
    Date: 2026-02-16 10:42:18 +0100
    
    Fix missing profiling metrics in EXPLAIN ANALYZE JSON output (duckdb/duckdb#20846)
    [Optimizer] Fix problem with struct extract pushdown (duckdb/duckdb#20945)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev7234"
+#define DUCKDB_PATCH_VERSION "0-dev7244"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.0-dev7234"
+#define DUCKDB_VERSION "v1.5.0-dev7244"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "928e9127e2d"
+#define DUCKDB_SOURCE_ID "e10e37bc42e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
