5b8813e72524666675b61017c952768d7591016a
Kirill Müller <kirill@cynkra.com>
2026-03-11 06:40:26 +0100

    Replace package name

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,12 @@
-library("testthat")
-library("DBI")
+# This file is part of the standard setup for testthat.
+# It is recommended that you do not modify it.
+#
+# Where should you do additional test configuration?
+# Learn more about the roles of various files in:
+# * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
+# * https://testthat.r-lib.org/articles/special-files.html
 
-test_check("duckdb")
+library(testthat)
+library(duckdb.1.4)
+
+test_check("duckdb.1.4")
