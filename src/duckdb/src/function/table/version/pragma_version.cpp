e3f223fe8e90767e69536b7ebeed12395533b719
krlmlr <krlmlr@users.noreply.github.com>
2026-03-18 03:17:34 +0000

    vendor: Update vendored sources to duckdb/duckdb@c2890df019ea9de091b5e5af5f8a54e751f13c88
    Date: 2026-03-11 08:35:13 +0100
    
    Lazy call `mmap` in `BlockAllocator` (duckdb/duckdb#21276)
    Switch MAIN_BRANCH_VERSIONING to False for v1.5-variegata branch (duckdb/duckdb#21239)
    Re-add accidentally removed -jsonlines command line parameter (duckdb/duckdb#21263)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,17 +1,17 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev64"
+#define DUCKDB_PATCH_VERSION "1-dev71"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
-#define DUCKDB_MINOR_VERSION 6
+#define DUCKDB_MINOR_VERSION 5
 #endif
 #ifndef DUCKDB_MAJOR_VERSION
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.6.0-dev64"
+#define DUCKDB_VERSION "v1.5.1-dev71"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "180aa69cc0"
+#define DUCKDB_SOURCE_ID "c2890df019"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
