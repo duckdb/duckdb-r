7c5ed4d1efc7f92cf05052f1aef3fd6ba170b066
krlmlr <krlmlr@users.noreply.github.com>
2026-03-18 04:40:58 +0000

    vendor: Update vendored sources to duckdb/duckdb@1d5b25613c4bae5f72e5db9c0c22358e43e43c1c
    Date: 2026-03-11 08:37:17 +0100
    
    Avoid potential division by 0 in `PhysicalHashJoin::PrepareFinalize` (duckdb/duckdb#21271)
    Dynamically determine radix bits for external aggregation (duckdb/duckdb#21274)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev71"
+#define DUCKDB_PATCH_VERSION "1-dev78"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.1-dev71"
+#define DUCKDB_VERSION "v1.5.1-dev78"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c2890df019"
+#define DUCKDB_SOURCE_ID "1d5b25613c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
