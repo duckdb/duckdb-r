e74675ed3b0d12e8938e45abd9f8bcd6a346c171
krlmlr <krlmlr@users.noreply.github.com>
2026-03-19 04:45:27 +0000

    vendor: Update vendored sources to duckdb/duckdb@a641c019096170760989d85bd8014b7988605392
    Date: 2026-03-14 11:53:10 +0100
    
    In the parquet reader, if we are globbing over a directory, use file sizes from the glob to estimate cardinalities instead of relying only on the first parquet file (duckdb/duckdb#21374)
    Parquet Reader: Allow merging of prefetch column ranges for columns that do not have table filters (duckdb/duckdb#21373)

--- src/duckdb/extension/parquet/parquet_reader.cpp
+++ src/duckdb/extension/parquet/parquet_reader.cpp
@@ -1211,6 +1211,18 @@ idx_t ParquetReader::NumRowGroups() const {
 	return GetFileMetadata()->row_groups.size();
 }
 
+idx_t ParquetReader::GetFileSize() const {
+	return file_handle->GetFileSize();
+}
+
+idx_t ParquetReader::GetDataSize() const {
+	idx_t data_size = 0;
+	for (auto &row_group : GetFileMetadata()->row_groups) {
+		data_size += row_group.total_compressed_size;
+	}
+	return data_size;
+}
+
 ParquetScanFilter::ParquetScanFilter(ClientContext &context, idx_t filter_idx, TableFilter &filter)
     : filter_idx(filter_idx), filter(filter) {
 	filter_state = TableFilterState::Initialize(context, filter);
@@ -1387,17 +1399,17 @@ AsyncResult ParquetReader::Scan(ClientContext &context, ParquetReaderScanState &
 				bool lazy_fetch = filters != nullptr;
 
 				// Prefetch column-wise
+				auto &root_reader = state.root_reader->Cast<StructColumnReader>();
 				for (idx_t i = 0; i < column_ids.size(); i++) {
 					auto col_idx = MultiFileLocalIndex(i);
 					auto file_col_idx = column_ids[col_idx];
-					auto &root_reader = state.root_reader->Cast<StructColumnReader>();
 
 					bool has_filter = false;
 					if (filters) {
 						auto entry = filters->filters.find(col_idx);
 						has_filter = entry != filters->filters.end();
 					}
-					root_reader.GetChildReader(file_col_idx).RegisterPrefetch(trans, !(lazy_fetch && !has_filter));
+					root_reader.GetChildReader(file_col_idx).RegisterPrefetch(trans, !has_filter);
 				}
 
 				trans.FinalizeRegistration();
