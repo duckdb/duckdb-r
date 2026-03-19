574f660139fa684d6051cb2d312a21342d01bfea
krlmlr <krlmlr@users.noreply.github.com>
2026-03-19 11:25:55 +0000

    vendor: Update vendored sources to duckdb/duckdb@c6a398eea4d2ef6b26ac4a8e7b61f09c31ad56b8
    Date: 2026-03-17 18:11:44 +0100
    
    Predicate factoring (duckdb/duckdb#21418)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev243"
+#define DUCKDB_PATCH_VERSION "1-dev248"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.1-dev243"
+#define DUCKDB_VERSION "v1.5.1-dev248"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "5467da56c4"
+#define DUCKDB_SOURCE_ID "c6a398eea4"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
