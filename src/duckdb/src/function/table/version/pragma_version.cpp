593b9b37a17fa9ebbd8658413dadc753b43844d2
krlmlr <krlmlr@users.noreply.github.com>
2026-03-19 05:09:13 +0000

    vendor: Update vendored sources to duckdb/duckdb@0b4f52552eb46012910eece8ee225a9a287ed6e1
    Date: 2026-03-14 21:36:35 +0100
    
    Fix duckdb/duckdb#21335: correctly add checkpoint and recovery WAL to allowed paths when launching an initial db with enable_external_access set to false (duckdb/duckdb#21379)
    Well-defined environment for MinGW builds (duckdb/duckdb#21371)
    MbedTLS: Actually throw exception  (duckdb/duckdb#21365)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "1-dev189"
+#define DUCKDB_PATCH_VERSION "1-dev195"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.1-dev189"
+#define DUCKDB_VERSION "v1.5.1-dev195"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a641c01909"
+#define DUCKDB_SOURCE_ID "0b4f52552e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
