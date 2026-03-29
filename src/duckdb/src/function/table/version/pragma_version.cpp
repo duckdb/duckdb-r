ee8aab1e575bb974018ab4ff29c193f6c430805f
krlmlr <krlmlr@users.noreply.github.com>
2026-03-29 07:37:45 +0000

    vendor: Update vendored sources to duckdb/duckdb@5c76aa7388d2983698ac3f4f33cb8f0a7d26175a
    Date: 2026-03-28 09:47:19 +0100
    
    Internal https://redirect.github.com/duckdb/duckdb/pull/8553: Window TopN Except (https://redirect.github.com/duckdb/duckdb/pull/21671)
    CLI: Add .help shortcuts (https://redirect.github.com/duckdb/duckdb/pull/21662)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev284"
+#define DUCKDB_PATCH_VERSION "2-dev289"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev284"
+#define DUCKDB_VERSION "v1.5.2-dev289"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "9414d79c27"
+#define DUCKDB_SOURCE_ID "5c76aa7388"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
