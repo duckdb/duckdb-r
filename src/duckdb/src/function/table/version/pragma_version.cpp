3d3a073112a4c7950c1d8a0a3d8b42b849cb23f9
krlmlr <krlmlr@users.noreply.github.com>
2026-03-29 07:37:05 +0000

    vendor: Update vendored sources to duckdb/duckdb@710adf3d0587022701c56f0b83fc60011f57efff
    Date: 2026-03-21 09:57:35 +0100
    
    Unify adding suffixes to path in `Path:: AddSuffixToPath` - fix temp directory split bug (https://redirect.github.com/duckdb/duckdb/pull/21527)
    Add setting for limiting the number of threads launched concurrently in the test runner (`max_test_threads`) (https://redirect.github.com/duckdb/duckdb/pull/21520)
    Fix parsing test path to skip (https://redirect.github.com/duckdb/duckdb/pull/21495)
    Fix https://redirect.github.com/duckdb/duckdb/pull/21512: correctly render empty results in .mode json (https://redirect.github.com/duckdb/duckdb/pull/21517)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev62"
+#define DUCKDB_PATCH_VERSION "2-dev71"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev62"
+#define DUCKDB_VERSION "v1.5.2-dev71"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "0e4246989b"
+#define DUCKDB_SOURCE_ID "710adf3d05"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
