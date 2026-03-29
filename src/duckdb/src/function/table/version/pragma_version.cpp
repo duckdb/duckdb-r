924eeec2bdb0aa8bef4be6ef4c73dc71da3a0018
krlmlr <krlmlr@users.noreply.github.com>
2026-03-29 07:37:47 +0000

    vendor: Update vendored sources to duckdb/duckdb@4b7b860448b156bb2d1d2e499bdf078b8dccf73a
    Date: 2026-03-28 17:34:10 +0100
    
    Fix issue with struct filter on missing structs (https://redirect.github.com/duckdb/duckdb/pull/21676)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev289"
+#define DUCKDB_PATCH_VERSION "2-dev291"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev289"
+#define DUCKDB_VERSION "v1.5.2-dev291"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "5c76aa7388"
+#define DUCKDB_SOURCE_ID "4b7b860448"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
