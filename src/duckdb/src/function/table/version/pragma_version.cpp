48fe336ccf0f7e927e4e5334962339ef5232c82f
krlmlr <krlmlr@users.noreply.github.com>
2026-03-29 07:37:10 +0000

    vendor: Update vendored sources to duckdb/duckdb@c5d5048669a0fa8f47cbfb861e9cb37b92539b46
    Date: 2026-01-29 17:29:59 +0100
    
    Use FileSystem APIs in QueryProfiler::Write (https://redirect.github.com/duckdb/duckdb/pull/20727)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "5-dev4"
+#define DUCKDB_PATCH_VERSION "5-dev6"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.5-dev4"
+#define DUCKDB_VERSION "v1.4.5-dev6"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b6fbc09f79"
+#define DUCKDB_SOURCE_ID "c5d5048669"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
