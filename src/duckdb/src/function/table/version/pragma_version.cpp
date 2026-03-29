d78fc09a6a9b2799bc8476346614a92f1c0f0340
krlmlr <krlmlr@users.noreply.github.com>
2026-03-29 07:37:20 +0000

    vendor: Update vendored sources to duckdb/duckdb@187d70c8d8a379afdd7eff1c8b7506986b9f4663
    Date: 2026-03-25 13:56:15 +0100
    
    Segfault due to unchecked malloc/realloc, proposed fix to https://redirect.github.com/duckdb/duckdb/pull/21593 (https://redirect.github.com/duckdb/duckdb/pull/21594)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev123"
+#define DUCKDB_PATCH_VERSION "2-dev127"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev123"
+#define DUCKDB_VERSION "v1.5.2-dev127"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "8dabba981e"
+#define DUCKDB_SOURCE_ID "187d70c8d8"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
