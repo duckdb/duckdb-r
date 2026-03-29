1c3fe6c9027118f5a6b5752a765a16c1457cfe7f
krlmlr <krlmlr@users.noreply.github.com>
2026-03-29 07:37:28 +0000

    vendor: Update vendored sources to duckdb/duckdb@ddbb5e2e4c5f139dc5f58583a078b26f3fc54325
    Date: 2026-03-25 07:59:07 +0100
    
    Backport `__EMSCRIPTEN__` fix (https://redirect.github.com/duckdb/duckdb/pull/21581)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "5-dev59"
+#define DUCKDB_PATCH_VERSION "5-dev61"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.5-dev59"
+#define DUCKDB_VERSION "v1.4.5-dev61"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "81e1e825ca"
+#define DUCKDB_SOURCE_ID "ddbb5e2e4c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
