5875654ac089cac20e42e79856423c7145312fcf
krlmlr <krlmlr@users.noreply.github.com>
2026-03-29 07:36:59 +0000

    vendor: Update vendored sources to duckdb/duckdb@6ff3b4f26cc9a916896f89167922704296ef9b6a
    Date: 2026-03-20 12:52:52 +0100
    
    Make some MultiStatements and PRAGMAs Transactional (https://redirect.github.com/duckdb/duckdb/pull/21171)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev8"
+#define DUCKDB_PATCH_VERSION "2-dev57"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev8"
+#define DUCKDB_VERSION "v1.5.2-dev57"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4c959e279e"
+#define DUCKDB_SOURCE_ID "6ff3b4f26c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
