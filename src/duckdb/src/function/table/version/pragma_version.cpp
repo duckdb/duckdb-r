f99c58abb19560e4a3d2d38c8d5bfa5485ca822f
krlmlr <krlmlr@users.noreply.github.com>
2026-03-29 07:37:08 +0000

    vendor: Update vendored sources to duckdb/duckdb@32ae75b32073de796798f03373761a88963a53a2
    Date: 2026-01-24 13:27:42 +0100
    
    Internal https://redirect.github.com/duckdb/duckdb/pull/7212: FILL Empty Chunk (https://redirect.github.com/duckdb/duckdb/pull/20657)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4"
+#define DUCKDB_PATCH_VERSION "5-dev2"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.4"
+#define DUCKDB_VERSION "v1.4.5-dev2"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "6ddac802ff"
+#define DUCKDB_SOURCE_ID "32ae75b320"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
