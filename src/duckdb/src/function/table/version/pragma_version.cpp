4badbf4fbc0d52fc53654925b01f6649656779dc
krlmlr <krlmlr@users.noreply.github.com>
2026-03-29 07:37:11 +0000

    vendor: Update vendored sources to duckdb/duckdb@123e15dc2d13eeb68287902831d21819afed5ce4
    Date: 2026-02-01 13:03:16 +0100
    
    Fix escape handling in AddEscapes function (https://redirect.github.com/duckdb/duckdb/pull/20760)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "5-dev6"
+#define DUCKDB_PATCH_VERSION "5-dev11"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 4
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.4.5-dev6"
+#define DUCKDB_VERSION "v1.4.5-dev11"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "c5d5048669"
+#define DUCKDB_SOURCE_ID "123e15dc2d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
