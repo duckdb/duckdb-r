33de9405fa7fd62281e9f6e17e67e70f35874ca2
krlmlr <krlmlr@users.noreply.github.com>
2026-03-29 07:37:38 +0000

    vendor: Update vendored sources to duckdb/duckdb@7a71122029637012d247b4ba13bdc7fc927bfec9
    Date: 2026-03-27 10:09:08 +0100
    
    Re-organize WAL replay slightly, and correctly deal with empty checkpoint WAL files in WAL recovery (https://redirect.github.com/duckdb/duckdb/pull/21645)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev267"
+#define DUCKDB_PATCH_VERSION "2-dev270"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev267"
+#define DUCKDB_VERSION "v1.5.2-dev270"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "9b34451b86"
+#define DUCKDB_SOURCE_ID "7a71122029"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
