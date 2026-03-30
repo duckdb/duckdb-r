80281a9285edfb3950cdf54533296bdafec8699e
krlmlr <krlmlr@users.noreply.github.com>
2026-03-30 11:21:15 +0000

    vendor: Update vendored sources to duckdb/duckdb@aad9569373f8093853c7268a21f6acbbcaa347f1
    Date: 2026-03-30 13:20:25 +0200
    
    Disable regular updates for geometry (https://redirect.github.com/duckdb/duckdb/pull/21641)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev295"
+#define DUCKDB_PATCH_VERSION "2-dev297"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev295"
+#define DUCKDB_VERSION "v1.5.2-dev297"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1fca3f5bda"
+#define DUCKDB_SOURCE_ID "aad9569373"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
