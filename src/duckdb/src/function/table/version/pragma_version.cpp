eb2354a765181ed3cbe40d3a43cffabd0deadebb
krlmlr <krlmlr@users.noreply.github.com>
2026-03-31 09:21:36 +0000

    vendor: Update vendored sources to duckdb/duckdb@16de6a6358c62636217783c1c8eef9ce738e5cde
    Date: 2026-03-31 10:32:49 +0200
    
    TopNWindowElimination fixes (https://redirect.github.com/duckdb/duckdb/pull/21663)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev307"
+#define DUCKDB_PATCH_VERSION "2-dev316"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev307"
+#define DUCKDB_VERSION "v1.5.2-dev316"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "df27220a52"
+#define DUCKDB_SOURCE_ID "16de6a6358"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
