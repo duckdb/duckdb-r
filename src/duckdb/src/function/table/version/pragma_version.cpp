e1e77ab500b5d4fa2b5d28fff69969a5cbaa0253
krlmlr <krlmlr@users.noreply.github.com>
2026-04-01 06:30:25 +0000

    vendor: Update vendored sources to duckdb/duckdb@f4cfd655152222e0bd268db3df3e88b38877d137
    Date: 2026-04-01 08:21:11 +0200
    
    Allow join filter pushdown for NOP collations (https://redirect.github.com/duckdb/duckdb/pull/21742)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev332"
+#define DUCKDB_PATCH_VERSION "2-dev334"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev332"
+#define DUCKDB_VERSION "v1.5.2-dev334"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "bd5ed38448"
+#define DUCKDB_SOURCE_ID "f4cfd65515"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
