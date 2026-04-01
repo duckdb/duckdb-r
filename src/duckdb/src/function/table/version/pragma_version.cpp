0edbf3cf477371aed4b15a6cf33f3a5780f10d3d
krlmlr <krlmlr@users.noreply.github.com>
2026-04-01 17:17:18 +0000

    vendor: Update vendored sources to duckdb/duckdb@10c4e2493ede91d9679e32528984a04c4d3a289e
    Date: 2026-04-01 18:47:32 +0200
    
    Use correct error message for name conflicts between table and views (https://redirect.github.com/duckdb/duckdb/pull/21760)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev354"
+#define DUCKDB_PATCH_VERSION "2-dev356"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev354"
+#define DUCKDB_VERSION "v1.5.2-dev356"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a0c4c47434"
+#define DUCKDB_SOURCE_ID "10c4e2493e"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
