d037e7e98f1a57636cff92f5dadadeb2940f08fd
krlmlr <krlmlr@users.noreply.github.com>
2026-04-06 08:28:36 +0000

    vendor: Update vendored sources to duckdb/duckdb@31883c66b907533ea9d8d554885506ca348475a4
    Date: 2026-04-06 10:13:12 +0200
    
    Fix Arrow REE INT64 run_ends using wrong template parameter (https://redirect.github.com/duckdb/duckdb/pull/21847)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev408"
+#define DUCKDB_PATCH_VERSION "2-dev410"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev408"
+#define DUCKDB_VERSION "v1.5.2-dev410"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "cbc6f2230e"
+#define DUCKDB_SOURCE_ID "31883c66b9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
