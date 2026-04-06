2cdbf91f590a6ac9e14fbf322f84fee046c5b712
krlmlr <krlmlr@users.noreply.github.com>
2026-04-06 21:10:32 +0000

    vendor: Update vendored sources to duckdb/duckdb@c241da23ae91f5cc0561a53800f3edddced79472
    Date: 2026-04-06 22:32:17 +0200
    
    Fix geometry TextWriter corrupting coordinates in scientific notation (https://redirect.github.com/duckdb/duckdb/pull/21893)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev417"
+#define DUCKDB_PATCH_VERSION "2-dev419"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev417"
+#define DUCKDB_VERSION "v1.5.2-dev419"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1518906e3d"
+#define DUCKDB_SOURCE_ID "c241da23ae"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
