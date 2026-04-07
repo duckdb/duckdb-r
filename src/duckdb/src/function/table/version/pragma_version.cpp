27b9179838edf6c0bc12acaa6ca26523e68b3d42
krlmlr <krlmlr@users.noreply.github.com>
2026-04-07 10:20:14 +0000

    vendor: Update vendored sources to duckdb/duckdb@b6dd5b55c5818bee011fdd6f50156985f3bc535b
    Date: 2026-04-07 12:14:03 +0200
    
    Bump Postgres, MySQL and SQLite (https://redirect.github.com/duckdb/duckdb/pull/21899)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev421"
+#define DUCKDB_PATCH_VERSION "2-dev423"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev421"
+#define DUCKDB_VERSION "v1.5.2-dev423"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "5c50ae787e"
+#define DUCKDB_SOURCE_ID "b6dd5b55c5"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
