0694c9476dbc0c38fb9eb33a6fb621b79927d75c
krlmlr <krlmlr@users.noreply.github.com>
2026-04-08 18:22:06 +0000

    vendor: Update vendored sources to duckdb/duckdb@cc8c034da711afbf10a7a40f0669779d49a5f1b1
    Date: 2026-04-08 20:15:31 +0200
    
    Fix incorrect results when using `try` within `if` (https://redirect.github.com/duckdb/duckdb/pull/21943)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev494"
+#define DUCKDB_PATCH_VERSION "2-dev496"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev494"
+#define DUCKDB_VERSION "v1.5.2-dev496"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "0fab542c84"
+#define DUCKDB_SOURCE_ID "cc8c034da7"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
