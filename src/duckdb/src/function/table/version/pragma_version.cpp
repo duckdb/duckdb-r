d14f3b690d829c152bc268f022ece5049c3d0155
krlmlr <krlmlr@users.noreply.github.com>
2026-04-08 14:31:59 +0000

    vendor: Update vendored sources to duckdb/duckdb@1f9fe9e2c1eccabc0fda637959c0a52b4f4f44aa
    Date: 2026-04-08 15:48:11 +0200
    
    Use `BLOB`, not `VARCHAR` for row group pruning (https://redirect.github.com/duckdb/duckdb/pull/21946)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev488"
+#define DUCKDB_PATCH_VERSION "2-dev490"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev488"
+#define DUCKDB_VERSION "v1.5.2-dev490"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ab70983594"
+#define DUCKDB_SOURCE_ID "1f9fe9e2c1"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
