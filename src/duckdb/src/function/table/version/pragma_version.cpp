4a4319e6e54b57444c8a4fd9112a8d992d7b553c
krlmlr <krlmlr@users.noreply.github.com>
2026-04-08 14:32:00 +0000

    vendor: Update vendored sources to duckdb/duckdb@0fab542c84ca94657616e519cd679334dcbd86b1
    Date: 2026-04-08 15:49:18 +0200
    
    Add HTTPUtil::CloseClient(...) with trivial no-op implementation, and base_url field to HTTPClient (https://redirect.github.com/duckdb/duckdb/pull/21924)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev490"
+#define DUCKDB_PATCH_VERSION "2-dev494"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev490"
+#define DUCKDB_VERSION "v1.5.2-dev494"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1f9fe9e2c1"
+#define DUCKDB_SOURCE_ID "0fab542c84"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
