1c0208422ccad40d78659f6201db1de084f431e7
krlmlr <krlmlr@users.noreply.github.com>
2026-04-09 18:18:10 +0000

    vendor: Update vendored sources to duckdb/duckdb@0f7eef287ff7a7bf80f02ea363517060323cc496
    Date: 2026-04-09 20:13:23 +0200
    
    Attempt to fix invalid memory access (https://redirect.github.com/duckdb/duckdb/pull/21985)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev535"
+#define DUCKDB_PATCH_VERSION "2-dev538"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev535"
+#define DUCKDB_VERSION "v1.5.2-dev538"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "accc0506cd"
+#define DUCKDB_SOURCE_ID "0f7eef287f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
