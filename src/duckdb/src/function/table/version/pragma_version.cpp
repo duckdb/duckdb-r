02c2017c104908ef40d5f68b6dceb186854a45c7
krlmlr <krlmlr@users.noreply.github.com>
2026-04-09 22:12:14 +0000

    vendor: Update vendored sources to duckdb/duckdb@f88ffceef35d2c5475747e475c80972eb79aec2e
    Date: 2026-04-09 23:28:06 +0200
    
    bump azure and delta for v1.5.2 (https://redirect.github.com/duckdb/duckdb/pull/21979)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev540"
+#define DUCKDB_PATCH_VERSION "2-dev543"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev540"
+#define DUCKDB_VERSION "v1.5.2-dev543"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "3ab4d25d01"
+#define DUCKDB_SOURCE_ID "f88ffceef3"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
