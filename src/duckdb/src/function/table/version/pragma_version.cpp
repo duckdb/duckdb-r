5154c8c9368df09b03c7bbe1181994e0d0043737
krlmlr <krlmlr@users.noreply.github.com>
2026-04-09 11:19:58 +0000

    vendor: Update vendored sources to duckdb/duckdb@64f0eae50bbb324ccf644bb140da7963ce9c874f
    Date: 2026-04-09 13:17:27 +0200
    
    Unlock transaction lock during fallback WAL write (https://redirect.github.com/duckdb/duckdb/pull/21969)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev524"
+#define DUCKDB_PATCH_VERSION "2-dev526"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev524"
+#define DUCKDB_VERSION "v1.5.2-dev526"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4812536fd8"
+#define DUCKDB_SOURCE_ID "64f0eae50b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
