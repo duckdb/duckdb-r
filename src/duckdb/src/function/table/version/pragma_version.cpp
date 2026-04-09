3dd9299bed0ff2ca60b517805eabf7b3acb6eaa9
krlmlr <krlmlr@users.noreply.github.com>
2026-04-09 10:23:28 +0000

    vendor: Update vendored sources to duckdb/duckdb@4812536fd84aa9ef5e845dd63968ee6ef13de99f
    Date: 2026-04-09 11:55:52 +0200
    
    Make Oids start at 20k to avoid unintended collisions (https://redirect.github.com/duckdb/duckdb/pull/20979)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev522"
+#define DUCKDB_PATCH_VERSION "2-dev524"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev522"
+#define DUCKDB_VERSION "v1.5.2-dev524"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "78348f6b12"
+#define DUCKDB_SOURCE_ID "4812536fd8"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
