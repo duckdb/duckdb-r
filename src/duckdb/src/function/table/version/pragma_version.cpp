1aa271e813b649e45ddb95192ed68b8fe90c9b88
krlmlr <krlmlr@users.noreply.github.com>
2026-04-09 09:25:34 +0000

    vendor: Update vendored sources to duckdb/duckdb@78348f6b12b5f2a3c30a68ef7ca1e56727ae7cca
    Date: 2026-04-09 08:55:16 +0200
    
    [Parquet][VARIANT] Add support for Snowflake-produced shredded VARIANT Parquet files (https://redirect.github.com/duckdb/duckdb/pull/21814)
    bump iceberg (https://redirect.github.com/duckdb/duckdb/pull/21967)
    Bump Postgres (https://redirect.github.com/duckdb/duckdb/pull/21958)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "2-dev514"
+#define DUCKDB_PATCH_VERSION "2-dev522"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.2-dev514"
+#define DUCKDB_VERSION "v1.5.2-dev522"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "90e369144a"
+#define DUCKDB_SOURCE_ID "78348f6b12"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
