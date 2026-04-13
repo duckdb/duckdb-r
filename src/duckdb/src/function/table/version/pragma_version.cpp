e23e4d57b4682c53b5c17e89feb878c8a961cb53
krlmlr <krlmlr@users.noreply.github.com>
2026-04-13 14:34:48 +0000

    vendor: Update vendored sources to duckdb/duckdb@fbe304454a70bdc6a5f70ac2ee0e2a7ad321c7b5
    Date: 2026-04-13 15:44:00 +0200
    
    Provide BWC support for join filter pushdowns (https://redirect.github.com/duckdb/duckdb/pull/22029)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev14"
+#define DUCKDB_PATCH_VERSION "3-dev16"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev14"
+#define DUCKDB_VERSION "v1.5.3-dev16"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ad406b8695"
+#define DUCKDB_SOURCE_ID "fbe304454a"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
