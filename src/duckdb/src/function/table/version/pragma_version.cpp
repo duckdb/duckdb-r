cb2f4ff6f59ea6cfda95ee67b2b54637877b06f7
krlmlr <krlmlr@users.noreply.github.com>
2026-04-15 12:20:55 +0000

    vendor: Update vendored sources to duckdb/duckdb@038fb6b79cda3fdb86b67170dca53ab8f375677f
    Date: 2026-04-15 14:18:14 +0200
    
    Set `query` field for statements in `ALTER TABLE ... ADD COLUMN ... DEFAULT ...` workaround (https://redirect.github.com/duckdb/duckdb/pull/22057)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev40"
+#define DUCKDB_PATCH_VERSION "3-dev53"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev40"
+#define DUCKDB_VERSION "v1.5.3-dev53"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "48f850fde1"
+#define DUCKDB_SOURCE_ID "038fb6b79c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
