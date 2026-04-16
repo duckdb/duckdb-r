6f01128ae6dddfe02105dddd5b648d3bc808be94
krlmlr <krlmlr@users.noreply.github.com>
2026-04-16 14:36:30 +0000

    vendor: Update vendored sources to duckdb/duckdb@8cc999c7b9e1535009934e65365208ecb41ed2f8
    Date: 2026-04-16 15:52:29 +0200
    
    Issue https://redirect.github.com/duckdb/duckdb/pull/22096: TopN Window Casts (https://redirect.github.com/duckdb/duckdb/pull/22098)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev67"
+#define DUCKDB_PATCH_VERSION "3-dev69"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev67"
+#define DUCKDB_VERSION "v1.5.3-dev69"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ae80e871fa"
+#define DUCKDB_SOURCE_ID "8cc999c7b9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
