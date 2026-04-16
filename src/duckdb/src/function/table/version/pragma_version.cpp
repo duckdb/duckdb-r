203228bb86d3c9a823adcd05760553c582edb8dc
krlmlr <krlmlr@users.noreply.github.com>
2026-04-16 17:29:56 +0000

    vendor: Update vendored sources to duckdb/duckdb@a343d23a963ade56acdcd52df1c556dc78e8bce2
    Date: 2026-04-16 18:50:31 +0200
    
    Add support for reading `VARIANT` using C API (https://redirect.github.com/duckdb/duckdb/pull/22065)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev69"
+#define DUCKDB_PATCH_VERSION "3-dev72"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev69"
+#define DUCKDB_VERSION "v1.5.3-dev72"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "8cc999c7b9"
+#define DUCKDB_SOURCE_ID "a343d23a96"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
