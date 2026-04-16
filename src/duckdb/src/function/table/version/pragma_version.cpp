8a93bcef3b85aab3744d61210b5cd191934d4731
krlmlr <krlmlr@users.noreply.github.com>
2026-04-16 07:33:45 +0000

    vendor: Update vendored sources to duckdb/duckdb@f35db8881aa90254aa92e1e9d5291c89b0a831eb
    Date: 2026-04-16 09:17:31 +0200
    
     Row group append (https://redirect.github.com/duckdb/duckdb/pull/22060)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev59"
+#define DUCKDB_PATCH_VERSION "3-dev65"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev59"
+#define DUCKDB_VERSION "v1.5.3-dev65"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "67fe1eed4f"
+#define DUCKDB_SOURCE_ID "f35db8881a"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
