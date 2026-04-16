0a69cd18f2e736745c673a30282bf79b41f2b5b7
krlmlr <krlmlr@users.noreply.github.com>
2026-04-16 07:33:44 +0000

    vendor: Update vendored sources to duckdb/duckdb@67fe1eed4f8111d8d7c81541fe8770ab9ce614bd
    Date: 2026-04-16 09:08:36 +0200
    
    Coorporative tasks might lead to busy spinning in `TaskExecutor::WorkOnTasks` (https://redirect.github.com/duckdb/duckdb/pull/22092)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev53"
+#define DUCKDB_PATCH_VERSION "3-dev59"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev53"
+#define DUCKDB_VERSION "v1.5.3-dev59"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "038fb6b79c"
+#define DUCKDB_SOURCE_ID "67fe1eed4f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
