cd5dececb299397906ff29de3bc655e63c85b176
krlmlr <krlmlr@users.noreply.github.com>
2026-04-17 12:19:31 +0000

    vendor: Update vendored sources to duckdb/duckdb@00beb59aed8adeb10212d2297942db6162891bc5
    Date: 2026-04-17 13:29:30 +0200
    
    fix commit iteration offset bug + relax RemoveFromIndexes assertion (https://redirect.github.com/duckdb/duckdb/pull/22094)
    Bump Julia to v1.5.2 (https://redirect.github.com/duckdb/duckdb/pull/22121)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev72"
+#define DUCKDB_PATCH_VERSION "3-dev79"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev72"
+#define DUCKDB_VERSION "v1.5.3-dev79"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "a343d23a96"
+#define DUCKDB_SOURCE_ID "00beb59aed"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
