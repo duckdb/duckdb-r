e1c1b57668c5a6e8305bc86416fad7d3a4a100bf
krlmlr <krlmlr@users.noreply.github.com>
2026-04-20 14:35:46 +0000

    vendor: Update vendored sources to duckdb/duckdb@bfbe1fb8f736fd2f98389a5b42a04f68c0d523a8
    Date: 2026-04-20 15:46:52 +0200
    
    Also execute auto-rollback on CLI ClientContext::Query() query (https://redirect.github.com/duckdb/duckdb/pull/22159)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev95"
+#define DUCKDB_PATCH_VERSION "3-dev97"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev95"
+#define DUCKDB_VERSION "v1.5.3-dev97"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b7ca2c99e5"
+#define DUCKDB_SOURCE_ID "bfbe1fb8f7"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
