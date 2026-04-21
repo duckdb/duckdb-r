94e4f7bd01c05ddbf8b6589a2b19bdf883da68b5
krlmlr <krlmlr@users.noreply.github.com>
2026-04-21 09:33:18 +0000

    vendor: Update vendored sources to duckdb/duckdb@a04d20515f80050ff44d1d6f5f968fa0b07f957d
    Date: 2026-04-21 10:52:00 +0200
    
    Fix CSV escape (https://redirect.github.com/duckdb/duckdb/pull/22176)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev108"
+#define DUCKDB_PATCH_VERSION "3-dev110"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev108"
+#define DUCKDB_VERSION "v1.5.3-dev110"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "12002785c2"
+#define DUCKDB_SOURCE_ID "a04d20515f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
