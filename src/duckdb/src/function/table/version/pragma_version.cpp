3b691f5974165910a0bcde649f8b8453dc948ea5
krlmlr <krlmlr@users.noreply.github.com>
2026-04-25 13:29:04 +0000

    vendor: Update vendored sources to duckdb/duckdb@f0413bfa84f07975a199013cd87ea04b76eb5fb9
    Date: 2026-04-25 15:20:22 +0200
    
    Fix UTC±NN00 cannot be parsed in SQL (https://redirect.github.com/duckdb/duckdb/pull/22244)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev130"
+#define DUCKDB_PATCH_VERSION "3-dev132"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev130"
+#define DUCKDB_VERSION "v1.5.3-dev132"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e5cda4269e"
+#define DUCKDB_SOURCE_ID "f0413bfa84"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
