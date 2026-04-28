42460a27c5171c5798836737ca838c42759c753b
krlmlr <krlmlr@users.noreply.github.com>
2026-04-28 07:48:08 +0000

    vendor: Update vendored sources to duckdb/duckdb@4337c06de543a904b98b891dcfb821e9d8141b4d
    Date: 2026-04-28 09:08:09 +0200
    
    Allow package builds to choose linked extensions (https://redirect.github.com/duckdb/duckdb/pull/22305)

--- src/duckdb/generated_extension_loader_package_build.cpp
+++ src/duckdb/generated_extension_loader_package_build.cpp
@@ -1,5 +1,17 @@
+#ifndef DUCKDB_EXTENSION_PARQUET_LINKED
+#define DUCKDB_EXTENSION_PARQUET_LINKED 1
+#endif
+
+#ifndef DUCKDB_EXTENSION_CORE_FUNCTIONS_LINKED
+#define DUCKDB_EXTENSION_CORE_FUNCTIONS_LINKED 1
+#endif
+
+#if DUCKDB_EXTENSION_PARQUET_LINKED
 #include "parquet_extension.hpp"
+#endif
+#if DUCKDB_EXTENSION_CORE_FUNCTIONS_LINKED
 #include "core_functions_extension.hpp"
+#endif
 #include "duckdb/main/extension/generated_extension_loader.hpp"
 #include "duckdb/main/extension_helper.hpp"
 
@@ -7,22 +19,30 @@ namespace duckdb {
 
 //! Looks through the package_build.py-generated list of extensions that are linked into DuckDB currently to try load <extension>
 ExtensionLoadResult ExtensionHelper::LoadExtension(DuckDB &db, const std::string &extension) {
-
+#if DUCKDB_EXTENSION_PARQUET_LINKED
     if (extension=="parquet") {
         db.LoadStaticExtension<ParquetExtension>();
         return ExtensionLoadResult::LOADED_EXTENSION;
     }
-        
+#endif
+#if DUCKDB_EXTENSION_CORE_FUNCTIONS_LINKED
     if (extension=="core_functions") {
         db.LoadStaticExtension<CoreFunctionsExtension>();
         return ExtensionLoadResult::LOADED_EXTENSION;
     }
-        
+#endif
+
     return ExtensionLoadResult::NOT_LOADED;
 }
 
 vector<string> LinkedExtensions(){
-    vector<string> VEC = {"parquet", "core_functions"
+    vector<string> VEC = {
+#if DUCKDB_EXTENSION_PARQUET_LINKED
+        "parquet",
+#endif
+#if DUCKDB_EXTENSION_CORE_FUNCTIONS_LINKED
+        "core_functions",
+#endif
     };
     return VEC;
 }
--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev137"
+#define DUCKDB_PATCH_VERSION "3-dev140"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev137"
+#define DUCKDB_VERSION "v1.5.3-dev140"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ca12adf671"
+#define DUCKDB_SOURCE_ID "4337c06de5"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
