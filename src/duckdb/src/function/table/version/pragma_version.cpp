dfe87ea44ee67dc2d85da92e8d87ae8462878a12
krlmlr <krlmlr@users.noreply.github.com>
2026-05-11 11:04:31 +0000

    vendor: Update vendored sources to duckdb/duckdb@407ba676d9182701a02fe85efb3f1e33b6cbc3be
    Date: 2026-05-11 12:58:18 +0200
    
    ExtensionInstall: Remove use of IsHTTP to IsRemoteFile (https://redirect.github.com/duckdb/duckdb/pull/21900)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev250"
+#define DUCKDB_PATCH_VERSION "3-dev252"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev250"
+#define DUCKDB_VERSION "v1.5.3-dev252"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "926cc3e520"
+#define DUCKDB_SOURCE_ID "407ba676d9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
