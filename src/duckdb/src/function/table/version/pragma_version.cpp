7269b2bdf7cad0bffe95804a3d999521bfdf5061
krlmlr <krlmlr@users.noreply.github.com>
2026-05-12 08:02:17 +0000

    vendor: Update vendored sources to duckdb/duckdb@cf73ab40ac218ca992c9ae71703a7f5101890e46
    Date: 2026-05-12 09:34:14 +0200
    
    Bump Postgres, MySQL and ODBC (https://redirect.github.com/duckdb/duckdb/pull/22579)
    Bump httpfs and remove patches (https://redirect.github.com/duckdb/duckdb/pull/22556)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev258"
+#define DUCKDB_PATCH_VERSION "3-dev262"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev258"
+#define DUCKDB_VERSION "v1.5.3-dev262"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "46a672fe76"
+#define DUCKDB_SOURCE_ID "cf73ab40ac"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
