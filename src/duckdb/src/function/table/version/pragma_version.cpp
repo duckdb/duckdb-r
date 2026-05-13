0c204cc9b07e76036e44649fa1dc1b8b3893c007
krlmlr <krlmlr@users.noreply.github.com>
2026-05-13 07:02:13 +0000

    vendor: Update vendored sources to duckdb/duckdb@6b290ddbed2d233d35b0b9d1a5ac5ea3f3f30c8b
    Date: 2026-05-13 08:52:32 +0200
    
    bump spatial again (https://redirect.github.com/duckdb/duckdb/pull/22602)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev306"
+#define DUCKDB_PATCH_VERSION "3-dev309"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev306"
+#define DUCKDB_VERSION "v1.5.3-dev309"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "1d9b65aa6c"
+#define DUCKDB_SOURCE_ID "6b290ddbed"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
