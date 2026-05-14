d6d8a958ac2460936878e86cd42be75736120e5f
krlmlr <krlmlr@users.noreply.github.com>
2026-05-14 07:00:18 +0000

    vendor: Update vendored sources to duckdb/duckdb@02d05e56481b8f52c2cdad8241c13196fd891011
    Date: 2026-05-14 08:25:02 +0200
    
    Add storage informations for v1.5.3 (https://redirect.github.com/duckdb/duckdb/pull/22638)
    Bump lance to 533e0ee6cf419e4be2af3af56182fb04b87978e1 (https://redirect.github.com/duckdb/duckdb/pull/22640)
    Bump DuckLake for release (https://redirect.github.com/duckdb/duckdb/pull/22651)
    parser_tools is apparently now a dependency for postgres (https://redirect.github.com/duckdb/duckdb/pull/22619)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev323"
+#define DUCKDB_PATCH_VERSION "3-dev331"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev323"
+#define DUCKDB_VERSION "v1.5.3-dev331"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "bcb51f03da"
+#define DUCKDB_SOURCE_ID "02d05e5648"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
