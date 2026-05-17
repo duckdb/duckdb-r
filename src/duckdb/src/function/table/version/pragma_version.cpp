929b6a01a96215402d433897566771a02a1f00c6
krlmlr <krlmlr@users.noreply.github.com>
2026-05-17 17:22:24 +0000

    vendor: Update vendored sources to duckdb/duckdb@b25ebaadb665ee38f74b5bbbb309fadc59e096d8
    Date: 2026-05-17 18:46:55 +0200
    
    Fix timer lifetime/timing issues (https://redirect.github.com/duckdb/duckdb/pull/22697)
    Bump DuckLake (https://redirect.github.com/duckdb/duckdb/pull/22698)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "3-dev371"
+#define DUCKDB_PATCH_VERSION "3-dev376"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.3-dev371"
+#define DUCKDB_VERSION "v1.5.3-dev376"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "54d39a64e8"
+#define DUCKDB_SOURCE_ID "b25ebaadb6"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
