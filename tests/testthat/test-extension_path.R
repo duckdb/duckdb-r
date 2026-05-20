be6578fb858767b0f60a8c1b9672cdbc67f5d405
Kirill Müller <kirill@cynkra.com>
2026-05-20 21:16:17 +0200

    Avoid hard-coding package name

--- tests/testthat/test-extension_path.R
+++ tests/testthat/test-extension_path.R
@@ -3,7 +3,7 @@ skip_on_os(c("windows"))
 
 test_that("duckdb extensions are stored inside the duckdb package install directory", {
   con <- local_con(config = list('allow_unsigned_extensions' = 'true'))
-  expected_dir <- file.path(system.file(package = "duckdb"), "extensions")
+  expected_dir <- system_file_path("extensions")
   err <- expect_error(dbExecute(con, "LOAD 'bogus'"))
   expect_true(grepl(expected_dir, conditionMessage(err), fixed = TRUE))
   expect_true(grepl("bogus.duckdb_extension", conditionMessage(err), fixed = TRUE))
