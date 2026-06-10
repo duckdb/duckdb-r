4797128c3f97a9f9cb8af7ccab6ebbf2c35664fa
Kirill Müller <kirill@cynkra.com>
2026-06-10 05:44:00 +0200

    vendor: Update vendored sources to duckdb/duckdb@b7373fb7590fd7dcd1a9362d598f6e847c59c3b4
    Date: 2026-06-02 08:39:39 +0200
    
    Fix partial column metadata reuse bug (https://redirect.github.com/duckdb/duckdb/pull/22994)
    
    # Conflicts:
    #	DESCRIPTION

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev68"
+#define DUCKDB_PATCH_VERSION "4-dev70"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.4-dev68"
+#define DUCKDB_VERSION "v1.5.4-dev70"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f0c930c615"
+#define DUCKDB_SOURCE_ID "b7373fb759"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
