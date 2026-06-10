225538272edf5629fa7621753d001fe695c9609b
Kirill Müller <kirill@cynkra.com>
2026-06-10 05:44:00 +0200

    vendor: Update vendored sources to duckdb/duckdb@936e23eaf579f5c8732ae3490b1e1cff4820eaa0
    Date: 2026-06-03 07:36:07 +0200
    
    MERGE INTO: only consider target table when binding `WHEN NOT MATCHED` and source table when binding `WHEN NOT MATCHED BY TARGET` (https://redirect.github.com/duckdb/duckdb/pull/23014)
    
    # Conflicts:
    #	DESCRIPTION

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev78"
+#define DUCKDB_PATCH_VERSION "4-dev80"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.4-dev78"
+#define DUCKDB_VERSION "v1.5.4-dev80"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ac79ba69fe"
+#define DUCKDB_SOURCE_ID "936e23eaf5"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
