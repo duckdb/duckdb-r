322746c8aa9c17ba95e437cb607232b00afe8929
Kirill Müller <kirill@cynkra.com>
2026-06-10 05:43:45 +0200

    vendor: Update vendored sources to duckdb/duckdb@c0765cea1fee6de532b825a456430623969ef262
    Date: 2026-05-27 14:47:49 +0200
    
    Fix geometry stats checkpointing when no changes are detected (https://redirect.github.com/duckdb/duckdb/pull/22882)
    
    # Conflicts:
    #	DESCRIPTION

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev35"
+#define DUCKDB_PATCH_VERSION "4-dev38"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.4-dev35"
+#define DUCKDB_VERSION "v1.5.4-dev38"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d53c7e8123"
+#define DUCKDB_SOURCE_ID "c0765cea1f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
