67b3baee72e32556051012984641bbabadcc9364
Kirill Müller <kirill@cynkra.com>
2026-06-10 05:44:01 +0200

    vendor: Update vendored sources to duckdb/duckdb@40721d5609648df8bf00671e2094ccee8d142c0b
    Date: 2026-06-03 14:07:45 +0200
    
    Merge Into: Avoid recursively binding in the ProjectionBinder (https://redirect.github.com/duckdb/duckdb/pull/23022)
    
    # Conflicts:
    #	DESCRIPTION

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev83"
+#define DUCKDB_PATCH_VERSION "4-dev85"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.4-dev83"
+#define DUCKDB_VERSION "v1.5.4-dev85"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "cdfe7bf954"
+#define DUCKDB_SOURCE_ID "40721d5609"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
