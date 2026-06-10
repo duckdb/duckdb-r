d7222547a913c788f432a759e38f58bc9c2dd6df
Kirill Müller <kirill@cynkra.com>
2026-06-10 05:44:02 +0200

    vendor: Update vendored sources to duckdb/duckdb@b9196d397dd252c3e72bbd62be1be8e79f7cef75
    Date: 2026-06-09 14:53:20 +0200
    
    Fix https://redirect.github.com/duckdb/duckdb/pull/21931: avoid trying to bind an expression that doesn't exist in `UNPIVOT` (https://redirect.github.com/duckdb/duckdb/pull/23156)
     Fix json argument order affecting result (https://redirect.github.com/duckdb/duckdb/pull/23144)
    Allow array type for array_to_json (https://redirect.github.com/duckdb/duckdb/pull/23129)
    
    # Conflicts:
    #	DESCRIPTION

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev148"
+#define DUCKDB_PATCH_VERSION "4-dev155"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.4-dev148"
+#define DUCKDB_VERSION "v1.5.4-dev155"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4f8d9cb5d4"
+#define DUCKDB_SOURCE_ID "b9196d397d"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
