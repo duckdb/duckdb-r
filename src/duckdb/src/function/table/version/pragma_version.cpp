47a986d7dd3ec6b6e9f2b52f6be33bc129d23b30
Kirill Müller <kirill@cynkra.com>
2026-06-10 05:44:00 +0200

    vendor: Update vendored sources to duckdb/duckdb@810a15558f1cd411b4a38b8e84ba74c1a724409f
    Date: 2026-06-02 17:42:10 +0200
    
    Fix case-insensitive column match in INSERT ... SELECT ON CONFLICT (https://redirect.github.com/duckdb/duckdb/pull/22825)
    
    # Conflicts:
    #	DESCRIPTION

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev73"
+#define DUCKDB_PATCH_VERSION "4-dev75"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.4-dev73"
+#define DUCKDB_VERSION "v1.5.4-dev75"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "6367663157"
+#define DUCKDB_SOURCE_ID "810a15558f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
