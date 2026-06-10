0d8bf955098268cad8332865a3a4569050d6fb98
Kirill Müller <kirill@cynkra.com>
2026-06-10 05:43:44 +0200

    vendor: Update vendored sources to duckdb/duckdb@d53c7e81239969ce35a39ce4340e23140b931024
    Date: 2026-05-27 10:40:27 +0200
    
    Use committed row count in `RowGroupReorderer` (https://redirect.github.com/duckdb/duckdb/pull/22884)
    Fix: JSON add list type check (https://redirect.github.com/duckdb/duckdb/pull/22862)
    Fix json_keys with wildcard paths (https://redirect.github.com/duckdb/duckdb/pull/22855)
    
    # Conflicts:
    #	DESCRIPTION

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev28"
+#define DUCKDB_PATCH_VERSION "4-dev35"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.4-dev28"
+#define DUCKDB_VERSION "v1.5.4-dev35"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "cd3b2ad3ee"
+#define DUCKDB_SOURCE_ID "d53c7e8123"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
