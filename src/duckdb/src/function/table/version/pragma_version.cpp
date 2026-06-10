63b4d0b496aa4d4d377a84fa87644d82f8bd97d0
Kirill Müller <kirill@cynkra.com>
2026-06-10 05:44:02 +0200

    vendor: Update vendored sources to duckdb/duckdb@07866eb051fda03d148fb40fa9cb188363606119
    Date: 2026-06-09 09:13:37 +0200
    
    Internal https://redirect.github.com/duckdb/duckdb/pull/9375: PRAGMA enum NULL (https://redirect.github.com/duckdb/duckdb/pull/23146)
    
    # Conflicts:
    #	DESCRIPTION

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev143"
+#define DUCKDB_PATCH_VERSION "4-dev145"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.4-dev143"
+#define DUCKDB_VERSION "v1.5.4-dev145"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "b4eeb55d58"
+#define DUCKDB_SOURCE_ID "07866eb051"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
