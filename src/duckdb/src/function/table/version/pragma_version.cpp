77a622c9cb289c3732f1497d6554a3f43aa46381
Kirill Müller <kirill@cynkra.com>
2026-06-10 05:43:41 +0200

    vendor: Update vendored sources to duckdb/duckdb@9a64d338f2fa1d3c1d43c016b09c538b529dd397
    Date: 2026-05-22 23:21:40 +0200
    
    Fix `WindowSelfJoinOptimizer` ignore exception (https://redirect.github.com/duckdb/duckdb/pull/22800)
    Remove time-out waiting for terminal background color (https://redirect.github.com/duckdb/duckdb/pull/22838)
    
    # Conflicts:
    #	DESCRIPTION

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev9"
+#define DUCKDB_PATCH_VERSION "4-dev13"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.4-dev9"
+#define DUCKDB_VERSION "v1.5.4-dev13"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "6e9cdf83f9"
+#define DUCKDB_SOURCE_ID "9a64d338f2"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
