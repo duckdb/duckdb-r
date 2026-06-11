ebcb452f993a69c1f0fa09247eaceedee4c83736
krlmlr <krlmlr@users.noreply.github.com>
2026-06-11 07:39:37 +0000

    vendor: Update vendored sources to duckdb/duckdb@8f2825a19f3509727c1c5ef104729a5f706324ad
    Date: 2026-06-11 08:21:14 +0200
    
    [Dev] Fix variant shredding analysis logic discrepancy with shredded writing (https://redirect.github.com/duckdb/duckdb/pull/23194)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev218"
+#define DUCKDB_PATCH_VERSION "4-dev222"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.4-dev218"
+#define DUCKDB_VERSION "v1.5.4-dev222"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e36f5ab9af"
+#define DUCKDB_SOURCE_ID "8f2825a19f"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
