02c18d2e5404fc9eb6eaf86badb802f26f5e0f21
krlmlr <krlmlr@users.noreply.github.com>
2026-06-15 07:56:58 +0000

    vendor: Update vendored sources to duckdb/duckdb@7f9baa98a7758e346c239e4f76bf80bc5ca21c19
    Date: 2026-06-15 09:35:19 +0200
    
    Fix NULL propagation for date parts of infinite dates (https://redirect.github.com/duckdb/duckdb/pull/23254)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4-dev325"
+#define DUCKDB_PATCH_VERSION "4-dev329"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.4-dev325"
+#define DUCKDB_VERSION "v1.5.4-dev329"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "8504862f65"
+#define DUCKDB_SOURCE_ID "7f9baa98a7"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
