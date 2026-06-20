932b0bbf438b7e1de8df33343b04a614ce2360dd
krlmlr <krlmlr@users.noreply.github.com>
2026-06-20 07:30:49 +0000

    vendor: Update vendored sources to duckdb/duckdb@044589684474e8e58f98fb76213cfad67d9abe3d
    Date: 2026-06-17 15:13:27 +0200
    
    Correctly get typed value stats for fully shredded variants (https://redirect.github.com/duckdb/duckdb/pull/23325)
    bump iceberg again  (https://redirect.github.com/duckdb/duckdb/pull/23311)
    Add support for test config extending (https://redirect.github.com/duckdb/duckdb/pull/23145)
    [Dev] Add LLDB scripts to help with debugging (https://redirect.github.com/duckdb/duckdb/pull/23297)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "4"
+#define DUCKDB_PATCH_VERSION "5-dev38"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.4"
+#define DUCKDB_VERSION "v1.5.5-dev38"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "08e34c447ba"
+#define DUCKDB_SOURCE_ID "0445896844"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
