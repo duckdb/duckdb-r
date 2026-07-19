a415c89af7071eba05e22c0893b0715ab03967d7
krlmlr <krlmlr@users.noreply.github.com>
2026-07-19 10:22:36 +0000

    vendor: Update vendored sources to duckdb/duckdb@76d5b0bda1be609960e19ac5db46acd7248c2faa
    Date: 2026-07-15 10:49:31 +0200
    
    bound alp/alprd exception positions to vector size (https://redirect.github.com/duckdb/duckdb/pull/23778)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "5-dev203"
+#define DUCKDB_PATCH_VERSION "5-dev205"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.5-dev203"
+#define DUCKDB_VERSION "v1.5.5-dev205"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "925680abaa"
+#define DUCKDB_SOURCE_ID "76d5b0bda1"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
