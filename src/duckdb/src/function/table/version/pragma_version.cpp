795699faf3cf2510dcbfb23e37f90d6a04f15d30
krlmlr <krlmlr@users.noreply.github.com>
2026-07-19 10:22:40 +0000

    vendor: Update vendored sources to duckdb/duckdb@e14c3db18700c8f730a3421f6b96a8655d7662c4
    Date: 2026-07-17 18:28:49 +0200
    
    Fix concurrent ALTER and INSERT crash (https://redirect.github.com/duckdb/duckdb/pull/23861)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "5-dev233"
+#define DUCKDB_PATCH_VERSION "5-dev238"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.5-dev233"
+#define DUCKDB_VERSION "v1.5.5-dev238"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "d8e8cd9aea"
+#define DUCKDB_SOURCE_ID "e14c3db187"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
