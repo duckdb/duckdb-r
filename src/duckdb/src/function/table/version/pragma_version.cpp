0b1ae5d4ebdb0f32c13020d8adb29fb17eee96e2
krlmlr <krlmlr@users.noreply.github.com>
2026-07-19 09:16:34 +0000

    vendor: Update vendored sources to duckdb/duckdb@39391b92ca9ef97e56d20311c93fbe6fc5b16f88
    Date: 2026-07-01 13:57:29 +0200
    
    Backport https://redirect.github.com/duckdb/duckdb/pull/23468 and https://redirect.github.com/duckdb/duckdb/pull/23510 (https://redirect.github.com/duckdb/duckdb/pull/23529)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "5-dev105"
+#define DUCKDB_PATCH_VERSION "5-dev108"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.5-dev105"
+#define DUCKDB_VERSION "v1.5.5-dev108"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "da9f597c75"
+#define DUCKDB_SOURCE_ID "39391b92ca"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
