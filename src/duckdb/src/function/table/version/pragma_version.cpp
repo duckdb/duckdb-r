a0b786abeac43ccb9ae9c5650268ffeb65c548d8
krlmlr <krlmlr@users.noreply.github.com>
2026-07-19 10:22:29 +0000

    vendor: Update vendored sources to duckdb/duckdb@f13d14e616a7d88d66a01f0588bae66b55af9add
    Date: 2026-07-09 15:20:59 -0700
    
    Fix DROP COLUMN corrupting per-column metadata block bookkeeping (https://redirect.github.com/duckdb/duckdb/pull/23714)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "5-dev161"
+#define DUCKDB_PATCH_VERSION "5-dev163"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.5-dev161"
+#define DUCKDB_VERSION "v1.5.5-dev163"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ce4ba75ed8"
+#define DUCKDB_SOURCE_ID "f13d14e616"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
