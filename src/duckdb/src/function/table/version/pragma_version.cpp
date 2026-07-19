c4480d323bbc28fd643d61c214bc539aa6ae5ae6
krlmlr <krlmlr@users.noreply.github.com>
2026-07-19 09:16:43 +0000

    vendor: Update vendored sources to duckdb/duckdb@599574338bfac54f5a8f993e710fd6eaf53270a0
    Date: 2026-07-03 10:22:12 +0200
    
    fix out-of-bounds read on empty byte array decimals (https://redirect.github.com/duckdb/duckdb/pull/23567)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "5-dev137"
+#define DUCKDB_PATCH_VERSION "5-dev139"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.5-dev137"
+#define DUCKDB_VERSION "v1.5.5-dev139"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "9a8e50ddee"
+#define DUCKDB_SOURCE_ID "599574338b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
