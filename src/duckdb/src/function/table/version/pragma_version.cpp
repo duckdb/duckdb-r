e887d6dabfaaa9e8d06f1efff754c53a8ad7d11c
krlmlr <krlmlr@users.noreply.github.com>
2026-07-19 09:16:48 +0000

    vendor: Update vendored sources to duckdb/duckdb@bd77d596392f567d1d8cc18d45874c78b145d3d3
    Date: 2026-07-07 11:27:33 +0200
    
    Fixed unsafe iteration when parent is `NULL` in string cast (https://redirect.github.com/duckdb/duckdb/pull/23593)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "5-dev148"
+#define DUCKDB_PATCH_VERSION "5-dev150"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.5-dev148"
+#define DUCKDB_VERSION "v1.5.5-dev150"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "4af19ec154"
+#define DUCKDB_SOURCE_ID "bd77d59639"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
