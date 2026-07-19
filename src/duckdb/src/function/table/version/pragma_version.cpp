f805e8006d021e7195ed6a1d7a5b0dd8123e5f91
krlmlr <krlmlr@users.noreply.github.com>
2026-07-19 09:16:29 +0000

    vendor: Update vendored sources to duckdb/duckdb@fda2906db545de8921dd939ccbdc79e35b666f1e
    Date: 2026-06-26 22:20:43 +0200
    
    Improve rle corruption error messages (https://redirect.github.com/duckdb/duckdb/pull/23480)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "5-dev89"
+#define DUCKDB_PATCH_VERSION "5-dev92"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.5-dev89"
+#define DUCKDB_VERSION "v1.5.5-dev92"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "dc72a8dc06"
+#define DUCKDB_SOURCE_ID "fda2906db5"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
