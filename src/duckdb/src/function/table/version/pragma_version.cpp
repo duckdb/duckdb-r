807c3e7bedd5adaa0667883c0473b58db9ea233c
krlmlr <krlmlr@users.noreply.github.com>
2026-07-19 09:16:30 +0000

    vendor: Update vendored sources to duckdb/duckdb@db41de85b1b3360600c2ed7eb2c1627b35dff426
    Date: 2026-06-29 09:13:19 +0200
    
    Issue https://redirect.github.com/duckdb/duckdb/pull/23457: Ordered FIRST_VALUE Framing  (https://redirect.github.com/duckdb/duckdb/pull/23499)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "5-dev92"
+#define DUCKDB_PATCH_VERSION "5-dev96"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.5-dev92"
+#define DUCKDB_VERSION "v1.5.5-dev96"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "fda2906db5"
+#define DUCKDB_SOURCE_ID "db41de85b1"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
