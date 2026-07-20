7ce1e4f52e266a07e564f99ac7ea8352d9df8ad5
krlmlr <krlmlr@users.noreply.github.com>
2026-07-20 17:32:22 +0000

    vendor: Update vendored sources to duckdb/duckdb@4a11fb2555db746a6bfbc9bafc1018d7638369fa
    Date: 2026-07-20 10:15:28 +0200
    
    [v1.5 backport] Fix: reset empty_range in the TIMESTAMP range() table function (https://redirect.github.com/duckdb/duckdb/pull/23879)
    Bump Postgres extension for 1.5 (https://redirect.github.com/duckdb/duckdb/pull/23930)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "5-dev238"
+#define DUCKDB_PATCH_VERSION "5-dev242"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 5
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 1
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v1.5.5-dev238"
+#define DUCKDB_VERSION "v1.5.5-dev242"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "e14c3db187"
+#define DUCKDB_SOURCE_ID "4a11fb2555"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
