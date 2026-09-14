354087589492ea0b899cd998f0ac3be5f18689f4
Kirill Müller <kirill@cynkra.com>
2026-09-14 07:50:15 +0000

    vendor: Update vendored sources to duckdb/duckdb@5aee32f9e667c1e4b70dbb58203f4ab75ab77501
    Date: 2026-09-07 07:15:52 +0000
    
    Improve parquet bloom filter pruning for IN operator on NULL (https://redirect.github.com/duckdb/duckdb/pull/25225)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev83907"
+#define DUCKDB_PATCH_VERSION "0-dev83910"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 2
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v2.0.0-dev83907"
+#define DUCKDB_VERSION "v2.0.0-dev83910"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "2c6ec25a44"
+#define DUCKDB_SOURCE_ID "5aee32f9e6"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
