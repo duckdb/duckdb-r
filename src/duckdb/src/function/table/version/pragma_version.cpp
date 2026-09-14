b9b3cef475e6ba29af8dec823be49646a41ed8cc
Kirill Müller <kirill@cynkra.com>
2026-09-14 07:49:37 +0000

    vendor: Update vendored sources to duckdb/duckdb@52a6368d41f6166cc6cdc511e48ff3eb0ef5203a
    Date: 2026-09-07 07:05:33 +0000
    
    Report missing prepared statement parameters in declaration order (https://redirect.github.com/duckdb/duckdb/pull/25299) (https://redirect.github.com/duckdb/duckdb/pull/25336)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev83898"
+#define DUCKDB_PATCH_VERSION "0-dev83900"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 2
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v2.0.0-dev83898"
+#define DUCKDB_VERSION "v2.0.0-dev83900"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "487155f6ef"
+#define DUCKDB_SOURCE_ID "52a6368d41"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
