6df455c3f08dfde4f08e49e626b2b2f9c5c88c94
Kirill Müller <kirill@cynkra.com>
2026-09-14 07:51:13 +0000

    vendor: Update vendored sources to duckdb/duckdb@1c832a351bf426c2c03d6a56936072e3a82b497a
    Date: 2026-09-07 07:24:08 +0000
    
    Fix missing `deleted_index`assignment on local table storage when table schema change (https://redirect.github.com/duckdb/duckdb/pull/25400)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev83919"
+#define DUCKDB_PATCH_VERSION "0-dev83922"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 2
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v2.0.0-dev83919"
+#define DUCKDB_VERSION "v2.0.0-dev83922"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "eb364821c3"
+#define DUCKDB_SOURCE_ID "1c832a351b"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
