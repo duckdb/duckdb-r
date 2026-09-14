5f80ad9bb8c14e14ddef1c5e7995824cdd2e884a
Kirill Müller <kirill@cynkra.com>
2026-09-14 07:48:35 +0000

    vendor: Update vendored sources to duckdb/duckdb@31151d40e6906f2056db8a2c45b336b153c9f871
    Date: 2026-09-07 07:01:43 +0000
    
    Properly transform qualified columns in rename/exclude (https://redirect.github.com/duckdb/duckdb/pull/25370)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev83887"
+#define DUCKDB_PATCH_VERSION "0-dev83889"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 2
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v2.0.0-dev83887"
+#define DUCKDB_VERSION "v2.0.0-dev83889"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "ae4bc674cf"
+#define DUCKDB_SOURCE_ID "31151d40e6"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
