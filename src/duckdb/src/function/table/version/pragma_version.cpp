9052aa68c489634201664b247314c41b92eb974b
Kirill Müller <kirill@cynkra.com>
2026-09-14 07:47:35 +0000

    vendor: Update vendored sources to duckdb/duckdb@71ec7fc9b9c3e23d2c6d65ef9a7ebdcda919564e
    Date: 2026-09-07 06:49:45 +0000
    
    Tidy snapshot handling in the transaction manager (https://redirect.github.com/duckdb/duckdb/pull/25389)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev83879"
+#define DUCKDB_PATCH_VERSION "0-dev83881"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 2
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v2.0.0-dev83879"
+#define DUCKDB_VERSION "v2.0.0-dev83881"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "48fecd2aa2"
+#define DUCKDB_SOURCE_ID "71ec7fc9b9"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
