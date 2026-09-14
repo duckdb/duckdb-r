98410231ce215e7513edfd808aea656c904f1e10
Kirill Müller <kirill@cynkra.com>
2026-09-14 07:53:47 +0000

    vendor: Update vendored sources to duckdb/duckdb@de6a4203c17e9c5973629ee50cfa405fc2c328b6
    Date: 2026-09-07 12:31:57 +0000
    
    Fix `PIVOT` allowing optional parenthesis (https://redirect.github.com/duckdb/duckdb/pull/25420)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev83958"
+#define DUCKDB_PATCH_VERSION "0-dev83961"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 2
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v2.0.0-dev83958"
+#define DUCKDB_VERSION "v2.0.0-dev83961"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "f7cbbed637"
+#define DUCKDB_SOURCE_ID "de6a4203c1"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
