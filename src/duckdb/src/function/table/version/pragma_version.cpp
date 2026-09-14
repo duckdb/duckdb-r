103062068488ddc61d778dc13a1b3a706f8c87b7
Kirill Müller <kirill@cynkra.com>
2026-09-14 07:53:08 +0000

    vendor: Update vendored sources to duckdb/duckdb@e45ab0f26c658bdffbe5e54b9fb8fcd3cd88a08d
    Date: 2026-09-07 10:41:24 +0000
    
    add debug and deprecated settings (https://redirect.github.com/duckdb/duckdb/pull/25330)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev83939"
+#define DUCKDB_PATCH_VERSION "0-dev83951"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 2
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v2.0.0-dev83939"
+#define DUCKDB_VERSION "v2.0.0-dev83951"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "cd8b915e99"
+#define DUCKDB_SOURCE_ID "e45ab0f26c"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
