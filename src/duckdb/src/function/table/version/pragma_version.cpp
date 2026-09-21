191e68535c1f89c7c8fac370a49b54e4d8127bbf
Kirill Müller <kirill@cynkra.com>
2026-09-21 07:46:11 +0000

    vendor: Update vendored sources to duckdb/duckdb@bcff50365875c455f6c402c06838007f0ced9ca7
    Date: 2026-09-21 06:48:45 +0000
    
    Fix individual UTF-8 validation bypass in DELTA_LENGTH_BYTE_ARRAY decoder (https://redirect.github.com/duckdb/duckdb/pull/25853)

--- src/duckdb/src/function/table/version/pragma_version.cpp
+++ src/duckdb/src/function/table/version/pragma_version.cpp
@@ -1,5 +1,5 @@
 #ifndef DUCKDB_PATCH_VERSION
-#define DUCKDB_PATCH_VERSION "0-dev85257"
+#define DUCKDB_PATCH_VERSION "0-dev85263"
 #endif
 #ifndef DUCKDB_MINOR_VERSION
 #define DUCKDB_MINOR_VERSION 0
@@ -8,10 +8,10 @@
 #define DUCKDB_MAJOR_VERSION 2
 #endif
 #ifndef DUCKDB_VERSION
-#define DUCKDB_VERSION "v2.0.0-dev85257"
+#define DUCKDB_VERSION "v2.0.0-dev85263"
 #endif
 #ifndef DUCKDB_SOURCE_ID
-#define DUCKDB_SOURCE_ID "114c56196d"
+#define DUCKDB_SOURCE_ID "bcff503658"
 #endif
 #include "duckdb/function/table/system_functions.hpp"
 #include "duckdb/main/database.hpp"
