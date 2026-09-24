7b6317b72cf84464c24f208ed2a8e408de9600fe
Kirill Müller <kirill@cynkra.com>
2026-09-24 11:46:22 +0000

    vendor: Update vendored sources to duckdb/duckdb@3f59160bb2d74e22ea53b346afbf204ec1b3de4f
    Date: 2026-09-24 10:50:29 +0000
    
    [MultiFileReader] Make missing mapping-candidate a recoverable error (https://redirect.github.com/duckdb/duckdb/pull/26116)

--- src/duckdb/generated_extension_loader_package_build.cpp
+++ src/duckdb/generated_extension_loader_package_build.cpp
@@ -33,7 +33,7 @@ int32_t duckdb_extension_parquet_describe(duckdb_extension_descriptor *descripto
 	descriptor->version = 1;
 	descriptor->name = "parquet";
 	descriptor->extension_version = EXT_VERSION_PARQUET;
-	descriptor->api_version = "233847abfa";
+	descriptor->api_version = "3f59160bb2";
 	descriptor->entry_cpp = (void (*)(void))parquet_duckdb_cpp_init;
 	return 0;
 }
@@ -66,7 +66,7 @@ int32_t duckdb_extension_core_functions_describe(duckdb_extension_descriptor *de
 	descriptor->version = 1;
 	descriptor->name = "core_functions";
 	descriptor->extension_version = EXT_VERSION_CORE_FUNCTIONS;
-	descriptor->api_version = "233847abfa";
+	descriptor->api_version = "3f59160bb2";
 	descriptor->entry_cpp = (void (*)(void))core_functions_duckdb_cpp_init;
 	return 0;
 }
